749b59292d57d3dfdd1e714f30cf0c9885f4b430
Lukas <lukasstrassel@googlemail.com>
2022-01-17 13:07:59 +0100

    feat: some initial layout (#2)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import theme from "../src/utils/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import "/public/fonts/inter/inter.css";
 import { MainLayout } from "../src/layouts/MainLayout";
@@ -23,13 +20,9 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </ThemeProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </CacheProvider>
   );
 }
