6d81344e8d49f7f530e61e51a0e76f3afdf01e45
sendra <polsendra@gmail.com>
2022-01-18 19:26:50 +0100

    fix: some cleaning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,7 @@ import '/public/fonts/inter/inter.css';
 import { MainLayout } from '../src/layouts/MainLayout';
 import { LanguageProvider } from '../src/libs/LanguageProvider';
 import dynamic from 'next/dynamic';
-// import { getSupportedChainIds } from 'src/helpers/config/markets-and-network-config';
-// import { Web3ReactProvider } from '@web3-react/core';
-// import { Web3Provider } from 'src/libs/web3-data-provider';
-// import { Web3ContextProvider } from 'src/libs/web3-data-provider';
-
-// @ts-ignore
+// @ts-expect-error this is to dynamically load the web3 provider so it has the windows object
 const Web3ContextProvider = dynamic(() => import("../src/libs/web3-data-provider").then(mod => mod.Web3ContextProvider), {
   ssr: false,
 });
@@ -24,10 +19,6 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-// function getWeb3Library(provider: any): ethers.providers.Web3Provider {
-//   return new ethers.providers.Web3Provider(provider);
-// }
-
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
@@ -35,17 +26,13 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      {/* <Web3ReactProvider getLibrary={getWeb3Library}>
-        <Web3Provider supportedChainIds={getSupportedChainIds()}> */}
-        <Web3ContextProvider>
-          <LanguageProvider>
+        <LanguageProvider>
+          <Web3ContextProvider>
             <MainLayout>
               <Component {...pageProps} />
             </MainLayout>
-          </LanguageProvider>
-        </Web3ContextProvider>
-        {/* </Web3Provider> */}
-      {/* </Web3ReactProvider> */}
+          </Web3ContextProvider>
+         </LanguageProvider>
     </CacheProvider>
   );
 }
