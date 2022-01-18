5775e203958c49fa13ef588e74ff74c4fa537c20
sendra <polsendra@gmail.com>
2022-01-18 21:14:07 +0100

    feat: added ens name. remaining to print the avatar correcly

--- src/layouts/ConnectWalletWidget.tsx
+++ src/layouts/ConnectWalletWidget.tsx
@@ -1,21 +1,104 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Divider, ListItemIcon, ListItemText, Menu, MenuItem, MenuList, Paper, Typography } from "@mui/material";
 import { useWeb3Context } from "src/libs/web3-data-provider";
+import { Cloud, ContentCopy, ContentCut, ContentPaste } from "@mui/icons-material";
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import RemoveCircleOutlineRoundedIcon from '@mui/icons-material/RemoveCircleOutlineRounded';
+import { getNetworkConfig } from "src/helpers/config/markets-and-network-config";
+import useGetEns from "src/libs/hooks/use-get-ens";
 
 export default function ConnectWalletWidget() {
-  const {connectWallet, disconnectWallet} = useWeb3Context();
+  const {connectWallet, disconnectWallet, currentAccount, connected, networkName, networkId} = useWeb3Context();
+
+  const { name: ensName, avatar: ensAvatar } = useGetEns(currentAccount);
   
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event: { currentTarget: React.SetStateAction<null>; }) => {
+    if (!connected) {
+      connectWallet();
+    } else {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDisconnect = () => {
+    if(connected) {
+      disconnectWallet()
+      setAnchorEl(null);
+    }
+  };
+
+  const handleCopy = async () => {
+    navigator.clipboard.writeText(currentAccount)
+    setAnchorEl(null);
+  };
+
+  const handleEtherscanLink = () => {
+    // link to etherscan in new window
+    const networkConfig = getNetworkConfig(networkId);
+    const explorerLink = `${networkConfig.explorerLink}/address/${currentAccount}`;
+    console.log('explorer link: ', explorerLink)
+    window.open(explorerLink, "_blank");
+    setAnchorEl(null);
+  };
+
+  console.log('cionnected: ', connected)
   return (
     <div>
       <Button
-        onClick={connectWallet}
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
       >
-        Connect Wallet
+        {currentAccount? (<div>ensAvatar {ensName? ensName : currentAccount}</div>) : 'Connect Wallet'}
       </Button>
-      <Button onClick={disconnectWallet}>
-        Disconnect
-      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem>
+          <p>Network </p>
+          {networkName}
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleCopy}>
+          <ListItemIcon>
+            <ContentCopyRoundedIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Copy address</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleEtherscanLink}>
+          <ListItemIcon>
+            <OpenInNewRoundedIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>View on Etherscan</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleDisconnect}>
+          <ListItemIcon>
+            <RemoveCircleOutlineRoundedIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Disconnect Wallet</ListItemText>
+        </MenuItem>
+        
+      </Menu>
     </div>
+
+
+    
   )
   
 }
