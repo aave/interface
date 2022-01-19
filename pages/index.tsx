c833230e7454ec909da9bfb81b1d33b7b6f6dfb6
Lukas <lukasstrassel@googlemail.com>
2022-01-19 11:32:38 +0100

    feat: new token component (#11)
    * feat: new token component
    
    * fix: finish up tokenIcon component
    
    * fix: move atoken to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { Link } from '../src/components/Link';
 import { Trans } from '@lingui/macro';
+import { TokenIcon } from 'src/components/TokenIcon';
 // import { useProtocolDataContext } from '../src/hooks/useProtocolData';
 
 const Home: NextPage = () => {
@@ -26,6 +27,8 @@ const Home: NextPage = () => {
         <Link href="/about" color="secondary">
           <Trans>Go to About page</Trans>
         </Link>
+        <TokenIcon symbol="aave" />
+        <TokenIcon symbol="aave" aToken />
       </Box>
     </Container>
   );
