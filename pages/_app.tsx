52087c1ebc7f528b7dde2c2f0e210f9c02ca6e1c
sendra <polsendra@gmail.com>
2022-01-19 12:41:41 +0100

    fix: fixed some styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
 import '/public/fonts/inter/inter.css';
 
-import { ApolloProvider, ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import * as React from 'react';
-import { apolloClient, apolloClient } from 'src/utils/apolloClient';
+import { apolloClient } from 'src/utils/apolloClient';
 
 import createEmotionCache from '../src/createEmotionCache';
-import { ProtocolDataProvider, ProtocolDataProvider } from '../src/hooks/useProtocolData';
+import { ProtocolDataProvider } from '../src/hooks/useProtocolData';
 import { MainLayout } from '../src/layouts/MainLayout';
 import { LanguageProvider } from '../src/libs/LanguageProvider';
-// @ts-expect-error this is to dynamically load the web3 provider so it has the windows object
+
 const Web3ContextProvider = dynamic(
+  // @ts-expect-error this is to dynamically load the web3 provider so it has the windows object
   () => import('../src/libs/web3-data-provider').then((mod) => mod.Web3ContextProvider),
   {
     ssr: false,
