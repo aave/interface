287e6a97c675979aef4da9e51fd24634750e9af1
sendra <polsendra@gmail.com>
2022-01-19 14:52:53 +0100

    fix: removed console log

--- src/layouts/WalletWidget.tsx
+++ src/layouts/WalletWidget.tsx
@@ -72,7 +72,7 @@ export default function WalletWidget() {
     window.open(explorerLink, '_blank');
     setAnchorEl(null);
   };
-  console.log('connected: ', connected, ' account: ', currentAccount);
+
   return (
     <div>
       <Button
