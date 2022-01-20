eafa77d90babf461a7b5670045ac332ef643dd34
Lukas <lukasstrassel@googlemail.com>
2022-01-20 11:58:14 +0100

    fix: use js over json

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', 'import', 'simple-import-sort', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'warn',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-named-as-default': 'error',
+    'import/no-unresolved': 'warn',
+    // disabled as with the static export Image does not make to much sense
+    '@next/next/no-img-element': 'off',
+  },
+};
