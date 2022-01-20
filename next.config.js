5c0828535726734c86b9ec6d16514b8600a438dc
Alex <argeares97@gmail.com>
2022-01-20 14:07:09 +0200

    feat: added `svgr`
    fix: AppGlobalStyles & favicon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+
   reactStrictMode: true,
   // assetPrefix: "./",
   trailingSlash: true,
