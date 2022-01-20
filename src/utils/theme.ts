d177f8b3893bd00a76521e62be2f15a6bb521f60
Alex <argeares97@gmail.com>
2022-01-20 15:10:24 +0200

    fix: theme colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,63 +1,91 @@
-import { red } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 declare module '@mui/material/styles/createPalette' {
-  interface ColorRange {
-    50: string;
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
+  interface TypeBackground {
+    default: string;
+    paper: string;
+    surface: string;
+    header: string;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface PaletteColor extends ColorRange {}
+  interface Palette {
+    gradients: {
+      aaveGradient: string;
+    };
+  }
 
   interface PaletteOptions {
-    primaryDark: PaletteColorOptions;
-  }
-  interface Palette {
-    primaryDark: PaletteColor;
+    gradients: {
+      aaveGradient: string;
+    };
   }
 }
 
-export const blueDark = {
-  50: '#E2EDF8',
-  100: '#CEE0F3',
-  200: '#91B9E3',
-  300: '#5090D3',
-  main: '#5090D3',
-  400: '#265D97',
-  500: '#1E4976',
-  600: '#173A5E',
-  700: '#132F4C', // contrast 13.64:1
-  800: '#001E3C',
-  900: '#0A1929',
-};
-
 // Create a theme instance.
-export const getTheme = (mode: 'light' | 'dark') =>
-  createTheme({
+export const getTheme = (mode: 'light' | 'dark') => {
+  const getColor = (lightColor: string, darkColor: string) =>
+    mode === 'dark' ? darkColor : lightColor;
+
+  return createTheme({
+    breakpoints: {
+      keys: ['xs', 'sm', 'md', 'lg', 'xl'],
+      values: { xs: 0, sm: 768, md: 1024, lg: 1728, xl: 1920 },
+    },
     palette: {
+      mode,
       primary: {
-        main: '#171926',
+        main: getColor('#00244D', '#FAFBFC'),
+        light: getColor('#47617F', '#FAFBFC'),
+        dark: getColor('#00244D', '#FAFBFCA3'),
       },
-      primaryDark: blueDark,
       secondary: {
-        main: '#19857b',
+        main: getColor('#FF607B', '#F48FB1'),
+        light: getColor('#FF607B', '#F6A5C0'),
+        dark: getColor('#FF607B', '#AA647B'),
       },
       error: {
-        main: red.A400,
+        main: getColor('#BC0000B8', '#F44336'),
+        light: getColor('#BC0000B8', '#E57373'),
+        dark: getColor('#BC0000', '#D32F2F'),
+      },
+      warning: {
+        main: getColor('#F89F1A', '#FFA726'),
+        light: getColor('#FFCE00', '#FFB74D'),
+        dark: getColor('#F89F1A', '#F57C00'),
+      },
+      info: {
+        main: getColor('#0062D2', '#29B6F6'),
+        light: getColor('#0062D2', '#4FC3F7'),
+        dark: getColor('#0062D2', '#0288D1'),
+      },
+      success: {
+        main: getColor('#4CAF50', '#66BB6A'),
+        light: getColor('#46BC4B', '#90FF95'),
+        dark: getColor('#46BC4B', '#388E3C'),
+      },
+      text: {
+        primary: getColor('#00244D', '#FFFFFF'),
+        secondary: getColor('#47617F', '#FFFFFFB2'),
+        disabled: getColor('#00244D61', '#FFFFFF80'),
       },
       background: {
-        default: mode === 'dark' ? '#171926' : '#ffffff',
+        default: getColor('#FAFBFC', '#090815'),
+        paper: getColor('#FFFFFF', '#272631'),
+        surface: getColor('#FAFBFC', '#2c2d3e'),
+        header: getColor('#020e1f', '#090815'),
       },
-      mode,
+      divider: getColor('#E0E5EA', '#FFFFFF1F'),
+      action: {
+        active: getColor('#00244D8A', '#FFFFFF8F'),
+        hover: getColor('#00244D0A', '#FFFFFF14'),
+        selected: getColor('#00244D14', '#FFFFFF29'),
+        disabled: getColor('#00244D42', '#FFFFFF4D'),
+        disabledBackground: getColor('#00244D42', '#FFFFFF1F'),
+        focus: getColor('#00244D1F', '#FFFFFF1F'),
+      },
+      gradients: {
+        aaveGradient: 'linear-gradient(248.86deg, #B6509E 10.51%, #2EBAC6 93.41%);',
+      },
     },
     spacing: 4,
     typography: {
@@ -73,5 +101,6 @@ export const getTheme = (mode: 'light' | 'dark') =>
       },
     },
   });
+};
 
 export default getTheme('light');
