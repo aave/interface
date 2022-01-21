bd483516fd904c2a7589d2e1cae0f6b8840a23e6
Lukas <lukasstrassel@googlemail.com>
2022-01-21 13:29:04 +0100

    feat: add bg data provider refresh

--- src/hooks/app-data-provider/BackgroundDataProvider.tsx
+++ src/hooks/app-data-provider/BackgroundDataProvider.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useIncentiveData } from './useIncentiveData';
 import { usePoolData } from './usePoolData';
 import { useUpdateWalletBalances } from './useWalletBalances';
 
 interface BackgroundDataProviderContextType {
   refetchWalletBalances: () => Promise<void>;
+  refechIncentiveData?: () => Promise<void>;
+  refetchPoolData?: () => Promise<void> | Promise<void[]>;
 }
 
-const BackgroundDataProviderContext = React.createContext<BackgroundDataProviderContextType>({
-  refetchWalletBalances: async () => {},
-});
+const BackgroundDataProviderContext = React.createContext<BackgroundDataProviderContextType>(
+  {} as BackgroundDataProviderContextType
+);
 
 /**
  * Naive provider that subscribes to different data sources to update the apollo cache.
@@ -17,12 +19,16 @@ const BackgroundDataProviderContext = React.createContext<BackgroundDataProvider
  * @returns
  */
 export const BackgroundDataProvider: React.FC = ({ children }) => {
-  // useIncentiveData();
-  // usePoolData();
+  const { refresh: refechIncentiveData } = useIncentiveData();
+  const { refresh: refetchPoolData } = usePoolData();
   const { refetch: refetchWalletBalances } = useUpdateWalletBalances();
   return (
-    <BackgroundDataProviderContext.Provider value={{ refetchWalletBalances }}>
+    <BackgroundDataProviderContext.Provider
+      value={{ refetchWalletBalances, refechIncentiveData, refetchPoolData }}
+    >
       {children}
     </BackgroundDataProviderContext.Provider>
   );
 };
+
+export const useBackgroundDataProvider = () => useContext(BackgroundDataProviderContext);
