0f7d1d7121da7d95133561dd869dffad57fa8ad9
Alex <argeares97@gmail.com>
2022-01-24 12:13:45 +0200

    fix: wallet widget

--- src/layouts/WalletWidget.tsx
+++ src/layouts/WalletWidget.tsx
@@ -1,11 +1,11 @@
 import { Trans } from '@lingui/macro';
 import { ContentCopy } from '@mui/icons-material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
 import {
   Box,
   Button,
   Divider,
+  ListItemIcon,
   ListItemText,
   Menu,
   MenuItem,
@@ -21,6 +21,7 @@ import { useWeb3Context } from 'src/libs/web3-data-provider/Web3ContextProvider'
 import { getNetworkConfig } from 'src/utils/marketsAndNetworksConfig';
 
 import DisconnectIcon from '/public/icons/disconnect.svg';
+import LinkIcon from '/public/icons/link.svg';
 
 import { textCenterEllipsis } from '../helpers/text-center-ellipsis';
 
@@ -92,28 +93,28 @@ export default function WalletWidget() {
       textCenterEllipsis(currentAccount, 4, 4)
     )
   ) : (
-    <Trans>Connect Wallet</Trans>
+    <Trans>Connect wallet</Trans>
   );
 
   return (
     <>
       <Button
-        variant="surface"
+        variant={connected ? 'surface' : 'gradient'}
         aria-label="wallet"
         id="wallet-button"
         aria-controls={open ? 'wallet-button' : undefined}
         aria-expanded={open ? 'true' : undefined}
         aria-haspopup="true"
         onClick={handleClick}
-        sx={{ padding: '5px 12px 5px 8px' }}
+        sx={{ p: connected ? '5px 8px' : undefined }}
         startIcon={
           connected && (
             <Box
               sx={{
-                width: 24,
-                height: 24,
+                width: 22,
+                height: 22,
                 borderRadius: '50%',
-                border: '1px solid #3E4365',
+                border: '1px solid #FAFBFC1F',
                 img: { width: '100%', height: '100%', borderRadius: '50%' },
               }}
             >
@@ -145,14 +146,14 @@ export default function WalletWidget() {
         }}
       >
         <MenuList disablePadding>
-          <MenuItem onClick={handleCopy}>
+          <MenuItem>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Box
                 sx={{
                   width: 36,
                   height: 36,
                   borderRadius: '50%',
-                  border: '1px solid #3E4365',
+                  border: '1px solid #FAFBFC1F',
                   mr: 3,
                   img: { width: '100%', height: '100%', borderRadius: '50%' },
                 }}
@@ -164,19 +165,15 @@ export default function WalletWidget() {
                 />
               </Box>
               <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                {ensNameAbbreviated && (
-                  <Typography variant="main16">{ensNameAbbreviated}</Typography>
-                )}
+                {ensNameAbbreviated && <Typography variant="h4">{ensNameAbbreviated}</Typography>}
 
                 <Typography
-                  variant={ensNameAbbreviated ? 'secondary12' : 'main16'}
-                  color={ensNameAbbreviated ? 'primary.light' : 'primary.main'}
+                  variant={ensNameAbbreviated ? 'caption' : 'h4'}
+                  color={ensNameAbbreviated ? 'text.secondary' : 'text.primary'}
                 >
                   {textCenterEllipsis(currentAccount, ensNameAbbreviated ? 12 : 7, 4)}
                 </Typography>
               </Box>
-
-              <ContentCopy fontSize="small" sx={{ ml: 3 }} />
             </Box>
           </MenuItem>
           <Divider />
@@ -191,51 +188,64 @@ export default function WalletWidget() {
                   mb: 1,
                 }}
               >
-                <Typography variant="secondary14" color="primary.light">
+                <Typography variant="caption" color="text.secondary">
                   <Trans>Network</Trans>
                 </Typography>
-                <Typography
-                  variant="buttonS"
-                  sx={{ borderRadius: '6px', border: '1px solid #E0E5EA', p: '0 6px' }}
-                >
+
+                <Button size="small" variant="outlined">
                   <Trans>Switch</Trans>
-                </Typography>
+                </Button>
               </Box>
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <Box
                   sx={{
                     bgcolor: networkColor,
-                    width: 7,
-                    height: 7,
+                    width: 6,
+                    height: 6,
                     mr: 2,
-                    border: '1px solid #E6E8F0',
+                    boxShadow: '0px 2px 1px rgba(0, 0, 0, 0.05), 0px 0px 1px rgba(0, 0, 0, 0.25)',
                     borderRadius: '50%',
                   }}
                 />
-                <Typography variant="main14">{networkConfig.name}</Typography>
+                <Typography variant="subheader1">{networkConfig.name}</Typography>
               </Box>
             </Box>
           </MenuItem>
           <Divider />
+
+          <MenuItem onClick={handleCopy}>
+            <ListItemIcon>
+              <ContentCopy fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>
+              <Trans>Copy address</Trans>
+            </ListItemText>
+          </MenuItem>
 
           <MenuItem
             component={Link}
             href={networkConfig.explorerLinkBuilder({ address: currentAccount })}
             onClick={handleClose}
           >
+            <ListItemIcon>
+              <SvgIcon fontSize="small">
+                <LinkIcon />
+              </SvgIcon>
+            </ListItemIcon>
             <ListItemText>
               <Trans>View on Etherscan</Trans>
             </ListItemText>
-            <OpenInNewRoundedIcon fontSize="small" />
           </MenuItem>
 
           <MenuItem onClick={handleDisconnect}>
+            <ListItemIcon>
+              <SvgIcon fontSize="small">
+                <DisconnectIcon />
+              </SvgIcon>
+            </ListItemIcon>
             <ListItemText>
               <Trans>Disconnect Wallet</Trans>
             </ListItemText>
-            <SvgIcon fontSize="small">
-              <DisconnectIcon />
-            </SvgIcon>
           </MenuItem>
         </MenuList>
       </Menu>
