73220825a61fd117c3ea0ecd9fe6ee5d2c5b695b
Lukas <lukasstrassel@googlemail.com>
2022-01-24 15:06:07 +0100

    feat: add trans

--- src/layouts/SettingsMenu.tsx
+++ src/layouts/SettingsMenu.tsx
@@ -1,34 +1,57 @@
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
 import {
   Button,
   FormControlLabel,
+  ListItemIcon,
   ListItemText,
   Menu,
   MenuItem,
-  MenuList,
   SvgIcon,
   Switch,
+  Typography,
 } from '@mui/material';
 import { useTheme } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
+import { t, Trans } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
+import { CheckIcon, ChevronLeftIcon } from '@heroicons/react/solid';
 
 import MenuSettingsIcon from '/public/icons/menuSettings.svg';
-
 import { ColorModeContext } from './AppGlobalStyles';
-import { LanguageSelector } from 'src/components/LanguageSelector';
+import { dynamicActivateLanguage } from 'src/libs/LanguageProvider';
+
+const langMap = {
+  en: t`English`,
+  es: t`Spanish`,
+};
 
 export function SettingsMenu() {
+  const { i18n } = useLingui();
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
+  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [languagesOpen, setLanguagesOpen] = useState(false);
 
-  const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+  const handleSettingsClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setAnchorEl(event.currentTarget);
+    setSettingsOpen(true);
+    setLanguagesOpen(false);
   };
+
+  const handleLanguageClick = () => {
+    setSettingsOpen(false);
+    setLanguagesOpen(true);
+  };
+
+  const handleCloseLanguage = () => {
+    setSettingsOpen(true);
+    setLanguagesOpen(false);
+  };
+
   const handleClose = () => {
     setAnchorEl(null);
+    setSettingsOpen(false);
+    setLanguagesOpen(false);
   };
 
   return (
@@ -37,10 +60,10 @@ export function SettingsMenu() {
         variant="surface"
         aria-label="settings"
         id="settings-button"
-        aria-controls={open ? 'settings-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
+        aria-controls={settingsOpen ? 'settings-menu' : undefined}
+        aria-expanded={settingsOpen ? 'true' : undefined}
         aria-haspopup="true"
-        onClick={handleClick}
+        onClick={handleSettingsClick}
         sx={{ p: '7px 8px', minWidth: 'unset', ml: 2 }}
       >
         <SvgIcon sx={{ color: 'common.white' }} fontSize="small">
@@ -54,7 +77,7 @@ export function SettingsMenu() {
           'aria-labelledby': 'settings-button',
         }}
         anchorEl={anchorEl}
-        open={open}
+        open={settingsOpen}
         onClose={handleClose}
         PaperProps={{
           style: {
@@ -62,8 +85,15 @@ export function SettingsMenu() {
           },
         }}
       >
+        <MenuItem disabled>
+          <Typography variant="subheader2">
+            <Trans>Global settings</Trans>
+          </Typography>
+        </MenuItem>
         <MenuItem onClick={colorMode.toggleColorMode} disableRipple>
-          <ListItemText>Dark mode</ListItemText>
+          <ListItemText>
+            <Trans>Dark mode</Trans>
+          </ListItemText>
           <FormControlLabel
             value="darkmode"
             control={<Switch disableRipple checked={theme.palette.mode === 'dark'} />}
@@ -71,10 +101,53 @@ export function SettingsMenu() {
             labelPlacement="start"
           />
         </MenuItem>
-        <MenuItem disableRipple>
-          <ListItemText>Language</ListItemText>
-          <LanguageSelector />
+        <MenuItem onClick={handleLanguageClick} disableRipple>
+          <ListItemText>
+            <Trans>Language</Trans>
+          </ListItemText>
+          {langMap[i18n.locale as keyof typeof langMap]}
         </MenuItem>
+      </Menu>
+      <Menu
+        id="settings-menu"
+        MenuListProps={{
+          'aria-labelledby': 'settings-button',
+        }}
+        anchorEl={anchorEl}
+        open={languagesOpen}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            minWidth: 240,
+          },
+        }}
+      >
+        <MenuItem onClick={handleCloseLanguage}>
+          <ListItemIcon>
+            <SvgIcon fontSize="small">
+              <ChevronLeftIcon />
+            </SvgIcon>
+          </ListItemIcon>
+          <ListItemText disableTypography>
+            <Typography variant="subheader2">
+              <Trans>Select language</Trans>
+            </Typography>
+          </ListItemText>
+        </MenuItem>
+        {Object.keys(langMap).map((lang) => (
+          <MenuItem disableRipple key={lang} onClick={() => dynamicActivateLanguage(lang)}>
+            <ListItemText>
+              <Trans>{langMap[lang as keyof typeof langMap]}</Trans>
+            </ListItemText>
+            {lang === i18n.locale && (
+              <ListItemIcon>
+                <SvgIcon fontSize="small">
+                  <CheckIcon />
+                </SvgIcon>
+              </ListItemIcon>
+            )}
+          </MenuItem>
+        ))}
       </Menu>
     </>
   );
