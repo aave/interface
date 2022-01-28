8e7c866f58ffd7f0ef971660959d99aa875ad65d
sendra <polsendra@gmail.com>
2022-01-28 10:03:14 +0100

    fix: removed unused stuff

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,23 +5,10 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
-import { AaveModal } from '../src/components/AaveModal/AaveModal';
-import { useAaveModal } from '../src/components/AaveModal/useAaveModal';
 import { Link } from '../src/components/primitives/Link';
-import { SupplyFlowModal } from '../src/flows/SupplyFlowModal/SupplyFlowModal';
 import { MainLayout } from '../src/layouts/MainLayout';
 
 export default function About() {
-  const [open, setOpen] = useAaveModal(false);
-  const [open2, setOpen2] = useAaveModal(false);
-  const args = {
-    tokenAddress: true,
-    supplyRewards: [{ tokenName: 'stkAAVE', tokenIcon: 'aave', apy: '6.78' }],
-    supplyApy: '4.6',
-    healthFactor: '3.2',
-    balance: '12340',
-    tokenSymbol: 'dai',
-  };
   return (
     <Container maxWidth="lg">
       <Box
@@ -42,19 +29,15 @@ export default function About() {
           </Button>
         </Box>
         <Box maxWidth="sm" sx={{ mt: 5 }}>
-          <Button variant="contained" onClick={() => setOpen(true)}>
+          <Button variant="contained">
             <Trans>Open Modal</Trans>
           </Button>
         </Box>
         <Box maxWidth="sm" sx={{ mt: 5 }}>
-          <Button variant="contained" onClick={() => setOpen2(true)}>
+          <Button variant="contained">
             <Trans>Open Supply Modal</Trans>
           </Button>
         </Box>
-        <AaveModal title="Sobre nosotros" open={open} onClose={() => setOpen(false)}>
-          Contenido
-        </AaveModal>
-        <SupplyFlowModal open={open2} onClose={() => setOpen2(false)} {...args} />
       </Box>
     </Container>
   );
