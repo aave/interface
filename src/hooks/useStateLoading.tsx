8a21151ab042846e12bcb50d9ee51d8902e5e3f4
kartojal <canillas.mail@gmail.com>
2022-01-31 11:55:35 +0100

    feat: simplify useLoading hook with booleans instead of enums. removed parameters for useGetGasPrices

--- src/hooks/useStateLoading.tsx
+++ src/hooks/useStateLoading.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react';
 
-export enum LOADING_STATE {
-  IDLE = 'idle',
-  LOADING = 'loading',
-  FINISHED = 'finished',
-}
+export const useStateLoading = (initState?: boolean) => {
+  const [state, setState] = useState(initState || false);
 
-const LOADING_STATES = [LOADING_STATE.IDLE, LOADING_STATE.LOADING];
-
-export const useStateLoading = (initState?: LOADING_STATE) => {
-  const [state, setState] = useState<LOADING_STATE>(initState || LOADING_STATE.IDLE);
-
-  const setLoading = (newState: LOADING_STATE) => {
-    if (LOADING_STATES.includes(state)) {
-      setState(newState);
-    }
+  const setLoading = (newState: boolean) => {
+    setState(!!newState);
   };
 
-  return { loading: LOADING_STATES.includes(state), setLoading };
+  return { loading: state, setLoading };
 };
