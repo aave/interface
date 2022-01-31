75d09226e8aa9729a77ce87864eff3809401bb2b
Lukas <lukasstrassel@googlemail.com>
2022-01-31 22:30:07 +0100

    feat: integrate supply modal

--- src/components/Supply/SupplyModal.tsx
+++ src/components/Supply/SupplyModal.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
+import { ModalType, useModalContext } from 'src/hooks/useModal';
 import { ActionModal } from '../primitives/ActionModal';
 import { SupplyModalContent } from './SupplyModalContent';
 
-export type SupplyProps = {
-  underlyingAsset: string;
-  open: boolean;
-  handleClose: () => void;
-};
-
-export const SupplyModal = ({ underlyingAsset, open, handleClose }: SupplyProps) => {
+export const SupplyModal = () => {
+  const { type, close, args } = useModalContext();
   return (
-    <ActionModal open={open} setOpen={handleClose}>
-      <SupplyModalContent underlyingAsset={underlyingAsset} handleClose={handleClose} />
+    <ActionModal open={type === ModalType.Supply} setOpen={close}>
+      {args?.underlyingAsset && (
+        <SupplyModalContent underlyingAsset={args.underlyingAsset} handleClose={close} />
+      )}
     </ActionModal>
   );
 };
