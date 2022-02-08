6f6d5a45578c333e21e66e49a58337e133209795
Lukas <lukasstrassel@googlemail.com>
2022-02-08 10:24:19 +0100

    feat: add improved asset input

--- src/components/AssetInput.tsx
+++ src/components/AssetInput.tsx
@@ -1,9 +1,19 @@
 import { Trans } from '@lingui/macro';
-import { Box, Button, Typography, InputBase } from '@mui/material';
+import {
+  Box,
+  Button,
+  Typography,
+  InputBase,
+  FormControl,
+  Select,
+  MenuItem,
+  SvgIcon,
+} from '@mui/material';
 import React from 'react';
 
 import { TokenIcon } from './primitives/TokenIcon';
 import { FormattedNumber } from './primitives/FormattedNumber';
+import { ChevronDownIcon } from '@heroicons/react/outline';
 
 export interface AssetInputProps {
   value: string;
@@ -12,6 +22,7 @@ export interface AssetInputProps {
   symbol: string;
   onChange: (value: string) => void;
   disabled?: boolean;
+  onSelect?: (asset: string) => void;
 }
 
 export const AssetInput: React.FC<AssetInputProps> = ({
@@ -21,6 +32,7 @@ export const AssetInput: React.FC<AssetInputProps> = ({
   symbol,
   onChange,
   disabled,
+  onSelect,
 }) => {
   const validNumber = new RegExp(/^\d*\.?\d*$/); // allow only digits with decimals
 
@@ -49,8 +61,42 @@ export const AssetInput: React.FC<AssetInputProps> = ({
             },
           }}
         />
-        <TokenIcon symbol={symbol} sx={{ mx: '4px' }} />
-        <Typography>{symbol}</Typography>
+        {onSelect ? (
+          <>
+            <TokenIcon symbol={symbol} sx={{ mx: '4px' }} />
+            <Typography>{symbol}</Typography>
+          </>
+        ) : (
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <Select
+              value={10}
+              onChange={onSelect}
+              variant="outlined"
+              IconComponent={(props) => (
+                <SvgIcon fontSize="medium" {...props}>
+                  <ChevronDownIcon />
+                </SvgIcon>
+              )}
+              renderValue={(asset) => {
+                return (
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <TokenIcon symbol={symbol} sx={{ mx: '4px' }} />
+                    <Typography>{symbol}</Typography>
+                  </Box>
+                );
+              }}
+            >
+              <MenuItem value={10}>
+                <SvgIcon fontSize="medium">
+                  <ChevronDownIcon />
+                </SvgIcon>
+                Ten
+              </MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        )}
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', pt: '4px' }}>
         <Typography sx={{ flexGrow: 1 }}>
