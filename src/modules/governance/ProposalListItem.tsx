81c25496fb38218ef5b4c3f05b47d17f92bc45ed
Lukas <lukasstrassel@googlemail.com>
2022-02-09 13:49:56 +0100

    fix: remove import

--- src/modules/governance/ProposalListItem.tsx
+++ src/modules/governance/ProposalListItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { GovernancePageProps } from 'pages/governance';
 import { Link, ROUTES } from 'src/components/primitives/Link';
 import { StateBadge } from './StateBadge';
