f795ec037711868023dc725ec670e57bc9c0db9c
Lukas <lukasstrassel@googlemail.com>
2022-02-11 10:30:29 +0100

    fix: move provider back to root

--- src/components/Borrow/BorrowModal.tsx
+++ src/components/Borrow/BorrowModal.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ModalType, useModalContext } from 'src/hooks/useModal';
-import { ActionModal } from '../primitives/ActionModal';
+import { BasicModal } from '../primitives/BasicModal';
 import { BorrowModalContent } from './BorrowModalContent';
 
 export const BorrowModal = () => {
   const { type, close, args } = useModalContext();
   return (
-    <ActionModal open={type === ModalType.Borrow} setOpen={close}>
+    <BasicModal open={type === ModalType.Borrow} setOpen={close}>
       {args?.underlyingAsset && (
         <BorrowModalContent underlyingAsset={args.underlyingAsset} handleClose={close} />
       )}
-    </ActionModal>
+    </BasicModal>
   );
 };
