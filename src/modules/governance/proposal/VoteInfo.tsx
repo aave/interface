4329b9f53a795478be0213addaaf5951e04526bb
Lukas <lukasstrassel@googlemail.com>
2022-02-15 16:27:25 +0100

    feat: fetch voting power for proposal

--- src/modules/governance/proposal/VoteInfo.tsx
+++ src/modules/governance/proposal/VoteInfo.tsx
@@ -6,13 +6,15 @@ import { useGovernanceDataProvider } from 'src/hooks/governance-data-provider/Go
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
 import { CustomProposalType } from 'src/static-build/proposal';
 
-export function VoteInfo({ id, state }: CustomProposalType) {
-  const [votingPower, setVotingPower] = useState<string>();
+export function VoteInfo({ id, state, strategy, startBlock }: CustomProposalType) {
+  const [votedPower, setVotedPower] = useState<string>();
   const [support, setSupport] = useState<boolean>();
   const [didVote, setDidVote] = useState<boolean>();
+  const [power, setPower] = useState<string>();
 
   const { governanceService } = useGovernanceDataProvider();
   const { currentAccount } = useWeb3Context();
+  const voteOngoing = state === ProposalState.Active || true; // TODO: remove true condition
 
   const fetchCurrentVote = async () => {
     try {
@@ -20,9 +22,10 @@ export function VoteInfo({ id, state }: CustomProposalType) {
         user: currentAccount,
         proposalId: id,
       });
+
       if (votingPower && votingPower.toString() !== '0') {
         setSupport(support);
-        setVotingPower(votingPower.toString());
+        setVotedPower(votingPower.toString());
         setDidVote(true);
       } else {
         setDidVote(false);
@@ -32,11 +35,24 @@ export function VoteInfo({ id, state }: CustomProposalType) {
     }
   };
 
+  const fetchVotingPower = async () => {
+    try {
+      const power = await governanceService.getVotingPowerAt({
+        user: currentAccount,
+        block: startBlock,
+        strategy,
+      });
+      setPower(power);
+    } catch (e) {
+      console.log('error fetching voting power for proposal', id);
+    }
+  };
+
   useEffect(() => {
     fetchCurrentVote();
-  }, []);
+    if (voteOngoing) fetchVotingPower();
+  }, [voteOngoing]);
 
-  const canVote = state === ProposalState.Active || true; // TODO: remove true condition
   return (
     <>
       <Typography variant="h3">
@@ -47,9 +63,11 @@ export function VoteInfo({ id, state }: CustomProposalType) {
         <br />
         InSupport: {support ? 'yes' : 'no'}
         <br />
-        With a power of: {votingPower}
+        Voted with a power of: {votedPower}
         <br />
-        {canVote && (
+        Power at the time of creation: {power}
+        <br />
+        {voteOngoing && (
           <Button variant="contained" onClick={() => console.log('TODO: cast vote')}>
             Vote
           </Button>
