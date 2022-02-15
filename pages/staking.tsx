8cd759c1cd4052d58cb5af0d88483a2fbdd16000
kartojal <canillas.mail@gmail.com>
2022-02-15 15:07:09 +0100

    feat: add tvl usd and emission data to staking

--- pages/staking.tsx
+++ pages/staking.tsx
@@ -1,9 +1,13 @@
+import { USD_DECIMALS } from '@aave/math-utils';
 import { Grid, Typography } from '@mui/material';
+import { BigNumber } from 'ethers/lib/ethers';
+import { formatEther } from 'ethers/lib/utils';
 import { ContentContainer } from 'src/components/ContentContainer';
 import { StakeModal } from 'src/components/Stake/StakeModal';
 import { StakeCooldownModal } from 'src/components/StakeCooldown/StakeCooldownModal';
 import { StakeRewardClaimModal } from 'src/components/StakeRewardClaim/StakeRewardClaimModal';
 import { UnStakeModal } from 'src/components/UnStake/UnStakeModal';
+import { useAppDataContext } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { StakeDataProvider, useStakeData } from 'src/hooks/stake-data-provider/StakeDataProvider';
 import { useModalContext } from 'src/hooks/useModal';
 import { MainLayout } from 'src/layouts/MainLayout';
@@ -13,9 +17,29 @@ import { StakingPanel } from 'src/modules/staking/StakingPanel';
 export default function Staking() {
   const data = useStakeData();
   const { openStake, openStakeCooldown, openUnstake, openStakeRewardsClaim } = useModalContext();
+
+  // Total funds at Safety Module in USD (stkaave tvl + stkbpt tvl)
+  const tvl = formatEther(
+    BigNumber.from(data.stakeGeneralResult?.stakeGeneralUIData.aave.stakeTokenTotalSupply || '0')
+      .mul(data.stakeGeneralResult?.stakeGeneralUIData.aave.stakeTokenPriceEth || '0')
+      .add(
+        BigNumber.from(
+          data.stakeGeneralResult?.stakeGeneralUIData.bpt.stakeTokenTotalSupply || '0'
+        ).mul(data.stakeGeneralResult?.stakeGeneralUIData.bpt.stakeTokenPriceEth || '0')
+      )
+      .div(data.stakeGeneralResult?.stakeGeneralUIData.usdPriceEth || 1)
+  );
+
+  // Total AAVE Emissions (stkaave dps + stkbpt dps)
+  const stkEmission = formatEther(
+    BigNumber.from(data.stakeGeneralResult?.stakeGeneralUIData.aave.distributionPerSecond || '0')
+      .add(data.stakeGeneralResult?.stakeGeneralUIData.bpt.distributionPerSecond || '0')
+      .mul('86400')
+  );
+
   return (
     <>
-      <StakingHeader />
+      <StakingHeader tvl={tvl} stkEmission={stkEmission} />
       <ContentContainer>
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6}>
