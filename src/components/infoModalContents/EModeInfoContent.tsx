ffb2d9e246f708a0c6ed8c8b1efd8c6e0f16dcd4
Mark Hinschberger <foodaka@users.noreply.github.com>
2022-02-15 18:22:47 +0100

    Update src/components/infoModalContents/EModeInfoContent.tsx
    Update emode text
    
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/components/infoModalContents/EModeInfoContent.tsx
+++ src/components/infoModalContents/EModeInfoContent.tsx
@@ -9,8 +9,7 @@ export const EModeInfoContent = () => {
     <InfoContentWrapper caption={<Trans>Efficiency Mode(E-Mode)</Trans>}>
       <Typography>
         <Trans>
-          E-Mode increases your borrowing power for a selected category of assets up to 99%.Enabling
-          E-Mode only allows you to borrow assets belonging to the selected category Stablecoins.
+          E-Mode increases your borrowing power for a selected category of assets up to 99%. Enabling E-mode will restrict the assets you can borrow against your collateral to the ones specified in the selected E-mode.
           Please visit out
           <Link href="https://docs.aave.com/faq/" variant="main14" sx={{ ml: 1 }}>
             <Trans>FAQ guide </Trans>
