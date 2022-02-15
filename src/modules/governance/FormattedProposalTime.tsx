acfb571c41b33b366cb773004befe63149782043
Lukas <lukasstrassel@googlemail.com>
2022-02-15 13:13:40 +0100

    fix: adjust theme a bit (#138)

--- src/modules/governance/FormattedProposalTime.tsx
+++ src/modules/governance/FormattedProposalTime.tsx
@@ -1,4 +1,5 @@
 import { ProposalState } from '@aave/contract-helpers';
+import { Trans } from '@lingui/macro';
 import { Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -11,6 +12,12 @@ interface FormattedProposalTimeProps {
   expirationTimestamp: number;
 }
 
+function RelativeWord({ state }: { state: ProposalState }) {
+  if ([ProposalState.Active, ProposalState.Queued, ProposalState.Pending].includes(state))
+    return <Trans>ends</Trans>;
+  return <Trans>on</Trans>;
+}
+
 export function FormattedProposalTime({
   state,
   executionTime,
@@ -18,20 +25,24 @@ export function FormattedProposalTime({
 }: FormattedProposalTimeProps) {
   return (
     <Typography component="span" variant="caption">
-      {state}&nbsp;
-      {expirationTimestamp &&
-        [ProposalState.Active, ProposalState.Queued, ProposalState.Pending].includes(state) &&
-        `ends ${dayjs.unix(expirationTimestamp).fromNow()}`}
-      {expirationTimestamp &&
-        [
-          ProposalState.Canceled,
-          ProposalState.Expired,
-          ProposalState.Failed,
-          ProposalState.Succeeded,
-        ].includes(state) &&
-        `on ${dayjs(expirationTimestamp * 1000).format('MMM DD, YYYY')}`}
-      {state === ProposalState.Executed &&
-        `on ${dayjs(executionTime * 1000).format('MMM DD, YYYY')}`}
+      <Typography
+        variant="caption"
+        color="text.secondary"
+        sx={{ display: { xs: 'none', md: 'inline' } }}
+      >
+        {state}&nbsp;
+        <RelativeWord state={state} />
+        &nbsp;
+      </Typography>
+      {[ProposalState.Active, ProposalState.Queued, ProposalState.Pending].includes(state) &&
+        dayjs.unix(expirationTimestamp).fromNow()}
+      {[
+        ProposalState.Canceled,
+        ProposalState.Expired,
+        ProposalState.Failed,
+        ProposalState.Succeeded,
+      ].includes(state) && dayjs.unix(expirationTimestamp).format('MMM DD, YYYY')}
+      {state === ProposalState.Executed && dayjs.unix(executionTime).format('MMM DD, YYYY')}
     </Typography>
   );
 }
