88d2c3e435847d73a23d0cf8574bea724a82d75c
Mark Hinschberger <foodaka@users.noreply.github.com>
2022-02-15 18:22:13 +0100

    Update src/components/infoModalContents/BorrowPowerInfoContent.tsx
    change deposited to supplied
    
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/components/infoModalContents/BorrowPowerInfoContent.tsx
+++ src/components/infoModalContents/BorrowPowerInfoContent.tsx
@@ -9,7 +9,7 @@ export const BorrowPowerInfoContent = () => {
       <Typography>
         <Trans>
           The % of your total bowering power used. This is based on the amount of your collateral
-          deposited and the total amount that you can borrow.
+          supplied and the total amount that you can borrow.
         </Trans>
       </Typography>
     </InfoContentWrapper>
