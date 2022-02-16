006169e8716f72104ad6cfb8973fd8629df275ed
Mark Hinschberger <markhinschberger@gmail.com>
2022-02-16 18:19:24 +0000

    fix trans component

--- src/components/infoModalContents/ApprovalInfoContent.tsx
+++ src/components/infoModalContents/ApprovalInfoContent.tsx
@@ -8,13 +8,11 @@ export const ApprovalInfoContent = () => {
   return (
     <InfoContentWrapper caption={<Trans>Approval</Trans>}>
       <Typography>
-        <Trans>
-          Before initiating a transaction within Aave you must sign and approve it. It doesnt cost
-          anything to sign. You can learn more in our{' '}
-          <Link fontWeight={500} href={'https://docs.aave.com/faq/'}>
-            FAQs.
-          </Link>{' '}
-        </Trans>
+        <Trans
+          id="Before initiating a transaction within Aave you must sign and approve it. It doesnt cost
+          anything to sign. You can learn more in our <0>FAQ</0> "
+          components={[<Link key={0} fontWeight={500} href={'https://docs.aave.com/faq/'} />]}
+        />
       </Typography>
     </InfoContentWrapper>
   );
