489e4b955d93de85e60990fd25932908f8722f52
Mark Hinschberger <markhinschberger@gmail.com>
2022-02-16 14:30:40 +0000

    approval text

--- src/components/infoModalContents/ApprovalInfoContent.tsx
+++ src/components/infoModalContents/ApprovalInfoContent.tsx
@@ -1,13 +1,20 @@
 import { Trans } from '@lingui/macro';
 import Typography from '@mui/material/Typography';
 
+import { Link } from '../primitives/Link';
 import { InfoContentWrapper } from './InfoContentWrapper';
 
 export const ApprovalInfoContent = () => {
   return (
     <InfoContentWrapper caption={<Trans>Approval</Trans>}>
       <Typography>
-        <Trans>Define Approval text</Trans>
+        <Trans>
+          Before initiating a transaction within Aave you must sign and approve it. It doesnt cost
+          anything to sign. You can learn more in our{' '}
+          <Link fontWeight={500} href={'https://docs.aave.com/faq/'}>
+            FAQs.
+          </Link>{' '}
+        </Trans>
       </Typography>
     </InfoContentWrapper>
   );
