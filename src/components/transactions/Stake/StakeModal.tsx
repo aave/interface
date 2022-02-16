8fac8d0825623d45d285e0c1d68ab70aac939316
Pol Sendra <sendra@users.noreply.github.com>
2022-02-16 18:10:07 +0100

    feat: governance flow (#156)
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/components/transactions/Stake/StakeModal.tsx
+++ src/components/transactions/Stake/StakeModal.tsx
@@ -7,7 +7,7 @@ export const StakeModal = () => {
   const { type, close, args } = useModalContext();
   return (
     <BasicModal open={type === ModalType.Stake} setOpen={close}>
-      {args?.icon && (
+      {args?.icon && args?.stakeAssetName && (
         <StakeModalContent
           icon={args.icon}
           stakeAssetName={args.stakeAssetName}
