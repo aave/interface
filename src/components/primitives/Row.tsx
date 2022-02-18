043af52853828ec8d1e13f2859f8027a429a5d9c
Argeare5 <argeares97@gmail.com>
2022-02-18 12:46:10 +0100

    fix: e-mode button styles (#204)

--- src/components/primitives/Row.tsx
+++ src/components/primitives/Row.tsx
@@ -16,8 +16,8 @@ export const Row = ({
 }: RowProps) => {
   return (
     <Box
-      {...rest}
       sx={{ display: 'flex', alignItems: align, justifyContent: 'space-between', ...rest.sx }}
+      {...rest}
     >
       {caption && (
         <Typography component="div" variant={captionVariant} sx={{ mr: 2 }}>
