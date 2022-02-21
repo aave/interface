2ae6bb140607c412a287461696a0884cd8a06d84
Lukas <lukasstrassel@googlemail.com>
2022-02-21 14:15:52 +0100

    fix: maintain viewBox

--- svgo.config.js
+++ svgo.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   multipass: true,
   plugins: [
-    'preset-default',
-    // viewBox is required to resize SVGs with CSS.
-    // @see https://github.com/svg/svgo/issues/1128
     {
-      name: 'removeViewBox',
-      active: false,
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // viewBox is required to resize SVGs with CSS.
+          // @see https://github.com/svg/svgo/issues/1128
+          removeViewBox: false,
+        },
+      },
     },
   ],
 };
