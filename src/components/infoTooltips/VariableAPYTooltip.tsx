2a596ed16375cd077811e0b5daa15cc782a123ad
Pol Sendra <sendra@users.noreply.github.com>
2022-02-23 15:57:40 +0100

    Update src/components/infoTooltips/VariableAPYTooltip.tsx
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/components/infoTooltips/VariableAPYTooltip.tsx
+++ src/components/infoTooltips/VariableAPYTooltip.tsx
@@ -6,7 +6,7 @@ export const VariableAPYTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        Variable interest rate will <b>fluctuate</b> based on the market. Recommended for short-term
+        Variable interest rate will <b>fluctuate</b> based on the market conditions. Recommended for short-term
         loans.
       </Trans>
     </TextWithTooltip>
