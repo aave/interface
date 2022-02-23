2b1fd2a3edbf367864693916d9ec01c9df4b8817
sendra <polsendra@gmail.com>
2022-02-23 15:38:49 +0100

    fix: added variable and stable tooltips

--- src/components/infoTooltips/APYTypeTooltip.tsx
+++ src/components/infoTooltips/APYTypeTooltip.tsx
@@ -6,7 +6,7 @@ export const APYTypeTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        Allows you to switch between <b>variable</b> and <b>stabel</b> interest rates, where
+        Allows you to switch between <b>variable</b> and <b>stable</b> interest rates, where
         variable rate can increase and decrease depending on the amount of liquidity in the reserve,
         and stable rate will stay the same for the duration of your loan.
       </Trans>
