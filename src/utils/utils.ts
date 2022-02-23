c1bcdbf0380fce36c74580edc3828cb47cbb55f1
Lukas <lukasstrassel@googlemail.com>
2022-02-23 18:31:54 +0100

    feat: add cancelability (#263)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,26 @@ export function hexToAscii(_hex: string): string {
   }
   return str;
 }
+
+export interface CancelablePromise<T = unknown> {
+  promise: Promise<T>;
+  cancel: () => void;
+}
+
+export const makeCancelable = <T>(promise: Promise<T>) => {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise<T>((resolve, reject) => {
+    promise.then(
+      (val) => (hasCanceled_ ? reject({ isCanceled: true }) : resolve(val)),
+      (error) => (hasCanceled_ ? reject({ isCanceled: true }) : reject(error))
+    );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    },
+  };
+};
