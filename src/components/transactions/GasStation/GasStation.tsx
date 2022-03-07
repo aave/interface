33f7007006e7c41d61a887fe75d64735a0adf249
Pol Sendra <sendra@users.noreply.github.com>
2022-03-07 19:15:38 +0100

    fix: 340 fix gas widget (#365)
    - closes #340

--- src/components/transactions/GasStation/GasStation.tsx
+++ src/components/transactions/GasStation/GasStation.tsx
@@ -12,12 +12,14 @@ import {
   Typography,
   useTheme,
   ClickAwayListener,
+  CircularProgress,
 } from '@mui/material';
 import sx from '@mui/system/sx';
 import { BigNumber } from 'ethers/lib/ethers';
 import { formatUnits, parseUnits } from 'ethers/lib/utils';
 import React, { useState } from 'react';
 import { useGasStation } from 'src/hooks/useGasStation';
+import { useModalContext } from 'src/hooks/useModal';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 
 import { useAppDataContext } from '../../../hooks/app-data-provider/useAppDataProvider';
@@ -73,6 +75,7 @@ export const GasStation: React.FC<GasStationProps> = ({ gasLimit }) => {
   const {
     currentNetworkConfig: { wrappedBaseAssetSymbol },
   } = useProtocolDataContext();
+  const { loadingTxns } = useModalContext();
 
   const wrappedAsset = reserves.find(
     (token) => token.symbol.toLowerCase() === wrappedBaseAssetSymbol?.toLowerCase()
@@ -109,7 +112,9 @@ export const GasStation: React.FC<GasStationProps> = ({ gasLimit }) => {
     <Box sx={{ display: 'flex', alignItems: 'center', mt: 6 }}>
       <LocalGasStationIcon color="primary" sx={{ fontSize: '16px', mr: 1.5 }} />
 
-      {totalGasCostsUsd ? (
+      {loadingTxns ? (
+        <CircularProgress color="inherit" size="16px" sx={{ mr: 2 }} />
+      ) : totalGasCostsUsd ? (
         <FormattedNumber value={totalGasCostsUsd} symbol="USD" color="text.secondary" />
       ) : (
         '-'
@@ -137,7 +142,7 @@ export const GasStation: React.FC<GasStationProps> = ({ gasLimit }) => {
             onClose={() => setOpen(false)}
             disableFocusListener
             disableHoverListener
-            placement="top"
+            placement="top-start"
             PopperComponent={PopperComponent}
             title={
               <Box sx={{ py: 4, px: 6, width: '280px' }}>
@@ -259,7 +264,8 @@ export const GasStation: React.FC<GasStationProps> = ({ gasLimit }) => {
               onClick={toggleDropdown}
               sx={{
                 cursor: 'pointer',
-                display: error ? 'none' : 'inline-block',
+                display: error ? 'none' : 'flex',
+                alignSelf: 'center',
                 fontSize: '16px',
                 ml: '2px',
               }}
