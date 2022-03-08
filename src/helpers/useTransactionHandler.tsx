3258a90889b899c143decf41d14e4934f3c9fda0
Pol Sendra <sendra@users.noreply.github.com>
2022-03-08 13:59:48 +0100

    Update src/helpers/useTransactionHandler.tsx

--- src/helpers/useTransactionHandler.tsx
+++ src/helpers/useTransactionHandler.tsx
@@ -185,7 +185,6 @@ export const useTransactionHandler = ({
 
   const action = async () => {
     if (approvalTx && usePermit && handleGetPermitTxns) {
-      console.log('0000');
       if (!signature || !signatureDeadline) throw new Error('signature needed');
       try {
         setMainTxState({ ...mainTxState, loading: true });
