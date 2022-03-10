395d8f6ecf8a8447b2300a5600fd491e36557873
Lukas <lukasstrassel@googlemail.com>
2022-03-10 11:38:41 +0100

    fix: start refactoring modals (#432)

--- src/components/transactions/Repay/RepayModal.tsx
+++ src/components/transactions/Repay/RepayModal.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { ModalType, useModalContext } from 'src/hooks/useModal';
-
+import { InterestRate } from '@aave/contract-helpers';
+import { Trans } from '@lingui/macro';
+import React, { useState } from 'react';
+import { ModalContextType, ModalType, useModalContext } from 'src/hooks/useModal';
 import { BasicModal } from '../../primitives/BasicModal';
+import { ModalWrapper } from '../FlowCommons/ModalWrapper';
+import { CollateralRepayModalContent } from './CollateralRepayModalContent';
 import { RepayModalContent } from './RepayModalContent';
+import { RepayType, RepayTypeSelector } from './RepayTypeSelector';
 
 export const RepayModal = () => {
-  const { type, close, args } = useModalContext();
-
+  const { type, close, args } = useModalContext() as ModalContextType<{
+    underlyingAsset: string;
+    currentRateMode: InterestRate;
+  }>;
+  const [repayType, setRepayType] = useState(RepayType.BALANCE);
   return (
     <BasicModal open={type === ModalType.Repay} setOpen={close}>
-      {args?.underlyingAsset && <RepayModalContent underlyingAsset={args.underlyingAsset} />}
+      <ModalWrapper title={<Trans>Repay</Trans>} underlyingAsset={args.underlyingAsset}>
+        {(params) => {
+          return (
+            <>
+              <RepayTypeSelector repayType={repayType} setRepayType={setRepayType} />
+              {repayType === RepayType.BALANCE && (
+                <RepayModalContent {...params} debtType={args.currentRateMode} />
+              )}
+              {repayType === RepayType.COLLATERAL && (
+                <CollateralRepayModalContent {...params} debtType={args.currentRateMode} />
+              )}
+            </>
+          );
+        }}
+      </ModalWrapper>
     </BasicModal>
   );
 };
