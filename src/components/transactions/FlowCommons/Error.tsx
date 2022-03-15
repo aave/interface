c79a885c6d70abd7975822d1f577ca329629a6d8
Mark Hinschberger <foodaka@users.noreply.github.com>
2022-03-15 13:20:14 +0100

    fix git links to repo (#485)

--- src/components/transactions/FlowCommons/Error.tsx
+++ src/components/transactions/FlowCommons/Error.tsx
@@ -48,7 +48,7 @@ export const TxErrorView = ({ txError }: { txError: TxErrorType }) => {
         <Typography>
           <Trans>
             You can report incident to our <Link href="https://discord.gg/7kHKnkDEUf">Discord</Link>{' '}
-            or <Link href="https://github.com/aave/aave-ui">Github</Link>.
+            or <Link href="https://github.com/aave/interface">Github</Link>.
           </Trans>
         </Typography>
 
