7e8fa62f71b5ddbcf9e30bcc7fac8d90f46ca57e
Lukas <lukasstrassel@googlemail.com>
2022-03-16 17:15:32 +0100

    fix: fix governance and staking (#502)

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const pageExtensions = ['page.tsx'];
-if (process.env.ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx');
-if (process.env.ENABLE_STAKING === 'true') pageExtensions.push('staking.tsx');
+if (process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx');
+if (process.env.NEXT_PUBLIC_ENABLE_STAKING === 'true') pageExtensions.push('staking.tsx');
 
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
