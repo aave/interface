223b6a4b419fa87ce5d3d77b7e490ba82638418c
defispartan <andrew@aave.com>
2022-03-25 04:58:34 -0500

    fix: wording

--- src/components/infoTooltips/NetAPYTooltip.tsx
+++ src/components/infoTooltips/NetAPYTooltip.tsx
@@ -6,9 +6,8 @@ export const NetAPYTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        The net APY is the combined effect of all supply and borrow positions on net worth,
-        including incentives. It is possible to have a negative net APY if debt APY is higher than
-        supply APY.
+        Net APY is the combined effect of all supply and borrow positions on net worth, including
+        incentives. It is possible to have a negative net APY if debt APY is higher than supply APY.
       </Trans>
     </TextWithTooltip>
   );
