17bd6675e747937296fc9c5502b2272964b00c0e
James Carter <james@loveelectronics.co.uk>
2022-04-01 11:30:41 +0100

    fix: corrected borrowing spelling in tooltip (#623)

--- src/components/infoTooltips/BorrowPowerTooltip.tsx
+++ src/components/infoTooltips/BorrowPowerTooltip.tsx
@@ -6,7 +6,7 @@ export const BorrowPowerTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        The % of your total bowering power used. This is based on the amount of your collateral
+        The % of your total borrowing power used. This is based on the amount of your collateral
         supplied and the total amount that you can borrow.
       </Trans>
     </TextWithTooltip>
