fe41aea95b6a826c2df07e39d1fecd08860d7899
sendra <polsendra@gmail.com>
2022-04-06 17:51:37 +0200

    fix: substracting fromhf of toHf

--- src/utils/hfUtils.ts
+++ src/utils/hfUtils.ts
@@ -105,9 +105,9 @@ export function calculateHFAfterSwap({
 
   return {
     hfEffectOfFromAmount,
-    hfAfterSwap: valueToBigNumber(hfEffectOfToAmount),
-    // user.healthFactor === '-1'
-    //   ? valueToBigNumber('-1')
-    //   : valueToBigNumber(user.healthFactor).minus(hfEffectOfFromAmount).plus(hfEffectOfToAmount),
+    hfAfterSwap:
+      user.healthFactor === '-1'
+        ? valueToBigNumber('-1')
+        : valueToBigNumber(hfEffectOfToAmount).minus(hfEffectOfFromAmount),
   };
 }
