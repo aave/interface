6b5a6125b47781f7489086d585168fdab18c9304
sendra <polsendra@gmail.com>
2022-04-07 18:51:22 +0200

    fix: a bit of cleaning

--- src/components/transactions/Repay/CollateralRepayActions.tsx
+++ src/components/transactions/Repay/CollateralRepayActions.tsx
@@ -1,17 +1,13 @@
 import { InterestRate, Pool } from '@aave/contract-helpers';
 import { Trans } from '@lingui/macro';
 import { BoxProps } from '@mui/material';
-import { utils } from 'ethers';
 import { useTransactionHandler } from 'src/helpers/useTransactionHandler';
 import { ComputedReserveData } from 'src/hooks/app-data-provider/useAppDataProvider';
-import { useGasStation } from 'src/hooks/useGasStation';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { useTxBuilderContext } from 'src/hooks/useTxBuilder';
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
-import { permitByChainAndToken } from 'src/ui-config/permitConfig';
 import { optimizedPath } from 'src/utils/utils';
 
-import { GasOption } from '../GasStation/GasStationProvider';
 import { TxActionsWrapper } from '../TxActionsWrapper';
 import { OptimalRate } from 'paraswap-core';
 import { getSwapCallData } from 'src/hooks/useSwap';
@@ -44,9 +40,8 @@ export const CollateralRepayActions = ({
   ...props
 }: RepayActionProps) => {
   const { lendingPool } = useTxBuilderContext();
-  const { currentChainId: chainId, currentMarketData } = useProtocolDataContext();
+  const { currentChainId: chainId } = useProtocolDataContext();
   const { currentAccount } = useWeb3Context();
-  const { state, gasPriceData } = useGasStation();
 
   const { approval, action, requiresApproval, loadingTxns, approvalTxState, mainTxState } =
     useTransactionHandler({
@@ -75,10 +70,6 @@ export const CollateralRepayActions = ({
           augustus,
         });
       },
-      customGasPrice:
-        state.gasOption === GasOption.Custom
-          ? state.customGas
-          : gasPriceData.data?.[state.gasOption].legacyGasPrice,
       skip: !amountToRepay || parseFloat(amountToRepay) === 0 || blocked,
       deps: [amountToRepay],
     });
