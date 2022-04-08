86e50ea4f6ea8fa654720274276b97fab91aeac3
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-04-08 05:15:26 +0100

    swap auto test

---
+++ cypress/integration/1-v3-markets/3-polygon-v3-market/swap.polygon-v3.spec.ts
@@ -0,0 +1,52 @@
+import { skipState } from '../../../support/steps/common';
+import { configEnvWithTenderlyPolygonFork } from '../../../support/steps/configuration.steps';
+import { supply, swap } from '../../../support/steps/main.steps';
+import assets from '../../../fixtures/assets.json';
+import { dashboardAssetValuesVerification } from '../../../support/steps/verification.steps';
+import constants from '../../../fixtures/constans.json';
+
+const testData = {
+  deposit: {
+    asset: assets.polygonV3Market.MATIC,
+    amount: 100,
+    hasApproval: true,
+  },
+  swap: [
+    {
+      fromAsset: assets.polygonV3Market.MATIC,
+      toAsset: assets.polygonV3Market.USDC,
+      isCollateralFromAsset: true,
+      amount: 10,
+      hasApproval: false,
+    },
+  ],
+  verifications: {
+    finalDashboard: [
+      {
+        type: constants.dashboardTypes.deposit,
+        assetName: assets.polygonV3Market.USDC.shortName,
+        wrapped: assets.polygonV3Market.USDC.wrapped,
+        collateralType: constants.collateralType.isCollateral,
+        isCollateral: true,
+      },
+      {
+        type: constants.dashboardTypes.deposit,
+        assetName: assets.polygonV3Market.MATIC.shortName,
+        amount: 90,
+        collateralType: constants.collateralType.isCollateral,
+        isCollateral: true,
+      },
+    ],
+  },
+};
+
+describe('SWAP, POLYGON V3 MARKET, INTEGRATION SPEC', () => {
+  const skipTestState = skipState(false);
+  configEnvWithTenderlyPolygonFork({ market: 'fork_proto_polygon_v3', v3: true });
+
+  supply(testData.deposit, skipTestState, true);
+  testData.swap.forEach((swapCase) => {
+    swap(swapCase, skipTestState, false);
+  });
+  dashboardAssetValuesVerification(testData.verifications.finalDashboard, skipTestState);
+});
