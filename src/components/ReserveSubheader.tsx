bf6a5062bcedfc1ca0c0f8b500ab4c0c440f78e3
defispartan <andrew@aave.com>
2022-04-18 22:33:15 -0500

    feat: add subheader component for usd values

---
+++ src/components/ReserveSubheader.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import { FormattedNumber } from './primitives/FormattedNumber';
+
+type ReserveSubheaderProps = {
+  value: string;
+  rightAlign?: boolean;
+};
+
+export function ReserveSubheader({ value, rightAlign }: ReserveSubheaderProps) {
+  return (
+    <Box
+      sx={{
+        p: rightAlign ? { xs: '0', xsm: '2px 0' } : { xs: '0 4px', xsm: '2px 4px' },
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <FormattedNumber
+        compact
+        value={value}
+        variant="secondary12"
+        color="text.secondary"
+        symbolsVariant="secondary12"
+        symbolsColor="text.secondary"
+        symbol="USD"
+      />
+    </Box>
+  );
+}
