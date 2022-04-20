4774b0cfd7a5e2475c0ea46e2facd841ea8c38ff
Lukas <lukasstrassel@googlemail.com>
2022-04-20 18:40:14 +0200

    [WIP] feat: collateral repay (#448)
    * feat: collateral repay
    
    * feat: add address
    
    * run i18n
    
    * fix: [WIP]: updated lib with corret usage of augustus. wip of tx object call
    
    * fix: a bit of cleaning
    
    * fix: enabled repay with collateral for avalanche, polygon and fantom v3
    
    * fix: added comment remainder of hf
    
    * fix: updated repay with c action with correct params
    
    * fix: WIP: updated modal content with a few calculations for repay with c
    
    * fix: adding more calcs
    
    * fix: [WIP]: health factor calculations
    
    * fix: WIP: missing repay max
    
    * fix: pulled main
    
    * fix: WIP: max repay with collateral calcs
    
    * fix: updated translations
    
    * fix: correct amount calculations
    
    * fix: calcs looks like are correct. augustus call data failing
    
    * fix: paraswap correctly configured. Having an error on transfering balance
    
    * fix: show correct minimum received symbol. some logs
    
    * fix: still not working but using now priceRoute amounts
    
    * fix: added lib with logs
    
    * fix: new logs and few fixes
    
    * fix: fixed input amount
    
    * fix: updated naming
    
    * fix: more name refactoring
    
    * fix: hf calcs look much better
    
    * fix: fixed hf calcs scientific notation which made calcs not exact
    
    * fix: using minimumReceived for tx building
    
    * fix: repay working. Pending exaustive testing
    
    * fix: add surplus on route when max is selected
    
    * fix: interface building
    
    * fix: repay with some and with all working
    
    * fix: fixed forks for repay
    
    * fix: cleaned code
    
    * fix: updated with clean utilities version
    
    * fix: fixed fork for repay
    
    * fix: fixed when repay all and no user reserve remains
    
    * fix: not updating background until tx is finished
    
    * fix: fixed build
    
    * fix: add custom title to input
    
    * fix: updated translations
    
    * fix tests
    
    * turn on arbitrum tests
    
    * fix: some logs on cache provider
    
    * fix: add query refetch
    
    * fix: removed logs. Updated repay with case for when user reserves dont exist
    
    * skip swap eth
    
    * skip failed specs
    
    Co-authored-by: defispartan <andrew@aave.com>
    Co-authored-by: sendra <polsendra@gmail.com>
    Co-authored-by: Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>

--- src/utils/hfUtils.ts
+++ src/utils/hfUtils.ts
@@ -8,6 +8,7 @@ import {
   ComputedReserveData,
   ExtendedFormattedUser,
 } from 'src/hooks/app-data-provider/useAppDataProvider';
+import BigNumber from 'bignumber.js';
 
 interface CalculateHFAfterSwapProps {
   fromAmount: BigNumberValue;
@@ -16,6 +17,16 @@ interface CalculateHFAfterSwapProps {
   toAmountAfterSlippage: BigNumberValue;
   toAssetData: ComputedReserveData;
   user: ExtendedFormattedUser;
+}
+
+interface CalculateHFAfterSwapRepayProps {
+  amountToReceiveAfterSwap: BigNumberValue;
+  amountToSwap: BigNumberValue;
+  fromAssetData: ComputedReserveData;
+  toAssetData: ComputedReserveData;
+  user: ExtendedFormattedUser;
+  repayWithUserReserve?: ComputedUserReserve;
+  debt: string;
 }
 
 export function calculateHFAfterSwap({
@@ -71,3 +82,72 @@ export function calculateHFAfterSwap({
         : valueToBigNumber(user.healthFactor).plus(hfEffectOfToAmount).minus(hfEffectOfFromAmount),
   };
 }
+
+export const calculateHFAfterRepay = ({
+  user,
+  amountToReceiveAfterSwap,
+  amountToSwap,
+  fromAssetData,
+  toAssetData,
+  repayWithUserReserve,
+  debt,
+}: CalculateHFAfterSwapRepayProps) => {
+  // it takes into account if in emode as threshold is different
+  const reserveLiquidationThreshold =
+    user.isInEmode && user.userEmodeCategoryId === fromAssetData.eModeCategoryId
+      ? fromAssetData.formattedEModeLiquidationThreshold
+      : fromAssetData.formattedReserveLiquidationThreshold;
+
+  // hf indicating how the state would be if we withdrew this amount.
+  // this is needed because on contracts hf can't be < 1 so in the case
+  // that fromHF < 1 we need to do a flashloan to not go below
+  let hfInitialEffectOfFromAmount = '0';
+
+  if (
+    repayWithUserReserve?.usageAsCollateralEnabledOnUser &&
+    fromAssetData.usageAsCollateralEnabled
+  ) {
+    hfInitialEffectOfFromAmount = calculateHealthFactorFromBalancesBigUnits({
+      collateralBalanceMarketReferenceCurrency: valueToBigNumber(amountToSwap).multipliedBy(
+        fromAssetData.formattedPriceInMarketReferenceCurrency
+      ),
+      borrowBalanceMarketReferenceCurrency: user.totalBorrowsMarketReferenceCurrency,
+      currentLiquidationThreshold: reserveLiquidationThreshold,
+    }).toString();
+  }
+
+  const fromAmountInMarketReferenceCurrency = valueToBigNumber(
+    BigNumber.min(amountToReceiveAfterSwap, debt)
+  )
+    .multipliedBy(toAssetData.priceInUSD)
+    .toString(10);
+  const debtLeftInMarketReference = valueToBigNumber(user.totalBorrowsUSD).minus(
+    fromAmountInMarketReferenceCurrency
+  );
+
+  const hfAfterRepayBeforeWithdraw = calculateHealthFactorFromBalancesBigUnits({
+    collateralBalanceMarketReferenceCurrency: user.totalCollateralUSD,
+    borrowBalanceMarketReferenceCurrency: debtLeftInMarketReference.toString(10),
+    currentLiquidationThreshold: user.currentLiquidationThreshold,
+  });
+
+  const hfRealEffectOfFromAmount =
+    fromAssetData.usageAsCollateralEnabled && repayWithUserReserve?.usageAsCollateralEnabledOnUser
+      ? calculateHealthFactorFromBalancesBigUnits({
+          collateralBalanceMarketReferenceCurrency: valueToBigNumber(amountToSwap).multipliedBy(
+            fromAssetData.priceInUSD
+          ),
+          borrowBalanceMarketReferenceCurrency: debtLeftInMarketReference.toString(10),
+          currentLiquidationThreshold: fromAssetData.formattedReserveLiquidationThreshold,
+        }).toString()
+      : '0';
+
+  const hfAfterSwap = hfAfterRepayBeforeWithdraw.eq(-1)
+    ? hfAfterRepayBeforeWithdraw
+    : hfAfterRepayBeforeWithdraw.minus(hfRealEffectOfFromAmount);
+
+  return {
+    hfEffectOfFromAmount: valueToBigNumber(hfInitialEffectOfFromAmount),
+    hfAfterSwap,
+  };
+};
