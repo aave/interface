d85bc2d23139bf0dde60e35c13b8aefdcf182cb7
defispartan <andrew@aave.com>
2022-04-20 23:39:36 -0500

    feat: finish box styling

--- src/components/ReserveOverviewBox.tsx
+++ src/components/ReserveOverviewBox.tsx
@@ -1,14 +1,39 @@
 import React, { ReactNode } from 'react';
-import Box from '@mui/material/Box';
+import { Box, Typography } from '@mui/material';
+import { Trans } from '@lingui/macro';
 
 type ReserveOverviewBoxProps = {
   children: ReactNode;
+  title: string;
 };
 
-export function ReserveOverviewBox({ children }: ReserveOverviewBoxProps) {
+export function ReserveOverviewBox({ title, children }: ReserveOverviewBoxProps) {
   return (
-    <Box border="1px solid" borderColor="divider" borderRadius="4px" paddingY="8px" width="33%">
-      <Box sx={{ display: 'flex', flexDirection: 'column', ml: 2 }}>{children}</Box>
+    <Box
+      sx={(theme) => ({
+        borderRadius: '6px',
+        border: `1px solid ${theme.palette.divider}`,
+        flex: '0 32%',
+        marginBottom: '2%',
+        height: { xs: '5em', xxs: '7em' },
+        maxWidth: '32%',
+      })}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          ml: 2,
+          height: '100%',
+          justifyContent: 'space-around',
+          padding: '4px',
+        }}
+      >
+        <Typography variant="secondary14" color="text.secondary" component="span">
+          <Trans>{title}</Trans>
+        </Typography>
+        {children}
+      </Box>
     </Box>
   );
 }
