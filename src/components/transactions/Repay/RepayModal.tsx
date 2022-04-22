cdc537f7e9e99b9f9ed3b52ea1f1c3990eaea49d
sendra <polsendra@gmail.com>
2022-04-22 17:00:46 +0200

    fix: a bit of optimization to only calculate when repay with collateral is active

--- src/components/transactions/Repay/RepayModal.tsx
+++ src/components/transactions/Repay/RepayModal.tsx
@@ -4,6 +4,8 @@ import { Trans } from '@lingui/macro';
 import React, { useState } from 'react';
 import { useAppDataContext } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { ModalContextType, ModalType, useModalContext } from 'src/hooks/useModal';
+import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
+import { isFeatureEnabled } from 'src/utils/marketsAndNetworksConfig';
 import { BasicModal } from '../../primitives/BasicModal';
 import { ModalWrapper } from '../FlowCommons/ModalWrapper';
 import { CollateralRepayModalContent } from './CollateralRepayModalContent';
@@ -16,39 +18,36 @@ export const RepayModal = () => {
     currentRateMode: InterestRate;
   }>;
   const { userReserves } = useAppDataContext();
+  const { currentMarketData } = useProtocolDataContext();
   const [repayType, setRepayType] = useState(RepayType.BALANCE);
-
-  let supplies: string[] = [];
-  if (userReserves.length > 0) {
-    supplies = userReserves
-      .filter((userReserve: UserReserveData) => userReserve.scaledATokenBalance !== '0')
-      .map((userReserve: UserReserveData) => userReserve.underlyingAsset.toLowerCase());
-  }
-
-  console.log('user reserves ', userReserves);
 
   return (
     <BasicModal open={type === ModalType.Repay} setOpen={close}>
       <ModalWrapper title={<Trans>Repay</Trans>} underlyingAsset={args.underlyingAsset}>
         {(params) => {
-          const remainingSupplies = supplies.filter(
-            (address: string) => address !== params.underlyingAsset.toLowerCase()
-          );
-
-          if (remainingSupplies.length > 0) {
-            return (
-              <>
-                {!mainTxState.txHash && (
-                  <RepayTypeSelector repayType={repayType} setRepayType={setRepayType} />
-                )}
-                {repayType === RepayType.BALANCE && (
-                  <RepayModalContent {...params} debtType={args.currentRateMode} />
-                )}
-                {repayType === RepayType.COLLATERAL && (
-                  <CollateralRepayModalContent {...params} debtType={args.currentRateMode} />
-                )}
-              </>
+          if (isFeatureEnabled.collateralRepay(currentMarketData)) {
+            const supplies = userReserves
+              .filter((userReserve: UserReserveData) => userReserve.scaledATokenBalance !== '0')
+              .map((userReserve: UserReserveData) => userReserve.underlyingAsset.toLowerCase());
+            const remainingSupplies = supplies.filter(
+              (address: string) => address !== params.underlyingAsset.toLowerCase()
             );
+
+            if (remainingSupplies.length > 0) {
+              return (
+                <>
+                  {!mainTxState.txHash && (
+                    <RepayTypeSelector repayType={repayType} setRepayType={setRepayType} />
+                  )}
+                  {repayType === RepayType.BALANCE && (
+                    <RepayModalContent {...params} debtType={args.currentRateMode} />
+                  )}
+                  {repayType === RepayType.COLLATERAL && (
+                    <CollateralRepayModalContent {...params} debtType={args.currentRateMode} />
+                  )}
+                </>
+              );
+            }
           }
 
           return <RepayModalContent {...params} debtType={args.currentRateMode} />;
