852792b13d207d5e52affc57ea5974fa8e83ee1b
Andrew Schmidt <andrew@aave.com>
2022-04-23 05:15:31 -0500

    fix: ability to claim rewards for supply/borrow positions that are withdrawn (#704)
    * fix: ability to claim rewards for supply/borrow positions that are withdrawn
    
    * add test for revard for v3
    
    Co-authored-by: Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>

--- src/components/transactions/ClaimRewards/ClaimRewardsActions.tsx
+++ src/components/transactions/ClaimRewards/ClaimRewardsActions.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro';
 import { Reward } from 'src/helpers/types';
 import { useTransactionHandler } from 'src/helpers/useTransactionHandler';
+import { useAppDataContext } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { useTxBuilderContext } from 'src/hooks/useTxBuilder';
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
@@ -21,22 +22,36 @@ export const ClaimRewardsActions = ({
   const { incentivesTxBuilderV2, incentivesTxBuilder } = useTxBuilderContext();
   const { currentMarketData } = useProtocolDataContext();
   const { currentAccount } = useWeb3Context();
+  const { reserves } = useAppDataContext();
 
   const { action, loadingTxns, mainTxState, requiresApproval } = useTransactionHandler({
     tryPermit: false,
     handleGetTxns: async () => {
       if (currentMarketData.v3) {
+        const allReserves: string[] = [];
+        reserves.forEach((reserve) => {
+          if (reserve.aIncentivesData && reserve.aIncentivesData.length > 0) {
+            allReserves.push(reserve.aTokenAddress);
+          }
+          if (reserve.vIncentivesData && reserve.vIncentivesData.length > 0) {
+            allReserves.push(reserve.variableDebtTokenAddress);
+          }
+          if (reserve.sIncentivesData && reserve.sIncentivesData.length > 0) {
+            allReserves.push(reserve.stableDebtTokenAddress);
+          }
+        });
+
         if (selectedReward.symbol === 'all') {
           return incentivesTxBuilderV2.claimAllRewards({
             user: currentAccount,
-            assets: selectedReward.assets,
+            assets: allReserves,
             to: currentAccount,
             incentivesControllerAddress: selectedReward.incentiveControllerAddress,
           });
         } else {
           return incentivesTxBuilderV2.claimRewards({
             user: currentAccount,
-            assets: selectedReward.assets,
+            assets: allReserves,
             to: currentAccount,
             incentivesControllerAddress: selectedReward.incentiveControllerAddress,
             reward: selectedReward.rewardTokenAddress,
