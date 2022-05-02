cd079c0eb2569cd3ab042b6abe8432064c08a64b
Andrew Schmidt <andrew@aave.com>
2022-05-02 00:42:57 -0500

    feat: add tooltip to available balance in repay with collateral modal

--- src/components/infoTooltips/MaxRepayWithCollateralTooltip.tsx
+++ src/components/infoTooltips/MaxRepayWithCollateralTooltip.tsx
@@ -5,7 +5,10 @@ import { TextWithTooltip, TextWithTooltipProps } from '../TextWithTooltip';
 export const MaxRepayWithCollateralTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
-      <Trans>TO-DO.</Trans>
+      <Trans>
+        The available balance is the minimum of your wallet balance, amount needed to pay off all
+        debt, or amount needed to remain above liquidation threshold.
+      </Trans>
     </TextWithTooltip>
   );
 };
