9da00f13f3d1e96a50411a67e2a6c21c54b3046a
bojank93 <bojan@aave.com>
2022-05-30 18:59:44 +0200

    Switching languages

---
+++ cypress/integration/2-settings/change.language.ts
@@ -0,0 +1,23 @@
+import { configEnvWithTenderlyMainnetFork } from '../../support/steps/configuration.steps';
+
+describe('Changing the language', () => {
+  configEnvWithTenderlyMainnetFork({});
+
+  it('User must be able to change the language from English to Spanish', () => {
+    cy.get('#settings-button').click();
+    cy.contains('Language').click();
+    cy.contains('Spanish').click();
+    cy.reload();
+    cy.get('a[href*="/markets/"]').contains('Mercados');
+    cy.get('a[href*="/"]').contains('Panel');
+  });
+
+  it('User must be able to swith from Spanish to Franch', () => {
+    cy.get('#settings-button').click();
+    cy.contains('Idioma').click(); // Spanish translation of the word Language
+    cy.contains('Francés').click();
+    cy.reload();
+    cy.get('a[href*="/markets/"]').contains('Marchés');
+    cy.get('#more-button').contains('Plus');
+  });
+});
