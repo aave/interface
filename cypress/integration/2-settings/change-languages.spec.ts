4765b765ad08275930d6e6bbfc2acbd2581c409b
bojank93 <bojan@aave.com>
2022-05-31 14:02:55 +0200

    Make additional changes

--- cypress/integration/2-settings/change-languages.spec.ts
+++ cypress/integration/2-settings/change-languages.spec.ts
@@ -1,30 +1,41 @@
+import { changeLanguageCase } from '../../support/steps/actions.steps';
 import { configEnvWithTenderlyMainnetFork } from '../../support/steps/configuration.steps';
 
 describe('Changing the language', () => {
   configEnvWithTenderlyMainnetFork({});
 
-  it('User must be able to change the language from English to Spanish', () => {
+  it('CASE: SWITCH LANGUAGE FROM ENG TO SPANISH', () => {
     cy.get('#settings-button').click();
     cy.contains('Language').click();
     cy.contains('Spanish').click();
     cy.reload();
+  });
+
+  it('CASE2:VERIFY SPANISH TRANSLATION', () => {
     cy.get('a[href*="/markets/"]').contains('Mercados');
     cy.get('a[href*="/"]').contains('Panel');
   });
 
-  it('User must be able to swith from Spanish to Franch', () => {
+  it('CASE3: SWITCH LANGUAGE FROM SPANISH TO FRENCH', () => {
     cy.get('#settings-button').click();
     cy.contains('Idioma').click(); // Spanish translation of the word Language
     cy.contains('Francés').click();
     cy.reload();
+  });
+
+  it('CASE4:VERIFY FRENCH TRANSLATION', () => {
     cy.get('a[href*="/markets/"]').contains('Marchés');
     cy.get('#more-button').contains('Plus');
   });
-  it('User must be able to switch language back to English', () => {
+
+  it('CASE5: SWITCH LANGUAGE FROM FRENCH TO ENGLISH', () => {
     cy.get('#settings-button').click();
     cy.contains('Language').click();
     cy.contains('Anglais').click();
     cy.reload();
+  });
+
+  it('CASE6:VEIRFY ENGLISH TRANSLATION', () => {
     cy.get('a[href*="/markets/"]').contains('Markets');
     cy.get('#more-button').contains('More');
   });
