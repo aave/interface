6ac532ef96ddd54dca3b0181804662dba371a641
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-06-02 11:57:29 +0100

    setup CI

--- cypress/integration/2-settings/change-languages.spec.ts
+++ cypress/integration/2-settings/change-languages.spec.ts
@@ -1,4 +1,3 @@
-import { iteratee } from 'lodash';
 import { configEnvWithTenderlyMainnetFork } from '../../support/steps/configuration.steps';
 
 const switchLanguageStep = (language: string, languageTo: string) => {
@@ -23,13 +22,13 @@ export const verifyTranslationOnMarketsPage = (totalBorrows: string, totalMarket
     cy.contains(totalMarketSize);
   });
 };
-/*const reloadPageAndCheckTranslation = (totalBorrows:string,totalMarketSize:string)=>{
-  it('step4:Reload the page and check Translation', () =>  {
-    cy.reload();
+const reloadPageAndCheckTranslation = (totalBorrows: string, totalMarketSize: string) => {
+  it('step4:Reload the page and check Translation', () => {
+    cy.reload(true);
     cy.contains(totalBorrows);
     cy.contains(totalMarketSize);
   });
-};*/
+};
 
 describe('Manipulation on the language', () => {
   describe('CASE1:Changing the language from English to Spanish', () => {
@@ -37,20 +36,20 @@ describe('Manipulation on the language', () => {
     switchLanguageStep('Language', 'Spanish');
     verifyTranslation('Mercados', 'Más');
     verifyTranslationOnMarketsPage('Total de préstamos', 'Tamaño total del mercado');
-    //reloadPageAndCheckTranslation('Total de préstamos', 'Tamaño total del mercado');
+    reloadPageAndCheckTranslation('Total de préstamos', 'Tamaño total del mercado');
   });
 
   describe('CASE2: Changing the Language from Spanish to French', () => {
     switchLanguageStep('Idioma', 'Francés');
     verifyTranslation('Marchés', 'Plus');
     verifyTranslationOnMarketsPage('Total des emprunts', 'Taille totale du marché');
-    //reloadPageAndCheckTranslation('Total des emprunts','Taille totale du marché');
+    reloadPageAndCheckTranslation('Total des emprunts', 'Taille totale du marché');
   });
 
   describe('CASE3: Change language from French to English', () => {
     switchLanguageStep('Language', 'Anglais');
     verifyTranslation('Markets', 'More');
     verifyTranslationOnMarketsPage('Total borrows', 'Total market size');
-    //reloadPageAndCheckTranslation('Total borrows', 'Total market size');
+    reloadPageAndCheckTranslation('Total borrows', 'Total market size');
   });
 });
