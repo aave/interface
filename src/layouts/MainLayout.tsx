5db0bda93e07a2c9beb6c3ea23eb42a800fa0679
Mark Grothe <grothem@gmail.com>
2022-06-10 15:59:22 -0500

    feat: Add top banner to indicate to users if testnet mode is toggled on

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,47 @@
-import { Box } from '@mui/material';
+import { Trans } from '@lingui/macro';
+import { Box, Button, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
 import React, { ReactNode } from 'react';
+import { ENABLE_TESTNET } from 'src/utils/marketsAndNetworksConfig';
 
 import { AppHeader } from './AppHeader';
 
 export function MainLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  const disableTestnet = () => {
+    const testnetsEnabledId = 'testnetsEnabled';
+    localStorage.setItem(testnetsEnabledId, 'false');
+    router.reload();
+  };
+
+  const banner = (
+    <Box
+      sx={{
+        height: 36,
+        bgcolor: '#1B2030',
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography variant="caption" color="#FFFFFF">
+        <Trans>The app is running in testnet mode</Trans>
+        <Button
+          variant="outlined"
+          size="small"
+          sx={{ mx: 2, backgroundColor: '#EBEBED' }}
+          onClick={disableTestnet}
+        >
+          DISABLE TESTNET
+        </Button>
+      </Typography>
+    </Box>
+  );
+
   return (
     <>
+      {ENABLE_TESTNET && banner}
       <AppHeader />
       <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
         {children}
