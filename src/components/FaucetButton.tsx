0a8a81ddbe58c3e7f799cf6fa9103d518ee76e20
Andrew Schmidt <andrew@aave.com>
2022-06-20 01:02:10 -0500

    feat: add tooltip to faucet link

--- src/components/FaucetButton.tsx
+++ src/components/FaucetButton.tsx
@@ -2,6 +2,7 @@ import { ExternalLinkIcon } from '@heroicons/react/outline';
 import { Trans } from '@lingui/macro';
 import { Button, SvgIcon, Typography } from '@mui/material';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
+import { DarkTooltip } from './infoTooltips/DarkTooltip';
 
 export const FaucetButton = () => {
   const { currentNetworkConfig } = useProtocolDataContext();
@@ -11,13 +12,15 @@ export const FaucetButton = () => {
   };
 
   return (
-    <Button variant="outlined" size="small" onClick={onClick}>
-      <Typography sx={{ display: 'inline-flex', alignItems: 'center' }} variant="buttonS">
-        <Trans>{currentNetworkConfig.name} Faucet</Trans>
-        <SvgIcon sx={{ fontSize: '14px', mx: '2px' }}>
-          <ExternalLinkIcon />
-        </SvgIcon>
-      </Typography>
-    </Button>
+    <DarkTooltip title="Get free assets to test the Aave Protocol">
+      <Button variant="outlined" size="small" onClick={onClick}>
+        <Typography sx={{ display: 'inline-flex', alignItems: 'center' }} variant="buttonS">
+          <Trans>{currentNetworkConfig.name} Faucet</Trans>
+          <SvgIcon sx={{ fontSize: '14px', mx: '2px' }}>
+            <ExternalLinkIcon />
+          </SvgIcon>
+        </Typography>
+      </Button>
+    </DarkTooltip>
   );
 };
