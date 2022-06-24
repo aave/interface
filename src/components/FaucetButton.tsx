6ddddd1092040d6f6bfe264bbdab52be1b7ed0b4
Andrew Schmidt <andrew@aave.com>
2022-06-23 23:01:09 -0500

    feat: consistent routing syntax

--- src/components/FaucetButton.tsx
+++ src/components/FaucetButton.tsx
@@ -1,19 +1,18 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline';
 import { Trans } from '@lingui/macro';
 import { Button, SvgIcon, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { DarkTooltip } from './infoTooltips/DarkTooltip';
+import { ROUTES } from './primitives/Link';
 
 export const FaucetButton = () => {
   const { currentNetworkConfig } = useProtocolDataContext();
-
-  const onClick = () => {
-    window.open(`${window.location.origin}/faucet`);
-  };
+  const router = useRouter();
 
   return (
     <DarkTooltip title="Get free assets to test the Aave Protocol">
-      <Button variant="outlined" size="small" onClick={onClick}>
+      <Button variant="outlined" size="small" onClick={() => router.push(ROUTES.faucet)}>
         <Typography sx={{ display: 'inline-flex', alignItems: 'center' }} variant="buttonS">
           <Trans>{currentNetworkConfig.name} Faucet</Trans>
           <SvgIcon sx={{ fontSize: '14px', mx: '2px' }}>
