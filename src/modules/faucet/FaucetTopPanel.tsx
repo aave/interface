d2f1350641e669e274e2768bb1ec67b179ca095c
Andrew Schmidt <andrew@aave.com>
2022-06-23 22:47:49 -0500

    fix: faucet header text color

--- src/modules/faucet/FaucetTopPanel.tsx
+++ src/modules/faucet/FaucetTopPanel.tsx
@@ -22,13 +22,15 @@ export const FaucetTopPanel = () => {
             withMarketSwitcher={true}
           />
           <Box sx={{ width: md ? (xsm ? '320px' : '540px') : '860px' }}>
-            <Typography variant="description" color="text.secondary">
+            <Typography variant="description" color="#A5A8B6">
               <Trans>
                 With testnet Faucet you can get free assets to test the Aave Protocol. Make sure to
                 switch your wallet provider to the appropriate testnet network, select desired
                 asset, and click ‘Faucet’ to get tokens transferred to your wallet. The assets on a
                 testnet are not “real,” meaning they have no monetary value.{' '}
-                <Link href="https://docs.aave.com/developers/guides/testing-guide">Learn more</Link>
+                <Link color="#A5A8B6" href="https://docs.aave.com/developers/guides/testing-guide">
+                  Learn more
+                </Link>
               </Trans>
             </Typography>
           </Box>
