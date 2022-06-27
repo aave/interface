7cc86cc66e6b51c6553be66cc2fe7aa6494868ef
Lukas <lukasstrassel@googlemail.com>
2022-06-27 15:29:49 +0200

    fix: force quorum to be 1

--- src/utils/marketsAndNetworksConfig.ts
+++ src/utils/marketsAndNetworksConfig.ts
@@ -147,7 +147,6 @@ export const getProvider = (chainId: ChainId): ethersProviders.Provider => {
       chainProviders.push({
         provider: new ethersProviders.StaticJsonRpcProvider(config.privateJsonRPCUrl, chainId),
         priority: 0,
-        weight: 2,
       });
     }
     if (config.publicJsonRPCUrl.length) {
@@ -164,7 +163,7 @@ export const getProvider = (chainId: ChainId): ethersProviders.Provider => {
     if (chainProviders.length === 1) {
       providers[chainId] = chainProviders[0].provider;
     } else {
-      providers[chainId] = new ethersProviders.FallbackProvider(chainProviders);
+      providers[chainId] = new ethersProviders.FallbackProvider(chainProviders, 1);
     }
   }
   return providers[chainId];
