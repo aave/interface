7b1145eba6bba86d7a0f1daaca2759d46b1229b9
bojank93 <bojan@aave.com>
2022-06-28 18:49:15 +0200

    Updated markets.json and modified test

--- cypress/integration/2-settings/switch-market.spec.ts
+++ cypress/integration/2-settings/switch-market.spec.ts
@@ -13,41 +13,13 @@ export const checkNameOfNetwork = (networkName: string) => {
 };
 
 describe('Change markets', () => {
-  Object.entries(markets).forEach(([keyTo, valueTo])=>{
+  configEnvWithTenderlyMainnetFork({});
+  Object.entries(markets).forEach(([keyTo, valueTo]) => {
     describe(`Switching market to ${keyTo}`, () => {
-      configEnvWithTenderlyMainnetFork({});
-      it(`step1: Change the network to the Avalanche ${valueTo.name}`, () => {
+      it(`step1: Change the network to ${valueTo.name}`, () => {
         changeNetwork(valueTo.dataCy);
       });
       checkNameOfNetwork(valueTo.name);
     });
-  });
-  describe('CASE1:Switching markets', () => {
-    configEnvWithTenderlyMainnetFork({});
-    it('step1: Change the network from ETH fork to the Avalanche', () => {
-      changeNetwork('marketSelector_proto_avalanche');
-    });
-    checkNameOfNetwork('Avalanche');
-  });
-
-  describe('CASE2: Switch from Avalanche to Polygon', () => {
-    it('step1: Change the network from Avalanche to Polygon', () => {
-      changeNetwork('marketSelector_proto_polygon');
-    });
-    checkNameOfNetwork('Polygon');
-  });
-
-  describe('CASE3: Switch from Polygon to Fantom', () => {
-    it('step1: Change the network from Polygon to Fantom', () => {
-      changeNetwork('marketSelector_proto_fantom_testnet_v3');
-    });
-    checkNameOfNetwork('Fantom');
-  });
-
-  describe('CASE4:Switch from Fantom to Arbitrum V3', () => {
-    it('step1:Change the network from Fantom to Arbitrum V3', () => {
-      changeNetwork('marketSelector_proto_arbitrum_v3');
-    });
-    checkNameOfNetwork('Arbitrum');
   });
 });
