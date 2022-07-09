ce29dde876697f91ddf66c1f8611871a411302e8
Andrew Schmidt <andrew@aave.com>
2022-07-08 19:30:43 -0500

    feat: switch messaging for add token to wallet after borrow

--- src/components/transactions/FlowCommons/Success.tsx
+++ src/components/transactions/FlowCommons/Success.tsx
@@ -121,11 +121,14 @@ export const TxSuccessView = ({
             >
               <TokenIcon
                 symbol={symbol}
-                aToken={true}
+                aToken={addToken && addToken.aToken ? true : false}
                 sx={{ fontSize: '32px', mt: '12px', mb: '8px' }}
               />
               <Typography variant="description" color="text.primary" sx={{ mx: '24px' }}>
-                <Trans>Add aToken to the wallet to track your supply balance.</Trans>
+                <Trans>
+                  Add {addToken && addToken.aToken ? 'aToken ' : 'token '} to wallet to track your
+                  balance.
+                </Trans>
               </Typography>
               <Button
                 onClick={() => {
