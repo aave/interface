28b6d544937ce708e8d1f748c517d1ebb686f228
Andrew Schmidt <andrew@aave.com>
2022-07-18 13:11:07 -0500

    feat: replace testnet markets (#888)
    * remove: kovan and rinkeby config
    
    * feat: add goerli v3 config
    
    * fix: ropsten public rpc
    
    * feat: update permit config
    
    * feat: update market switcher labels
    
    * feat: re-add eth rinkeby v3
    
    * remove: optimistic kovan network config
    
    * bump: utils version to add optimism goerli chainId
    
    * feat: add optimism goerli v3 config and deprecate eth testnets
    
    * remove: goerli v2 for now
    
    * bump: utils for updated chainId list
    
    * feat: temporarily re-add kovan testnets
    
    * fix switch market test
    
    Co-authored-by: Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>

--- src/ui-config/stakeConfig.ts
+++ src/ui-config/stakeConfig.ts
@@ -32,23 +32,6 @@ export const mainnetStakeConfig: StakeConfig = {
   wsStakeDataUrl: 'wss://cache-api-1.aave.com/graphql',
 };
 
-// kovan config
-export const kovanStakeConfig: StakeConfig = {
-  chainId: ChainId.kovan,
-  stakeDataProvider: '0x2bd8bfcfa19D4c9982d86d7CEf71e3A2e1bcf2cD',
-  tokens: {
-    [Stake.aave]: {
-      TOKEN_STAKING: '0xf2fbf9A6710AfDa1c4AaB2E922DE9D69E0C97fd2',
-      STAKING_REWARD_TOKEN: '0xb597cd8d3217ea6477232f9217fa70837ff667af',
-      STAKING_HELPER: '0xf267aCc8BF1D8b41c89b6dc1a0aD8439dfbc890c',
-    },
-    [Stake.bpt]: {
-      TOKEN_STAKING: '0x31ce45Ab6E26C72c47C52c27498D460099545ef2',
-      STAKING_REWARD_TOKEN: '0xb597cd8d3217ea6477232f9217fa70837ff667af',
-    },
-  },
-};
-
 export const getStakeConfig = () => {
   return mainnetStakeConfig;
 };
