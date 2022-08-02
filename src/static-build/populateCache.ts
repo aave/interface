e7b042c14acfa213bb550c151f8f29ccf6d86ca8
Andrew Schmidt <andrew@aave.com>
2022-08-02 12:41:56 -0500

    fix: governance cache failure (#962)
    * fix: governance cache failure
    
    * revert: change to vote cache
    
    * fix: disabled unused vote caching due to parsing error

--- src/static-build/populateCache.ts
+++ src/static-build/populateCache.ts
@@ -1,17 +1,21 @@
 import { governanceContract } from '../modules/governance/utils/governanceProvider';
 import { Ipfs } from './ipfs';
 import { Proposal } from './proposal';
-import { Vote } from './vote';
+//import { Vote } from './vote';
+
+/*
+Voting cache is currently disabled due to issue #992
+*/
 
 export async function populateCache() {
   const count = await governanceContract.getProposalsCount();
   const ipfsFetcher = new Ipfs();
   const proposalFetcher = new Proposal();
-  const voteFetcher = new Vote();
+  //const voteFetcher = new Vote();
   for (let i = 0; i < count; i++) {
     const proposal = await proposalFetcher.populate(i);
     await ipfsFetcher.populate(i, proposal);
-    await voteFetcher.populate(i, proposal.startBlock, proposal.endBlock);
+    // await voteFetcher.populate(i, proposal.startBlock, proposal.endBlock);
   }
 }
 populateCache().then(() => console.log('finished'));
