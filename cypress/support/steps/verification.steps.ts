894c9f5906d9b1c1149e4b4aa14099b9d489c1dc
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-08-05 13:13:25 +0100

    fix critical test for fantom

--- cypress/support/steps/verification.steps.ts
+++ cypress/support/steps/verification.steps.ts
@@ -255,7 +255,7 @@ export const checkDashboardHealthFactor = (
                 valueFrom +
                 ' till ' +
                 valueTo,
-              timeout: 20000,
+              timeout: 40000,
               interval: 500,
             }
           );
