2a8be0d84ecff4accf469f409976a156b353f6e1
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-08-17 20:59:35 +0100

    middle implement

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+
+const folder = `cypress/e2e/0-v2-markets/3-avalanche-v2-market/`;
 
 export default defineConfig({
   viewportWidth: 1200,
   viewportHeight: 800,
   defaultCommandTimeout: 40000,
   pageLoadTimeout: 120000,
-  video: true,
+  video: false,
   watchForFileChanges: false,
   scrollBehavior: 'center',
   retries: {
@@ -16,7 +18,10 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
+    specPattern :[
+      folder + "0-assets/avax.avalanche-v2.cy.ts"
+    ]
   },
 })
---
+++ cypress/configs/v2-markets/avalanche-v2-smoke.config.ts
@@ -0,0 +1,53 @@
+// {
+//   "integrationFolder": "cypress/e2e/0-v2-markets/3-avalanche-v2-market",
+//   "testFiles": [
+//     "0-assets/avax.avalanche-v2.cy.ts",
+//     "0-assets/usdt.avalanche-v2.cy.ts",
+//     "reward.avalanche-v2.cy.ts"
+//   ],
+//   "viewportWidth": 1200,
+//   "viewportHeight": 800,
+//   "defaultCommandTimeout": 40000,
+//   "pageLoadTimeout": 120000,
+//   "video": false,
+//   "scrollBehavior": "center",
+//   "retries": {
+//     "runMode": 1,
+//     "openMode": 0
+//   }
+// }
+
+import { defineConfig } from 'cypress'
+
+
+const folder = `cypress/e2e/0-v2-markets/3-avalanche-v2-market/`
+
+
+
+export default defineConfig({
+  viewportWidth: 1200,
+  viewportHeight: 800,
+  defaultCommandTimeout: 40000,
+  pageLoadTimeout: 120000,
+  video: true,
+  watchForFileChanges: false,
+  scrollBehavior: 'center',
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require('../../plugins/index.js')(on, config);
+    },
+    specPattern :[
+      folder + "0-assets/avax.avalanche-v2.cy.ts"
+    ]
+  },
+})
+
