dbd67968c11a6df4e4e58e019706184433888ab1
Andrew Schmidt <andrew@aave.com>
2022-08-17 00:10:35 -0500

    refactor: emode selection menu

--- src/components/transactions/Emode/EmodeSelect.tsx
+++ src/components/transactions/Emode/EmodeSelect.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react';
 import { Trans } from '@lingui/macro';
-import { Box, Checkbox, FormLabel, Typography } from '@mui/material';
+import { FormLabel, Typography } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import * as React from 'react';
-import { Row } from 'src/components/primitives/Row';
 import { EmodeCategory } from 'src/helpers/types';
-import { useAppDataContext } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { getEmodeMessage } from './EmodeNaming';
 
 export type EmodeSelectProps = {
   emodeCategories: Record<number, EmodeCategory>;
-  selectedEmode: number;
+  selectedEmode: number | undefined;
   setSelectedEmode: React.Dispatch<React.SetStateAction<EmodeCategory | undefined>>;
   baseAssetSymbol: string;
   userEmode: number;
@@ -25,22 +22,20 @@ export const EmodeSelect = ({
   baseAssetSymbol,
   userEmode,
 }: EmodeSelectProps) => {
-  const { user } = useAppDataContext();
-  const [disableEmode, setDisableEmode] = useState<boolean>(selectedEmode === 0 ? true : false);
   return (
     <FormControl sx={{ mb: 1, width: '100%' }}>
       <FormLabel sx={{ mb: 1, color: 'text.secondary' }}>
-        <Trans>Switch asset category</Trans>
+        <Trans>Asset category</Trans>
       </FormLabel>
 
       <Select
+        defaultValue={0}
         value={selectedEmode}
         onChange={(e) => {
           setSelectedEmode(emodeCategories[Number(e.target.value)]);
         }}
         className="EmodeSelect"
         data-cy="EmodeSelect"
-        disabled={disableEmode}
         sx={{
           width: '100%',
           height: '44px',
@@ -64,13 +59,16 @@ export const EmodeSelect = ({
         }}
         native={false}
         renderValue={(emode) => {
-          if (disableEmode) {
-            return <Typography color="text.primary">----------</Typography>;
-          }
           if (emode !== 0) {
             return (
               <Typography color="text.primary">
-                {getEmodeMessage(emodeCategories[selectedEmode].id, baseAssetSymbol)}
+                {getEmodeMessage(emodeCategories[emode].id, baseAssetSymbol)}
+              </Typography>
+            );
+          } else {
+            return (
+              <Typography color="text.muted">
+                <Trans>Select</Trans>
               </Typography>
             );
           }
@@ -93,59 +91,6 @@ export const EmodeSelect = ({
           }
         })}
       </Select>
-
-      {user.userEmodeCategoryId !== 0 && (
-        <>
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', my: 2 }}>
-            <Typography variant="description" color="text.primary">
-              <Trans>or</Trans>
-            </Typography>
-          </Box>
-          <Row
-            sx={{
-              width: '100%',
-              alignItems: 'center',
-              height: '44px',
-              borderRadius: '6px',
-              borderColor: 'divider',
-              outline: 'none !important',
-              color: 'text.primary',
-              '.MuiOutlinedInput-input': {
-                backgroundColor: 'transparent',
-              },
-              '.MuiOutlinedInput-notchedOutline, .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'divider',
-                outline: 'none !important',
-                borderWidth: '1px',
-              },
-              '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'divider',
-                borderWidth: '1px',
-              },
-              '&.EmodeSelect .MuiSelect-icon': { color: 'text.primary' },
-            }}
-          >
-            <FormLabel sx={{ color: 'text.secondary' }}>
-              <Trans>Disable E-mode</Trans>
-            </FormLabel>
-
-            <Checkbox
-              sx={{ px: 0 }}
-              value={disableEmode}
-              data-cy={`disableCheckboxEmode`}
-              defaultChecked={false}
-              onChange={() => {
-                if (disableEmode) {
-                  setSelectedEmode(userEmode === 1 ? emodeCategories[2] : emodeCategories[1]);
-                } else {
-                  setSelectedEmode(emodeCategories[0]);
-                }
-                setDisableEmode(!disableEmode);
-              }}
-            />
-          </Row>
-        </>
-      )}
     </FormControl>
   );
 };
