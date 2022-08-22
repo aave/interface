604976c4f9113a8af17520678ae1e2b0de0fe01f
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-08-22 15:17:09 +0100

    full v2 spec

--- cypress/configs/v2-markets/avalanche-v2-smoke.config.ts
+++ cypress/configs/v2-markets/avalanche-v2-smoke.config.ts
@@ -27,7 +27,11 @@ const folder = `./cypress/e2e/0-v2-markets/3-avalanche-v2-market/`;
 export default defineConfig({
   ...defaultConfig,
   e2e: {
-    specPattern: [folder + '0-assets/avax.avalanche-v2.cy.ts'],
+    specPattern: [
+      folder + '0-assets/avax.avalanche-v2.cy.ts',
+      folder + "0-assets/usdt.avalanche-v2.cy.ts",
+      folder + "reward.avalanche-v2.cy.ts"
+    ],
   },
 
   // viewportWidth: 1200,
