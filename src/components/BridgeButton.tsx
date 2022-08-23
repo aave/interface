744743f1a69a8414f6dad95fab8dc0cec356139b
Mark Grothe <grothem@gmail.com>
2022-08-23 16:26:07 -0500

    fix: use start and end icon props

--- src/components/BridgeButton.tsx
+++ src/components/BridgeButton.tsx
@@ -8,14 +8,19 @@ export const BridgeButton = ({ bridge }: Pick<NetworkConfig, 'bridge'>) => {
   if (!bridge) return null;
 
   return (
-    <Button component={Link} size="small" variant="outlined" href={bridge.url || ''}>
-      <img src={bridge.icon} alt={bridge.name} style={{ width: 14, height: 14 }} />
-      <Typography sx={{ mx: 1 }} variant="buttonS">
-        {bridge.name}
-      </Typography>
-      <SvgIcon sx={{ fontSize: 14 }}>
-        <ExternalLinkIcon />
-      </SvgIcon>
+    <Button
+      startIcon={<img src={bridge.icon} alt={bridge.name} style={{ width: 14, height: 14 }} />}
+      endIcon={
+        <SvgIcon sx={{ width: 14, height: 14 }}>
+          <ExternalLinkIcon />
+        </SvgIcon>
+      }
+      component={Link}
+      size="small"
+      variant="outlined"
+      href={bridge.url || ''}
+    >
+      <Typography variant="buttonS">{bridge.name}</Typography>
     </Button>
   );
 };
--- src/components/FaucetButton.tsx
+++ src/components/FaucetButton.tsx
@@ -10,18 +10,27 @@ export const FaucetButton = () => {
 
   return (
     <DarkTooltip title="Get free assets to test the Aave Protocol">
-      <Button component={Link} href={ROUTES.faucet} variant="outlined" size="small">
-        <img
-          src={currentNetworkConfig.networkLogoPath}
-          alt={currentNetworkConfig.name}
-          style={{ width: 14, height: 14 }}
-        />
-        <Typography sx={{ mx: 1 }} variant="buttonS">
+      <Button
+        startIcon={
+          <img
+            src={currentNetworkConfig.networkLogoPath}
+            alt={currentNetworkConfig.name}
+            style={{ width: 14, height: 14 }}
+          />
+        }
+        endIcon={
+          <SvgIcon sx={{ width: 14, height: 14 }}>
+            <ExternalLinkIcon />
+          </SvgIcon>
+        }
+        component={Link}
+        href={ROUTES.faucet}
+        variant="outlined"
+        size="small"
+      >
+        <Typography variant="buttonS">
           <Trans>{currentNetworkConfig.name} Faucet</Trans>
         </Typography>
-        <SvgIcon sx={{ fontSize: 14 }}>
-          <ExternalLinkIcon />
-        </SvgIcon>
       </Button>
     </DarkTooltip>
   );
