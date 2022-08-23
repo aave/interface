3272eca9f885d97623b9937dcf5587fb3e5fae4c
Nikita Boakrev <nikitaboakrev@Nikitas-MacBook-Pro.local>
2022-08-23 15:30:43 +0100

    fix

--- cypress/configs/v2-markets/ethereum-v2-full.config.ts
+++ cypress/configs/v2-markets/ethereum-v2-full.config.ts
@@ -1,32 +1,7 @@
-{
-  "integrationFolder": "cypress/e2/0-v2-markets/0-main-v2-market",
-  "testFiles": [
-    "**/*.*"
-  ],
-  "ignoreTestFiles": [
-    "eth.aave-v2.cy.ts",
-    "dai.aave-v2.cy.ts",
-    "swap.aave-v2.cy.ts",
-    "stake.aave-v2.cy.ts",
-    "reward.aave-v2.cy.ts",
-    "critical-conditions.aave-v2.cy.ts"
-  ],
-  "viewportWidth": 1200,
-  "viewportHeight": 800,
-  "defaultCommandTimeout": 40000,
-  "pageLoadTimeout": 120000,
-  "video": false,
-  "scrollBehavior": "center",
-  "retries": {
-    "runMode": 1,
-    "openMode": 0
-  }
-}
-
 import { defineConfig } from 'cypress';
 import { defaultConfig } from '../base.cypress';
 
-const folder = `./cypress/e2e/0-v2-markets/3-avalanche-v2-market/`;
+const folder = `./cypress/e2e/0-v2-markets/0-main-v2-market/`;
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
