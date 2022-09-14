c954477966ad5d50fcbb76ee0076b0b8f6d3affd
Mark Grothe <grothem@gmail.com>
2022-09-14 10:55:59 +0100

    chore: run prettier

--- cypress/configs/v2-markets/amm-v2-full.config.ts
+++ cypress/configs/v2-markets/amm-v2-full.config.ts
@@ -6,10 +6,7 @@ const folder = `./cypress/e2e/0-v2-markets/1-amm-v2-market/`;
 export default defineConfig({
   ...defaultConfig,
   e2e: {
-    specPattern: [folder + "**/*.*"],
-    excludeSpecPattern: [
-      "/**/eth.amm-v2.cy.ts",
-      "/**/usdt.amm-v2.cy.ts",
-    ],
+    specPattern: [folder + '**/*.*'],
+    excludeSpecPattern: ['/**/eth.amm-v2.cy.ts', '/**/usdt.amm-v2.cy.ts'],
   },
 });
--- cypress/configs/v3-markets/fantom-v3-full.config.ts
+++ cypress/configs/v3-markets/fantom-v3-full.config.ts
@@ -6,14 +6,14 @@ const folder = `./cypress/e2e/1-v3-markets/5-fantom-v3-market/`;
 export default defineConfig({
   ...defaultConfig,
   e2e: {
-    specPattern: [folder + "**/*.*"],
+    specPattern: [folder + '**/*.*'],
     excludeSpecPattern: [
-      "/**/ftm.fantom-v3.cy.ts",
-      "/**/usdt.fantom-v3.cy.ts",
-      "/**/swap.fantom-v3.cy.ts",
-      "/**/e-mode.fantom-v3.cy.ts",
-      "/**/isolated-mode.fantom-v3.cy.ts",
-      "/**/critical-conditions.fantom-v3.cy.ts",
+      '/**/ftm.fantom-v3.cy.ts',
+      '/**/usdt.fantom-v3.cy.ts',
+      '/**/swap.fantom-v3.cy.ts',
+      '/**/e-mode.fantom-v3.cy.ts',
+      '/**/isolated-mode.fantom-v3.cy.ts',
+      '/**/critical-conditions.fantom-v3.cy.ts',
     ],
   },
 });
