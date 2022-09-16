046d552f91ee5607257ff488d4911aa66db5acae
Drew Cook <drewcook29@gmail.com>
2022-09-16 14:56:02 +0100

    chore: remove throw

--- pages/reserve-overview.page.tsx
+++ pages/reserve-overview.page.tsx
@@ -30,7 +30,6 @@ export default function ReserveOverview() {
   const [mode, setMode] = useState<'overview' | 'actions' | ''>('');
 
   useEffect(() => {
-    throw new Error('woah');
     if (!mode) setMode('overview');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lg]);
