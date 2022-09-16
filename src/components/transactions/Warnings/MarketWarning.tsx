a9e1c77b8ca31fcdf30514f2bf548b5bf28d044f
Andrew Schmidt <andrew@aave.com>
2022-09-16 08:14:19 -0500

    feat: improve frozen warning mappings

---
+++ src/components/transactions/Warnings/MarketWarning.tsx
@@ -0,0 +1,31 @@
+import { Trans } from '@lingui/macro';
+import { Link, Typography, AlertColor } from '@mui/material';
+
+import { Warning } from '../../primitives/Warning';
+
+interface MarketWarningProps {
+  learnMore?: boolean; // Modify wording on link text,
+  warningMessage: string;
+  linkHref?: string;
+  warningType: AlertColor;
+}
+
+export const MarketWarning = ({
+  learnMore,
+  warningMessage,
+  linkHref,
+  warningType,
+}: MarketWarningProps) => {
+  return (
+    <Warning severity={warningType}>
+      <Typography variant="caption">
+        <Trans>
+          {warningMessage}{' '}
+          <Link href={linkHref} target="_blank">
+            {learnMore ? 'Learn More' : 'Join the community discussion'}
+          </Link>
+        </Trans>
+      </Typography>
+    </Warning>
+  );
+};
