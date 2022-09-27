25514ecf4d12aff31855ee03b8de318bbb4a992b
Andrew Schmidt <andrew@aave.com>
2022-09-27 15:06:03 +0200

    fix: remove duplicate warning (#1173)

--- src/components/transactions/Borrow/BorrowModalContent.tsx
+++ src/components/transactions/Borrow/BorrowModalContent.tsx
@@ -222,7 +222,6 @@ export const BorrowModalContent = ({
   return (
     <>
       {borrowCap.determineWarningDisplay({ borrowCap })}
-      {debtCeiling.determineWarningDisplay({ debtCeiling })}
       {poolReserve.isIsolated && debtCeiling.determineWarningDisplay({ debtCeiling })}
 
       <AssetInput
