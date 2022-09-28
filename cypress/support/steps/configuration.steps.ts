20470e4d979bf913f9f36926ed20b1e0333fbe98
Mark Grothe <grothem@gmail.com>
2022-09-28 15:32:01 -0500

    test: don't set a watch wallet in cypress tests

--- cypress/support/steps/configuration.steps.ts
+++ cypress/support/steps/configuration.steps.ts
@@ -59,7 +59,6 @@ export const configEnvWithTenderly = ({
         win.localStorage.setItem('selectedAccount', walletAddress.toLowerCase());
         win.localStorage.setItem('selectedMarket', market);
         win.localStorage.setItem('testnetsEnabled', enableTestnet.toString());
-        win.localStorage.setItem('watchModeOnlyAddress', walletAddress.toLowerCase());
       },
     });
   });
