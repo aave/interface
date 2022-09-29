c79c84c618539e8ceb8c2edf56190aa8950b4fce
Mark Grothe <grothem@gmail.com>
2022-09-29 12:47:24 -0500

    fix: swap ipfs gateway

--- src/ui-config/governanceConfig.ts
+++ src/ui-config/governanceConfig.ts
@@ -42,6 +42,6 @@ export const governanceConfig: GovernanceConfig = {
     AAVE_GOVERNANCE_V2_EXECUTOR_LONG: '0xEE56e2B3D491590B5b31738cC34d5232F378a8D5',
     AAVE_GOVERNANCE_V2_HELPER: '0x16ff7583ea21055bf5f929ec4b896d997ff35847',
   },
-  ipfsGateway: 'https://gateway.pinata.cloud/ipfs',
-  fallbackIpfsGateway: 'https://cloudflare-ipfs.com/ipfs',
+  ipfsGateway: 'https://cloudflare-ipfs.com/ipfs',
+  fallbackIpfsGateway: 'https://gateway.pinata.cloud/ipfs',
 };
