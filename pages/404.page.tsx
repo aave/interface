b130af9ac2233d9bac4991c272478c0ab85e71ab
Graham Nelson <grahamnelson@live.com>
2022-09-29 11:27:34 +0200

    chore: remove-brackets

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -25,7 +25,7 @@ export default function Aave404Page() {
           }}
         >
           <Box sx={{ maxWidth: 444, m: '0 auto' }}>
-            <img width="100%" height="auto" src={`/404/404.svg`} alt="404 - Page not found" />
+            <img width="100%" height="auto" src="/404/404.svg" alt="404 - Page not found" />
           </Box>
           <Typography variant="display1" sx={{ mt: 2 }}>
             <Trans>Page not found</Trans>
