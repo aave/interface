aa355d162af6d321a3854ff945a4a7d56ea2036d
Andrew Schmidt <andrew@aave.com>
2022-10-18 15:00:15 -0500

    feat: add skeleton state to price impact

--- src/components/infoTooltips/PriceImpactTooltip.tsx
+++ src/components/infoTooltips/PriceImpactTooltip.tsx
@@ -1,10 +1,44 @@
 import { Trans } from '@lingui/macro';
+import { Box, Skeleton } from '@mui/material';
 
-import { TextWithTooltip, TextWithTooltipProps } from '../TextWithTooltip';
+import { FormattedNumber } from '../primitives/FormattedNumber';
+import { TextWithTooltip } from '../TextWithTooltip';
 
-export const PriceImpactTooltip = ({ ...rest }: TextWithTooltipProps) => {
+interface PriceImpactTooltipProps {
+  loading: boolean;
+  priceImpact: string;
+}
+
+export const PriceImpactTooltip = ({ loading, priceImpact }: PriceImpactTooltipProps) => {
   return (
-    <TextWithTooltip {...rest} variant="secondary12" color="text.secondary">
+    <TextWithTooltip
+      variant="secondary12"
+      color="text.secondary"
+      text={
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Trans>
+            Price impact{' '}
+            {loading ? (
+              <Skeleton
+                variant="rectangular"
+                height={12}
+                width={25}
+                sx={{ borderRadius: '4px', display: 'flex', marginLeft: '4px' }}
+              />
+            ) : (
+              <FormattedNumber
+                value={priceImpact}
+                visibleDecimals={2}
+                variant="secondary12"
+                color="text.secondary"
+                sx={{ marginLeft: '4px' }}
+              />
+            )}
+            %
+          </Trans>
+        </Box>
+      }
+    >
       <Trans>
         Price impact is the spread between the total value of the entry tokens swapped and the
         destination tokens obtained (in USD), which results from the limited liquidity of the
