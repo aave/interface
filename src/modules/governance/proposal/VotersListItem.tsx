bb0368bcdf9caf9aaaf13587dedf906b3b07b4ed
Drew Cook <drewcook29@gmail.com>
2022-10-20 22:34:48 +0200

    fix: forego using ENS avatars

--- src/modules/governance/proposal/VotersListItem.tsx
+++ src/modules/governance/proposal/VotersListItem.tsx
@@ -1,10 +1,8 @@
 import { ExternalLinkIcon } from '@heroicons/react/solid';
 import { Avatar, Box, SvgIcon, Typography } from '@mui/material';
 import makeBlockie from 'ethereum-blockies-base64';
-import { SyntheticEvent, useEffect, useState } from 'react';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
 import { Link } from 'src/components/primitives/Link';
-import usePreviousState from 'src/hooks/usePreviousState';
 
 import { textCenterEllipsis } from '../../../helpers/text-center-ellipsis';
 import type { GovernanceVoter } from './VotersListContainer';
@@ -15,23 +13,8 @@ type VotersListItemProps = {
 };
 
 export const VotersListItem = ({ compact, voter }: VotersListItemProps): JSX.Element | null => {
-  const { address, ensAvatar, ensName, proposalVotingPower, twitterAvatar } = voter;
+  const { address, ensName, proposalVotingPower, twitterAvatar } = voter;
   const blockieAvatar = makeBlockie(address !== '' ? address : 'default');
-
-  // For avatars, the order of precedence is Twitter, then ENS, then default Blockie
-  // If ENS avatar does not exist, then use Blockie
-  const [displayAvatar, setDisplayAvatar] = useState<string>(
-    twitterAvatar ?? ensAvatar ?? blockieAvatar
-  );
-  const [avatarErrored, setAvatarErrored] = useState<boolean>(false);
-  const prevAvatarErrored = usePreviousState<boolean>(avatarErrored);
-  const handleAvatarError = (e: SyntheticEvent) => {
-    e.preventDefault();
-    setAvatarErrored(true);
-  };
-  useEffect(() => {
-    if (!prevAvatarErrored && avatarErrored) setDisplayAvatar(blockieAvatar);
-  }, [avatarErrored]); /* eslint-disable-line react-hooks/exhaustive-deps */
 
   // This function helps determine how to display either the address or ENS name, in a way where the list looks good and names are about equal length. This takes into account if the list should be compact or not.
   const displayName = (name: string) => {
@@ -77,11 +60,7 @@ export const VotersListItem = ({ compact, voter }: VotersListItemProps): JSX.Ele
     <Box sx={{ my: 6, '&:first-of-type': { mt: 0 }, '&:last-of-type': { mb: 0 } }}>
       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
-          <Avatar
-            src={displayAvatar}
-            sx={{ width: 24, height: 24, mr: 2 }}
-            onError={handleAvatarError}
-          />
+          <Avatar src={twitterAvatar ?? blockieAvatar} sx={{ width: 24, height: 24, mr: 2 }} />
           <Link href={`https://etherscan.io/address/${address}`}>
             <Typography
               variant="subheader1"
