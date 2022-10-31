4293dc772e8642c443a6e7f9db075d54f75eb7a3
Andrew Schmidt <andrew@aave.com>
2022-10-31 13:21:25 -0500

    chore: clean up layout

--- src/components/infoTooltips/AMPLToolTip.tsx
+++ src/components/infoTooltips/AMPLToolTip.tsx
@@ -6,7 +6,13 @@ import { AMPLWarning } from '../Warnings/AMPLWarning';
 
 export const AMPLToolTip = () => {
   return (
-    <ContentWithTooltip tooltipContent={<Box>{<AMPLWarning />}</Box>}>
+    <ContentWithTooltip
+      tooltipContent={
+        <Box>
+          <AMPLWarning />
+        </Box>
+      }
+    >
       <SvgIcon sx={{ fontSize: '20px', color: 'warning.main', ml: 2 }}>
         <ExclamationIcon />
       </SvgIcon>
