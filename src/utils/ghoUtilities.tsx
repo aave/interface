baad13ef7643f48a200acb27723a27eb8e6192db
Mark Grothe <grothem@gmail.com>
2022-11-16 11:18:17 -0600

    refactor: added util function for converting bps to percentage

--- src/utils/ghoUtilities.tsx
+++ src/utils/ghoUtilities.tsx
@@ -101,3 +101,12 @@ export const weightedAverageAPY = (
     totalBorrowAmount
   );
 };
+
+/**
+  Converts a BPS value into a percentage. BPS values are returned from the smart contract for discount rates and fee values. This is represented as percentages across the UI.
+  @param bpsValue {BigNumber} - The BigNumber representing the uint256 returned from the smart contracts to be converted
+  @returns {number} - A percentage of basis points. i.e. 2% would be 0.02
+*/
+export const convertBpsToPercentage = (bpsValue: BigNumber): number => {
+  return bpsValue.toNumber() * 0.0001;
+};
