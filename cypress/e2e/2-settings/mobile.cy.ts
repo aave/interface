f734c7393745b5ec1cf03720bbe047688f61ac6d
bojank93 <bojan@aave.com>
2022-11-17 15:17:15 +0100

    Added coverage for the X button and dark mode

--- cypress/e2e/2-settings/mobile.cy.ts
+++ cypress/e2e/2-settings/mobile.cy.ts
@@ -9,7 +9,16 @@ const checkContent = (name: string, link: string) => {
 };
 
 const doFindMobileMenuElement = (value: string) => {
-  return cy.get('[data-cy="mobile-menu"]').find(value).click();
+  cy.get('[data-cy="mobile-menu"]').contains(value).click();
+};
+
+const switchLanguage = (selector: string) => {
+  cy.get('[data-cy="mobile-menu"]').contains('Language').click({ force: true });
+  cy.get('[data-cy="mobile-menu"]').contains(selector).click();
+};
+
+const backgroundColour = (color: string) => {
+  cy.get('body').should('have.css', 'background-color', `${color}`);
 };
 
 describe('MOBILE RESOLUTION SPEC, AAVE V2 MARKET', () => {
@@ -22,10 +31,11 @@ describe('MOBILE RESOLUTION SPEC, AAVE V2 MARKET', () => {
       checkContent('Stake', '/staking/');
     });
   });
+
   describe('CASE2:Check proposal pages', () => {
     const _maxResolution = 312;
     it('Verify width of proposal body with long text', () => {
-      doFindMobileMenuElement('[data-cy="menuGovernance"]');
+      cy.get('[data-cy="mobile-menu"]').find('[data-cy="menuGovernance"]').click();
       cy.get('a[href*="/governance/proposal/109/"]')
         .contains('Whitelist Balancer’s Liquidity Mining Claim')
         .click();
@@ -38,6 +48,38 @@ describe('MOBILE RESOLUTION SPEC, AAVE V2 MARKET', () => {
         .contains('Chaos Labs Risk Platform Proposal')
         .click();
       checkWidthOfText(_maxResolution);
+    });
+  });
+
+  describe('CASE3:Switch language and check translation', () => {
+    it('Open menu and switch language', () => {
+      cy.get('#settings-button-mobile').click();
+      switchLanguage('Spanish');
+      cy.contains('Seleccionar idioma');
+      doFindMobileMenuElement('Inglés');
+      doFindMobileMenuElement('Select language');
+    });
+  });
+
+  describe('CASE4: Switch to Dark mode', () => {
+    it('step1:Switch to dark mode', () => {
+      doFindMobileMenuElement('Dashboard');
+      cy.get('#settings-button-mobile').click();
+      doFindMobileMenuElement('Dark mode');
+      doFindMobileMenuElement('Dashboard');
+    });
+    it('step2: Check background color(dark)', () => {
+      backgroundColour('rgb(27, 32, 48)');
+    });
+  });
+
+  describe('CASE5:Open Markets page and close it', () => {
+    it('Open Markets page and then open menu and close menu', () => {
+      cy.get('#settings-button-mobile').click();
+      doFindMobileMenuElement('Markets');
+      cy.get('#settings-button-mobile').click();
+      cy.get('[data-cy="closeMenuMobile"]').click();
+      cy.contains('Total market size');
     });
   });
 });
