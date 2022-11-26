fe1750bc8ffaa66d4085f80c1a24778676c3dcf6
Lukas <lukasstrassel@googlemail.com>
2022-11-26 09:56:58 +0100

    fix: governance images (#1330)
    * fix: governance images
    
    * fix: fix type

--- pages/governance/proposal/[proposalId].governance.tsx
+++ pages/governance/proposal/[proposalId].governance.tsx
@@ -239,7 +239,14 @@ export default function ProposalPage({
                     <ReactMarkdown
                       remarkPlugins={[remarkGfm]}
                       components={{
-                        img({ src, alt }) {
+                        img({ src: _src, alt }) {
+                          if (!_src) return null;
+                          const src = /^\.\.\//.test(_src)
+                            ? _src.replace(
+                                '../',
+                                'https://raw.githubusercontent.com/aave/aip/main/content/'
+                              )
+                            : _src;
                           return <CenterAlignedImage src={src} alt={alt} />;
                         },
                         a({ node, ...rest }) {
