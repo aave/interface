f9504d5693b1113012df6cd8b423417610b3c526
Mark Grothe <grothem@gmail.com>
2022-11-28 13:47:27 -0600

    fix: lint

--- src/modules/markets/Gho/GhoMarketAssetsListMobileItem.tsx
+++ src/modules/markets/Gho/GhoMarketAssetsListMobileItem.tsx
@@ -1,6 +1,5 @@
 import { Trans } from '@lingui/macro';
 import { Box, Button, Divider, Typography, useTheme } from '@mui/material';
-import { GhoDiscountedBorrowAPYTag } from 'src/components/GhoDiscountedBorrowAPYTag';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
 import { Link, ROUTES } from 'src/components/primitives/Link';
 import { Row } from 'src/components/primitives/Row';
@@ -21,7 +20,6 @@ export const GhoMarketAssetsListMobileItem = ({ reserve }: GhoMarketAssetsListMo
   const theme = useTheme();
 
   const {
-    ghoDiscountRatePercent,
     ghoLoadingData,
     ghoLoadingMarketData,
     ghoComputed: { borrowAPYWithMaxDiscount },
