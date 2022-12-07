bffa27a47f69492b06d2e22d44bd8cbcce1a13dd
Mark Grothe <grothem@gmail.com>
2022-12-07 14:01:47 -0600

    fix: stub address screening api call

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,10 @@
 import './commands';
+
+before(() => {
+  cy.intercept('GET', 'https://aave-api-v2.aave.com/addresses/status*', {
+    statusCode: 200,
+    body: {
+      addressAllowed: true,
+    },
+  });
+});
