0d094b4f256118a17d7a63aa96922db69a8c6d1a
Mark Grothe <grothem@gmail.com>
2022-12-07 14:23:43 -0600

    fix: idToken not required, defaults to true

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,13 +2,11 @@ import NextAuth from 'next-auth';
 import Okta from 'next-auth/providers/okta';
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     Okta({
       clientId: process.env.OKTA_CLIENTID as string,
       clientSecret: process.env.OKTA_CLIENTSECRET as string,
       issuer: process.env.OKTA_DOMAIN as string,
-      idToken: true,
     }),
   ],
 };
