01fa96d028ec37e878e7cd97c23692970f31e421
Mark Grothe <grothem@gmail.com>
2022-12-08 13:34:27 -0600

    fix: revet intercept

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,10 +1,1 @@
 import './commands';
-
-before(() => {
-  cy.intercept('GET', 'https://aave-api-v2.aave.com/addresses/status*', {
-    statusCode: 200,
-    body: {
-      addressAllowed: true,
-    },
-  });
-});
