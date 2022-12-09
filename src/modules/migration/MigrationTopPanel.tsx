7896f1525b63288b27cd34693ccc676173a77fd1
Alex <argeares97@gmail.com>
2022-12-09 14:35:23 +0200

    fix: migrate screen top panel

--- src/modules/migration/MigrationTopPanel.tsx
+++ src/modules/migration/MigrationTopPanel.tsx
@@ -1,25 +1,25 @@
 import { Trans } from '@lingui/macro';
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { PageTitle } from 'src/components/TopInfoPanel/PageTitle';
 import { TopInfoPanel } from 'src/components/TopInfoPanel/TopInfoPanel';
 
+import { getMarketInfoById } from '../../components/MarketSwitcher';
+import { useProtocolDataContext } from '../../hooks/useProtocolDataContext';
+
 export const MigrationTopPanel = () => {
-  const { breakpoints } = useTheme();
-  const md = useMediaQuery(breakpoints.down('md'));
-  const xsm = useMediaQuery(breakpoints.down('xsm'));
+  const { currentMarket } = useProtocolDataContext();
+  const { market } = getMarketInfoById(currentMarket);
 
   return (
     <TopInfoPanel
       pageTitle={<></>}
       titleComponent={
-        <Box>
-          <PageTitle pageTitle={<Trans>Migration</Trans>} withMarketSwitcher={true} />
-          <Box sx={{ width: md ? (xsm ? '320px' : '540px') : '860px' }}>
-            <Typography variant="description" color="#A5A8B6">
-              <Trans>TODO: text about migration</Trans>
-            </Typography>
-          </Box>
-        </Box>
+        <PageTitle
+          pageTitle={
+            <Trans>
+              Migrate from {market.marketTitle} V2 to {market.marketTitle} V3
+            </Trans>
+          }
+        />
       }
     />
   );
