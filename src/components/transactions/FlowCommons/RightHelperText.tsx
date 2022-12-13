1d76a697bd8cd9201394dd6302d44309ad2face5
Andrew Schmidt <andrew@aave.com>
2022-12-13 09:14:58 -0600

    feat: add approval preference button

--- src/components/transactions/FlowCommons/RightHelperText.tsx
+++ src/components/transactions/FlowCommons/RightHelperText.tsx
@@ -1,12 +1,13 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline';
-import { CogIcon } from '@heroicons/react/solid';
 import { Trans } from '@lingui/macro';
 import { Box, Link, SvgIcon, Typography } from '@mui/material';
+import { ReactNode } from 'react';
 import { MOCK_SIGNED_HASH } from 'src/helpers/useTransactionHandler';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 
 export type RightHelperTextProps = {
   approvalHash?: string;
+  approvalMethodToggleButton: ReactNode;
 };
 
 const ExtLinkIcon = () => (
@@ -15,46 +16,46 @@ const ExtLinkIcon = () => (
   </SvgIcon>
 );
 
-export const RightHelperText = ({ approvalHash }: RightHelperTextProps) => {
+export const RightHelperText = ({
+  approvalHash,
+  approvalMethodToggleButton,
+}: RightHelperTextProps) => {
   const { currentNetworkConfig } = useProtocolDataContext();
   const isSigned = approvalHash === MOCK_SIGNED_HASH;
-  // a signature will not be reviewable on etherscan
+  // a signature is not submitted on-chain so there is no link to review
   if (!approvalHash || isSigned)
     return (
       <Box sx={{ display: 'inline-flex', alignItems: 'center', mb: 2 }}>
         <Typography variant="subheader2" color="text.secondary">
           <Trans>Approve with</Trans>&nbsp;
         </Typography>
-        <Typography variant="subheader2" color="info.main">
-          <Trans>Signed message</Trans>
-        </Typography>
-        <SvgIcon sx={{ fontSize: 14, ml: 1, color: 'info.main' }}>
-          <CogIcon />
-        </SvgIcon>
+        {approvalMethodToggleButton}
       </Box>
     );
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-      }}
-    >
-      {approvalHash && (
-        <Link
-          variant="helperText"
-          href={currentNetworkConfig.explorerLinkBuilder({ tx: approvalHash })}
-          sx={{ display: 'inline-flex', alignItems: 'center' }}
-          underline="hover"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <Trans>Review approval tx details</Trans>
-          <ExtLinkIcon />
-        </Link>
-      )}
-    </Box>
-  );
+  if (approvalHash)
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+      >
+        {approvalHash && (
+          <Link
+            variant="helperText"
+            href={currentNetworkConfig.explorerLinkBuilder({ tx: approvalHash })}
+            sx={{ display: 'inline-flex', alignItems: 'center' }}
+            underline="hover"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Trans>Review approval tx details</Trans>
+            <ExtLinkIcon />
+          </Link>
+        )}
+      </Box>
+    );
+  return <></>;
 };
