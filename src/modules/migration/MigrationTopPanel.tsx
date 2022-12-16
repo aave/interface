a6c62da430a4974a6ea49b116155ff3c6f1da2ee
Andrew Schmidt <andrew@aave.com>
2022-12-15 23:47:30 -0600

    feat: back button for migration

--- src/modules/migration/MigrationTopPanel.tsx
+++ src/modules/migration/MigrationTopPanel.tsx
@@ -1,4 +1,7 @@
 import { Trans } from '@lingui/macro';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackOutlined';
+import { Box, Button, SvgIcon, useMediaQuery, useTheme } from '@mui/material';
+import { useRouter } from 'next/router';
 import { PageTitle } from 'src/components/TopInfoPanel/PageTitle';
 import { TopInfoPanel } from 'src/components/TopInfoPanel/TopInfoPanel';
 
@@ -6,20 +9,54 @@ import { getMarketInfoById } from '../../components/MarketSwitcher';
 import { useProtocolDataContext } from '../../hooks/useProtocolDataContext';
 
 export const MigrationTopPanel = () => {
+  const router = useRouter();
   const { currentMarket } = useProtocolDataContext();
   const { market } = getMarketInfoById(currentMarket);
 
+  const theme = useTheme();
+  const downToSM = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <TopInfoPanel
-      pageTitle={<></>}
       titleComponent={
-        <PageTitle
-          pageTitle={
-            <Trans>
-              Migrate from {market.marketTitle} V2 to {market.marketTitle} V3
-            </Trans>
-          }
-        />
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: downToSM ? 'flex-start' : 'center',
+              alignSelf: downToSM ? 'flex-start' : 'center',
+              mb: 8,
+              minHeight: '40px',
+              flexDirection: downToSM ? 'column' : 'row',
+            }}
+          >
+            <Button
+              variant="surface"
+              size="medium"
+              color="primary"
+              startIcon={
+                <SvgIcon sx={{ fontSize: '20px' }}>
+                  <ArrowBackRoundedIcon />
+                </SvgIcon>
+              }
+              onClick={() => {
+                // https://github.com/vercel/next.js/discussions/34980
+                if (history.state.idx !== 0) router.back();
+                else router.push('/markets');
+              }}
+              sx={{ mr: 3, mb: downToSM ? '24px' : '0' }}
+            >
+              <Trans>Go Back</Trans>
+            </Button>
+          </Box>
+          <PageTitle
+            pageTitle={
+              <Trans>
+                Migrate from {market.marketTitle} V2 to {market.marketTitle} V3
+              </Trans>
+            }
+          />
+        </Box>
       }
     />
   );
