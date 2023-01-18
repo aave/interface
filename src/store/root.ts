74bd1aca6203ed31c446e94a3a5dd4897f6ae7bc
Mark Grothe <grothem@gmail.com>
2023-01-18 11:16:55 -0600

    feat: permissioned faucets (#1359)
    * feat: captcha faucet
    
    * fix: env example
    
    * feat: new market, send up faucet address to api
    
    * feat: configure new markets
    
    * feat: send up token symbol
    
    * feat: new ui for captcha faucet
    
    * chore: new markets
    
    * feat: captcha faucet modal
    
    * fix: goerli facuet config
    
    * fix: error handling
    
    * fix: auto refresh token
    
    * fix: updated env types
    
    * ci: add turnstile key
    
    * fix: cleanup reused code
    
    * fix: updated wording in success modal
    
    * chore: i18n
    
    * refactor: tx success view
    
    * fix: remove txHash prop
    
    * feat: added v3.0.1 market, use new flag to determine which faucet to use
    
    * feat: handle all types of faucets
    
    * fix: don't use modal wrapper for permissioned faucets
    
    * fix: use latest temp build of utils, fixed race condtion with multiple calls in flight
    
    * feat: udated UiPoolDataProvider contracts
    
    * fix: v2 market config
    
    * fix: updated ui pool data provider for goerli v3
    
    * fix: updated v2 goerli, removed console log
    
    * fix: update prod markets with new data provider contracts
    
    * fix: better error handling
    
    * fix: updated v3 markets
    
    * fix: op v3 testnet
    
    * feat: goerli market config
    
    * fix: add site key to env
    
    * fix: updated config for prod v2 markets
    
    * fix: market config
    
    * chore: use latest utils packages
    
    * Fix tests
    
    * fix: temporaily disalbe pokt rpcs for testing
    
    * fix: attribute name
    
    * fix: test e-mode
    
    * fix: add back in pokt rpcs
    
    * fix: remove dependency
    
    Co-authored-by: Vladimir Yumatov <vladimir@aave.com>
    Co-authored-by: Mark Hinschberger <foodaka@users.noreply.github.com>
    Co-authored-by: bojank93 <bojan@aave.com>
    Co-authored-by: NikitaY <right2maresko@gmail.com>

--- src/store/root.ts
+++ src/store/root.ts
@@ -1,7 +1,9 @@
+import { V3FaucetService } from '@aave/contract-helpers';
 import { enableMapSet } from 'immer';
 import { CustomMarket } from 'src/ui-config/marketsConfig';
+import { ENABLE_TESTNET, STAGING_ENV } from 'src/utils/marketsAndNetworksConfig';
 import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, subscribeWithSelector } from 'zustand/middleware';
 
 import { createGovernanceSlice, GovernanceSlice } from './governanceSlice';
 import { createIncentiveSlice, IncentiveSlice } from './incentiveSlice';
@@ -22,16 +24,18 @@ export type RootStore = StakeSlice &
   GovernanceSlice;
 
 export const useRootStore = create<RootStore>()(
-  devtools((...args) => {
-    return {
-      ...createStakeSlice(...args),
-      ...createProtocolDataSlice(...args),
-      ...createWalletSlice(...args),
-      ...createPoolSlice(...args),
-      ...createIncentiveSlice(...args),
-      ...createGovernanceSlice(...args),
-    };
-  })
+  subscribeWithSelector(
+    devtools((...args) => {
+      return {
+        ...createStakeSlice(...args),
+        ...createProtocolDataSlice(...args),
+        ...createWalletSlice(...args),
+        ...createPoolSlice(...args),
+        ...createIncentiveSlice(...args),
+        ...createGovernanceSlice(...args),
+      };
+    })
+  )
 );
 
 // hydrate state from localeStorage to not break on ssr issues
@@ -71,3 +75,38 @@ export const useIncentiveDataSubscription = createSingletonSubscriber(() => {
 export const useGovernanceDataSubscription = createSingletonSubscriber(() => {
   return useRootStore.getState().refreshGovernanceData();
 }, 60000);
+
+let latest: V3FaucetService;
+useRootStore.subscribe(
+  (state) => state.currentMarketData,
+  async (selected) => {
+    const { setIsFaucetPermissioned: setFaucetPermissioned, jsonRpcProvider } =
+      useRootStore.getState();
+    if (ENABLE_TESTNET || STAGING_ENV) {
+      if (!selected.v3) {
+        setFaucetPermissioned(false);
+        return;
+      }
+
+      // If there are multiple calls in flight, we only want to use the result from the latest one.
+      // Use the instance of the service to check if it's the latest one since it is recreated
+      // everytime this subscription fires.
+      const service = new V3FaucetService(jsonRpcProvider(), selected.addresses.FAUCET);
+      latest = service;
+      service
+        .isPermissioned()
+        .then((isPermissioned) => {
+          if (latest === service) {
+            setFaucetPermissioned(isPermissioned);
+          }
+        })
+        .catch((e) => {
+          console.error('error checking faucet permission', e);
+          setFaucetPermissioned(false);
+        });
+    } else {
+      setFaucetPermissioned(false);
+    }
+  },
+  { fireImmediately: true }
+);
