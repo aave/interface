daab5e134443a4dfb2e73568e2a850928ac0c48c
Andrew Schmidt <andrew@aave.com>
2023-01-24 18:39:51 -0600

    chore: default to mainnet V3 on first load

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -44,16 +44,15 @@ export type MarketDataType = {
 };
 
 export enum CustomMarket {
-  // v3 test networks
+  // v3 test networks, all v3.0.1 with permissioned faucet
   proto_arbitrum_goerli_v3 = 'proto_arbitrum_goerli_v3',
   proto_mumbai_v3 = 'proto_mumbai_v3',
   proto_fantom_testnet_v3 = 'proto_fantom_testnet_v3',
   proto_fuji_v3 = 'proto_fuji_v3',
   proto_goerli_v3 = 'proto_goerli_v3',
   proto_optimism_goerli_v3 = 'proto_optimism_goerli_v3',
-  // v3.0.1 mainnets
-  proto_mainnet_v3_1 = 'proto_mainnet_v3_1',
   // v3 mainnets
+  proto_mainnet_v3 = 'proto_mainnet_v3',
   proto_optimism_v3 = 'proto_optimism_v3',
   proto_fantom_v3 = 'proto_fantom_v3',
   proto_harmony_v3 = 'proto_harmony_v3',
@@ -75,33 +74,7 @@ export enum CustomMarket {
 export const marketsData: {
   [key in keyof typeof CustomMarket]: MarketDataType;
 } = {
-  [CustomMarket.proto_mainnet]: {
-    marketTitle: 'Ethereum',
-    chainId: ChainId.mainnet,
-    enabledFeatures: {
-      governance: true,
-      staking: true,
-      liquiditySwap: true,
-      collateralRepay: true,
-      incentives: true,
-    },
-    addresses: {
-      LENDING_POOL_ADDRESS_PROVIDER: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5'.toLowerCase(),
-      LENDING_POOL: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
-      WETH_GATEWAY: '0xEFFC18fC3b7eb8E676dac549E0c693ad50D1Ce31',
-      REPAY_WITH_COLLATERAL_ADAPTER: '0x80Aca0C645fEdABaa20fd2Bf0Daf57885A309FE6',
-      SWAP_COLLATERAL_ADAPTER: '0x135896DE8421be2ec868E0b811006171D9df802A',
-      WALLET_BALANCE_PROVIDER: '0x8E8dAd5409E0263a51C0aB5055dA66Be28cFF922',
-      UI_POOL_DATA_PROVIDER: '0x00e50FAB64eBB37b87df06Aa46b8B35d5f1A4e1A',
-      UI_INCENTIVE_DATA_PROVIDER: '0xD01ab9a6577E1D84F142e44D49380e23A340387d',
-      COLLECTOR: '0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c',
-    },
-    halIntegration: {
-      URL: 'https://app.hal.xyz/recipes/aave-track-your-health-factor',
-      marketName: 'aavev2',
-    },
-  },
-  [CustomMarket.proto_mainnet_v3_1]: {
+  [CustomMarket.proto_mainnet_v3]: {
     marketTitle: 'Ethereum',
     chainId: ChainId.mainnet,
     v3: true,
@@ -127,6 +100,32 @@ export const marketsData: {
     //   URL: 'https://app.hal.xyz/recipes/aave-track-your-health-factor',
     //   marketName: 'aavev3',
     // },
+  },
+  [CustomMarket.proto_mainnet]: {
+    marketTitle: 'Ethereum',
+    chainId: ChainId.mainnet,
+    enabledFeatures: {
+      governance: true,
+      staking: true,
+      liquiditySwap: true,
+      collateralRepay: true,
+      incentives: true,
+    },
+    addresses: {
+      LENDING_POOL_ADDRESS_PROVIDER: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5'.toLowerCase(),
+      LENDING_POOL: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
+      WETH_GATEWAY: '0xEFFC18fC3b7eb8E676dac549E0c693ad50D1Ce31',
+      REPAY_WITH_COLLATERAL_ADAPTER: '0x80Aca0C645fEdABaa20fd2Bf0Daf57885A309FE6',
+      SWAP_COLLATERAL_ADAPTER: '0x135896DE8421be2ec868E0b811006171D9df802A',
+      WALLET_BALANCE_PROVIDER: '0x8E8dAd5409E0263a51C0aB5055dA66Be28cFF922',
+      UI_POOL_DATA_PROVIDER: '0x00e50FAB64eBB37b87df06Aa46b8B35d5f1A4e1A',
+      UI_INCENTIVE_DATA_PROVIDER: '0xD01ab9a6577E1D84F142e44D49380e23A340387d',
+      COLLECTOR: '0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c',
+    },
+    halIntegration: {
+      URL: 'https://app.hal.xyz/recipes/aave-track-your-health-factor',
+      marketName: 'aavev2',
+    },
   },
   // [CustomMarket.permissioned_market]: {
   //   marketTitle: 'Ethereum Permissioned Market example',
