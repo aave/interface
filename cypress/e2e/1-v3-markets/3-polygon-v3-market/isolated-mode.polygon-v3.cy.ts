b683dd5407161df2f12c256962116f3cbd349ba3
nikita <right2maresko@gmail.com>
2023-01-30 19:09:36 +0000

    fix: skip unstable tests for polygon

--- cypress/e2e/1-v3-markets/3-polygon-v3-market/isolated-mode.polygon-v3.cy.ts
+++ cypress/e2e/1-v3-markets/3-polygon-v3-market/isolated-mode.polygon-v3.cy.ts
@@ -77,7 +77,8 @@ const testData = {
   ],
 };
 
-describe('ISOLATED MODE SPEC, POLYGON V3 MARKET', () => {
+//skip due unstable swap and polygon at all
+describe.skip('ISOLATED MODE SPEC, POLYGON V3 MARKET', () => {
   const skipTestState = skipState(false);
   configEnvWithTenderlyPolygonFork({ market: 'fork_proto_polygon_v3', v3: true });
   describe('Get isolated asset', () => {
