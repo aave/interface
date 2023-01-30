2492906bf0a7b24369e227f11afc24993d6e84f0
NikitaY <right2maresko@gmail.com>
2023-01-30 15:58:07 +0000

    fix: test coverage for stake and polygon isolated mode

--- cypress/e2e/1-v3-markets/3-polygon-v3-market/isolated-mode.polygon-v3.cy.ts
+++ cypress/e2e/1-v3-markets/3-polygon-v3-market/isolated-mode.polygon-v3.cy.ts
@@ -15,7 +15,7 @@ const testData = {
   testCases: {
     depositMATIC: {
       asset: assets.polygonV3Market.MATIC,
-      amount: 50,
+      amount: 100,
       hasApproval: true,
     },
     swapMATIC: {
