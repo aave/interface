af0c82671572702b98e66b250c0fdc32c9b81c07
DeFi Spartan <andrew@aave.com>
2023-02-02 11:49:01 -0600

    feat: remove individual config and default to enable permit for all v3 testnet assets (#1457)
    * feat: remove individual config and default to enable permit for all v3 testnet assets
    
    * fix: add special case for wmatic permit disable

--- src/store/protocolDataSlice.ts
+++ src/store/protocolDataSlice.ts
@@ -54,14 +54,22 @@ export const createProtocolDataSlice: StateCreator<
     tryPermit: (reserveAddress: string) => {
       const currentNetworkConfig = get().currentNetworkConfig;
       const currentMarketData = get().currentMarketData;
+      // current chain id, or underlying chain id for fork networks
       const underlyingChainId = currentNetworkConfig.isFork
         ? currentNetworkConfig.underlyingChainId
         : currentMarketData.chainId;
-      const tryPermit =
+      // enable permit for all v3 test network assets or v3 production assets included in permitConfig)
+      const testnetPermitEnabled = Boolean(
         currentMarketData.v3 &&
-        underlyingChainId &&
-        permitByChainAndToken[underlyingChainId]?.[utils.getAddress(reserveAddress).toLowerCase()];
-      return Boolean(tryPermit);
+          currentNetworkConfig.isTestnet &&
+          reserveAddress.toLowerCase() !== '0xb685400156cf3cbe8725958deaa61436727a30c3' // WMATIC on Mumbai is a special case
+      );
+      const productionPermitEnabled = Boolean(
+        currentMarketData.v3 &&
+          underlyingChainId &&
+          permitByChainAndToken[underlyingChainId]?.[utils.getAddress(reserveAddress).toLowerCase()]
+      );
+      return testnetPermitEnabled || productionPermitEnabled;
     },
   };
 };
