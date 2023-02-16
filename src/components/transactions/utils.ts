d7c3f4c32a81f2aaf5485a0102070381996d3685
Mark Grothe <grothem@gmail.com>
2023-02-16 14:58:53 -0600

    chore: cleanup

--- src/components/transactions/utils.ts
+++ src/components/transactions/utils.ts
@@ -12,8 +12,3 @@ export const useFlashloan = (healthFactor: string, hfEffectOfFromAmount: string)
     new BigNumber(healthFactor).minus(new BigNumber(hfEffectOfFromAmount)).lt('1.05')
   );
 };
-
-// The stETH contract has a bug where there is a 2gwei precision error when
-// calling balanceOf() making it impossible to flashlon this asset.
-// const stETHAddress = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84';
-// const stETHChainId = ChainId.mainnet;
