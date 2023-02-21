05f9eba7735cbbf29eaa8817af0efda31054b087
Joaquin Battilana <battilanajn@gmail.com>
2023-02-21 15:17:22 +0000

    feat: added tooltips to voting and proposition power titles

--- src/modules/governance/VotingPowerInfoPanel.tsx
+++ src/modules/governance/VotingPowerInfoPanel.tsx
@@ -2,6 +2,7 @@ import { Trans } from '@lingui/macro';
 import { Box, Typography } from '@mui/material';
 import { AvatarSize } from 'src/components/Avatar';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
+import { TextWithTooltip } from 'src/components/TextWithTooltip';
 import { UserDisplay } from 'src/components/UserDisplay';
 import { useVotingPower } from 'src/hooks/governance-data-provider/useVotingPower';
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
@@ -21,15 +22,34 @@ export function VotingPowerInfoPanel() {
       {currentAccount && (
         <Box sx={{ display: 'flex', mt: 6, justifyContent: 'space-between' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Typography variant="description">
-              <Trans>Voting power</Trans>
-            </Typography>
+            <TextWithTooltip text="Voting power" variant="description">
+              <>
+                <p>
+                  <Trans>
+                    Total voting power based on your AAVE/stkAAVE balance and received delegations.
+                  </Trans>
+                </p>
+                <p>
+                  <Trans>Use it to vote for or against active proposals.</Trans>
+                </p>
+              </>
+            </TextWithTooltip>
             <FormattedNumber value={powers?.votingPower || 0} variant="h2" visibleDecimals={2} />
           </Box>
           <Box>
-            <Typography variant="description">
-              <Trans>Proposition power</Trans>
-            </Typography>
+            <TextWithTooltip text="Proposition power" variant="description">
+              <>
+                <p>
+                  <Trans>
+                    Total proposition power based on your AAVE/stkAAVE balance and received
+                    delegations.
+                  </Trans>
+                </p>
+                <p>
+                  <Trans>You need at least 80.00K power to submit a proposal.</Trans>
+                </p>
+              </>
+            </TextWithTooltip>
             <FormattedNumber
               value={powers?.propositionPower || 0}
               variant="h2"
