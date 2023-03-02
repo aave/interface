7888e6cc8bbec7b955e6ad08f31e590b94e082cd
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-03-02 16:25:18 +0000

    feat: addresses for sepolia

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -56,6 +56,7 @@ export enum CustomMarket {
   proto_fuji_v3 = 'proto_fuji_v3',
   proto_goerli_v3 = 'proto_goerli_v3',
   proto_optimism_goerli_v3 = 'proto_optimism_goerli_v3',
+  proto_sepolia_v3 = 'proto_sepolia_v3',
   // v3 mainnets
   proto_mainnet_v3 = 'proto_mainnet_v3',
   proto_optimism_v3 = 'proto_optimism_v3',
@@ -235,6 +236,23 @@ export const marketsData: {
       WALLET_BALANCE_PROVIDER: markets.AaveV3Goerli.WALLET_BALANCE_PROVIDER,
       UI_POOL_DATA_PROVIDER: markets.AaveV3Goerli.UI_POOL_DATA_PROVIDER,
       UI_INCENTIVE_DATA_PROVIDER: markets.AaveV3Goerli.UI_INCENTIVE_DATA_PROVIDER,
+    },
+  },
+  [CustomMarket.proto_sepolia_v3]: {
+    marketTitle: 'Ethereum Sepolia',
+    v3: true,
+    chainId: '11155111',
+    enabledFeatures: {
+      faucet: true,
+    },
+    addresses: {
+      LENDING_POOL_ADDRESS_PROVIDER: '0x0496275d34753A48320CA58103d5220d394FF77F', // markets.AaveV3Sepolia.POOL_ADDRESSES_PROVIDER,
+      LENDING_POOL: '0xE7EC1B0015eb2ADEedb1B7f9F1Ce82F9DAD6dF08', // markets.AaveV3Sepolia.POOL,
+      WETH_GATEWAY: '0xe70cDC67C91d5519DD4682cA162E40480773255a', // markets.AaveV3Sepolia.WETH_GATEWAY,
+      FAUCET: '0x1f885520b7BD528E46b390040F12E753Dce43004', // markets.AaveV3Sepolia.FAUCET,
+      WALLET_BALANCE_PROVIDER: '0x4f04218ECC7D2756f84836D2DC267293504cAbCc', // markets.AaveV3Sepolia.WALLET_BALANCE_PROVIDER,
+      UI_POOL_DATA_PROVIDER: '0x7cE1062e0688BC3D7699987A38bba84353F69881', // markets.AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
+      UI_INCENTIVE_DATA_PROVIDER: '0x7cE1062e0688BC3D7699987A38bba84353F69881', // markets.AaveV3Sepolia.UI_INCENTIVE_DATA_PROVIDER,
     },
   },
   [CustomMarket.proto_arbitrum_v3]: {
