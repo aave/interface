6d88755cb4d64811500adf4fd372dd9451edc472
DeFi Spartan <andrew@aave.com>
2023-03-16 13:26:44 -0500

    fix: price impact calculations (#1530)
    * fix: decimals of price impact percentage
    
    * fix: use correct percent change formula
    
    * fix: use correct percent change formula and exact output amount

--- src/components/transactions/Swap/SwapModalContent.tsx
+++ src/components/transactions/Swap/SwapModalContent.tsx
@@ -165,10 +165,14 @@ export const SwapModalContent = ({
     poolReserve.reserveLiquidationThreshold !== '0';
 
   // calculate impact based on $ difference
-  const priceImpact =
-    outputAmountUSD && outputAmountUSD !== '0'
-      ? new BigNumber(1).minus(new BigNumber(inputAmountUSD).dividedBy(outputAmountUSD)).toFixed(2)
+  const priceDifference: BigNumber = new BigNumber(outputAmountUSD).minus(inputAmountUSD);
+  let priceImpact =
+    inputAmountUSD && inputAmountUSD !== '0'
+      ? priceDifference.dividedBy(inputAmountUSD).times(100).toFixed(2)
       : '0';
+  if (priceImpact === '-0.00') {
+    priceImpact = '0.00';
+  }
 
   return (
     <>
