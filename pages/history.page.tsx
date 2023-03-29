9823f4c846b0d15a74f5ddbdd4b8df7e0994d81f
aschmidt20 <andrew@aave.com>
2023-03-29 01:21:56 -0500

    fix: lint on history page build

--- pages/history.page.tsx
+++ pages/history.page.tsx
@@ -1,29 +1,23 @@
 import * as React from 'react';
+import { ContentContainer } from 'src/components/ContentContainer';
+import { useTransactionHistory } from 'src/hooks/useTransactionHistory';
 import { MainLayout } from 'src/layouts/MainLayout';
 import { HistoryTopPanel } from 'src/modules/history/HistoryTopPanel';
-
-import { useTransactionHistory } from 'src/hooks/useTransactionHistory';
 import TransactionsList from 'src/modules/history/TransactionsList';
-import { ContentContainer } from '../src/components/ContentContainer';
 
 export default function History() {
-    const { data: transactions, isLoading } = useTransactionHistory();
+  const { data: transactions, isLoading } = useTransactionHistory();
 
-    return (
-        <>
-            <HistoryTopPanel />
-            <ContentContainer>
-                <TransactionsList transactions={transactions} loading={isLoading} />
-            </ContentContainer>
-        </>
-    );
+  return (
+    <>
+      <HistoryTopPanel />
+      <ContentContainer>
+        <TransactionsList transactions={transactions} loading={isLoading} />
+      </ContentContainer>
+    </>
+  );
 }
 
 History.getLayout = function getLayout(page: React.ReactElement) {
-    return (
-        <MainLayout>
-            {page}
-        </MainLayout>
-    );
+  return <MainLayout>{page}</MainLayout>;
 };
-
