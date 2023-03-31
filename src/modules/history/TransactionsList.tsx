7ac68bc62e82fa40ac385b1ed2503498a1074a5b
aschmidt20 <andrew@aave.com>
2023-03-30 22:22:37 -0500

    feat: default and loading state on tx history page

--- src/modules/history/TransactionsList.tsx
+++ src/modules/history/TransactionsList.tsx
@@ -1,12 +1,14 @@
 import { Trans } from '@lingui/macro';
 import { Typography, useMediaQuery, useTheme } from '@mui/material';
 import * as React from 'react';
+import { ConnectWalletPaper } from 'src/components/ConnectWalletPaper';
 import { ListColumn } from 'src/components/lists/ListColumn';
 import { ListHeaderTitle } from 'src/components/lists/ListHeaderTitle';
 import { ListHeaderWrapper } from 'src/components/lists/ListHeaderWrapper';
 import { ListItem } from 'src/components/lists/ListItem';
 import { ListWrapper } from 'src/components/lists/ListWrapper';
 import { Link } from 'src/components/primitives/Link';
+import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
 import { useRootStore } from 'src/store/root';
 
 import { FaucetItemLoader } from '../faucet/FaucetItemLoader';
@@ -27,6 +29,16 @@ export default function TransactionsAssetsList({
   const theme = useTheme();
   const downToXSM = useMediaQuery(theme.breakpoints.down('xsm'));
   const { currentNetworkConfig } = useRootStore();
+  const { currentAccount, loading: web3Loading } = useWeb3Context();
+
+  if (!currentAccount || loading) {
+    return (
+      <ConnectWalletPaper
+        loading={web3Loading || loading}
+        description={<Trans> Please connect your wallet to view transaction history.</Trans>}
+      />
+    );
+  }
 
   return (
     <ListWrapper
