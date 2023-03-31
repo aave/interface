c5dbcb8f17ae85b902b34e0b3fb98fd80f2c2287
Mark Grothe <grothem@gmail.com>
2023-03-31 06:27:46 -0500

    fix: collateral swap hf and swap target collateralization details (#1534)
    * fix: check for isNaN on future hf number
    
    * fix: use asset collateral type in swap modal details
    
    * fix: updated collateral states with latest design
    
    * fix: collateral states
    
    * fix: handle max amount swap cases
    
    * test: skip wbtc for optimis due max caps on market
    
    * fix: design feedback
    
    * fix: typo
    
    * fix: tooltip text for isolated enabled assets
    
    * fix: build error
    
    * test: fix coverage
    
    * test: fix avalacnhe test coverage
    
    * test: improve waiters for e2e coverage
    
    * test: ci coverage fix
    
    * test: ci coverage fix
    
    ---------
    
    Co-authored-by: NikitaY <right2maresko@gmail.com>

--- src/components/transactions/Swap/SwapModalContent.tsx
+++ src/components/transactions/Swap/SwapModalContent.tsx
@@ -8,7 +8,9 @@ import { Warning } from 'src/components/primitives/Warning';
 import { Asset, AssetInput } from 'src/components/transactions/AssetInput';
 import { TxModalDetails } from 'src/components/transactions/FlowCommons/TxModalDetails';
 import { StETHCollateralWarning } from 'src/components/Warnings/StETHCollateralWarning';
+import { CollateralType } from 'src/helpers/types';
 import { useCollateralSwap } from 'src/hooks/paraswap/useCollateralSwap';
+import { getDebtCeilingData } from 'src/hooks/useAssetCaps';
 import { useModalContext } from 'src/hooks/useModal';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
@@ -23,7 +25,7 @@ import {
 } from '../../../hooks/app-data-provider/useAppDataProvider';
 import { ModalWrapperProps } from '../FlowCommons/ModalWrapper';
 import { TxSuccessView } from '../FlowCommons/Success';
-import { ErrorType, useFlashloan, zeroLTVBlockingWithdraw } from '../utils';
+import { ErrorType, getAssetCollateralType, useFlashloan, zeroLTVBlockingWithdraw } from '../utils';
 import { ParaswapErrorDisplay } from '../Warnings/ParaswapErrorDisplay';
 import { SwapActions } from './SwapActions';
 import { SwapModalDetails } from './SwapModalDetails';
@@ -174,6 +176,56 @@ export const SwapModalContent = ({
     priceImpact = '0.00';
   }
 
+  const { debtCeilingReached: sourceDebtCeiling } = getDebtCeilingData(swapTarget.reserve);
+  const swapSourceCollateralType = getAssetCollateralType(
+    userReserve,
+    user.totalCollateralUSD,
+    user.isInIsolationMode,
+    sourceDebtCeiling
+  );
+
+  const { debtCeilingReached: targetDebtCeiling } = getDebtCeilingData(swapTarget.reserve);
+  let swapTargetCollateralType = getAssetCollateralType(
+    swapTarget,
+    user.totalCollateralUSD,
+    user.isInIsolationMode,
+    targetDebtCeiling
+  );
+
+  // If the user is swapping all of their isolated asset to an asset that is not supplied,
+  // then the swap target will be enabled as collateral as part of the swap.
+  if (
+    isMaxSelected &&
+    swapSourceCollateralType === CollateralType.ISOLATED_ENABLED &&
+    swapTarget.underlyingBalance === '0'
+  ) {
+    if (swapTarget.reserve.isIsolated) {
+      swapTargetCollateralType = CollateralType.ISOLATED_ENABLED;
+    } else {
+      swapTargetCollateralType = CollateralType.ENABLED;
+    }
+  }
+
+  // If the user is swapping all of their enabled asset to an isolated asset that is not supplied,
+  // and no other supplied assets are being used as collateral,
+  // then the swap target will be enabled as collateral and the user will be in isolation mode.
+  if (
+    isMaxSelected &&
+    swapSourceCollateralType === CollateralType.ENABLED &&
+    swapTarget.underlyingBalance === '0'
+  ) {
+    const reservesAsCollateral = user.userReservesData.filter(
+      (r) => r.usageAsCollateralEnabledOnUser
+    );
+
+    if (
+      reservesAsCollateral.length === 1 &&
+      reservesAsCollateral[0].underlyingAsset === userReserve.underlyingAsset
+    ) {
+      swapTargetCollateralType = CollateralType.ISOLATED_ENABLED;
+    }
+  }
+
   return (
     <>
       {/* {showIsolationWarning && (
@@ -242,8 +294,8 @@ export const SwapModalContent = ({
           showHealthFactor={showHealthFactor}
           healthFactor={user?.healthFactor}
           healthFactorAfterSwap={hfAfterSwap.toString(10)}
-          swapSource={userReserve}
-          swapTarget={swapTarget}
+          swapSource={{ ...userReserve, collateralType: swapSourceCollateralType }}
+          swapTarget={{ ...swapTarget, collateralType: swapTargetCollateralType }}
           toAmount={outputAmount}
           fromAmount={amount === '' ? '0' : amount}
           loading={loadingSkeleton}
