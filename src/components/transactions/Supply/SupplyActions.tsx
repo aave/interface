01bbb76ece164d6e93c102abd05abbb4a87abb0d
aschmidt20 <andrew@aave.com>
2023-04-12 00:00:52 -0500

    chore: generalize requires approval helper

--- src/components/transactions/Supply/SupplyActions.tsx
+++ src/components/transactions/Supply/SupplyActions.tsx
@@ -84,7 +84,7 @@ export const SupplyActions = React.memo(
         if (approvedAmount) {
           const fetchedRequiresApproval = checkRequiresApproval({
             approvedAmount: approvedAmount.amount,
-            amountToSupply,
+            amount: amountToSupply,
             signedAmount: signatureParams ? signatureParams.amount : '0',
           });
           setRequiresApproval(fetchedRequiresApproval);
