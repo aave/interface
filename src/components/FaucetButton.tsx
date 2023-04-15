9f7db31b8139b8c5bb967df3ed74abe49e90e587
Josh DeCristi <jdecristi777a@gmail.com>
2023-04-14 20:44:35 -0400

    fix: replace in FaucetButton

--- src/components/FaucetButton.tsx
+++ src/components/FaucetButton.tsx
@@ -1,17 +1,17 @@
-import { ExternalLinkIcon } from '@heroicons/react/outline';
 import { Trans } from '@lingui/macro';
-import { Button, SvgIcon, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 
+import ExternalLinkButton from './ExternalLinkButton';
 import { DarkTooltip } from './infoTooltips/DarkTooltip';
-import { Link, ROUTES } from './primitives/Link';
+import { ROUTES } from './primitives/Link';
 
 export const FaucetButton = () => {
   const { currentNetworkConfig } = useProtocolDataContext();
 
   return (
     <DarkTooltip title="Get free assets to test the Aave Protocol">
-      <Button
+      <ExternalLinkButton
         startIcon={
           <img
             src={currentNetworkConfig.networkLogoPath}
@@ -19,20 +19,13 @@ export const FaucetButton = () => {
             style={{ width: 14, height: 14 }}
           />
         }
-        endIcon={
-          <SvgIcon sx={{ width: 14, height: 14 }}>
-            <ExternalLinkIcon />
-          </SvgIcon>
-        }
-        component={Link}
         href={ROUTES.faucet}
-        variant="outlined"
         size="small"
       >
         <Typography variant="buttonS">
           <Trans>{currentNetworkConfig.name} Faucet</Trans>
         </Typography>
-      </Button>
+      </ExternalLinkButton>
     </DarkTooltip>
   );
 };
