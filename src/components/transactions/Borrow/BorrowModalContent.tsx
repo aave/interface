18da26f41161dc224d37e6a16e2b7d041a751b7f
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-05-03 10:21:03 +0100

    chore: remove debug

--- src/components/transactions/Borrow/BorrowModalContent.tsx
+++ src/components/transactions/Borrow/BorrowModalContent.tsx
@@ -254,7 +254,7 @@ export const BorrowModalContent = ({
         capType={CapType.borrowCap}
         isMaxSelected={isMaxSelected}
         maxValue={maxAmountToBorrow}
-        balanceText={<Trans>Available sdss</Trans>}
+        balanceText={<Trans>Available</Trans>}
         event={{
           eventName: BORROW_MODAL.MAX_BORROW,
           eventParams: {
