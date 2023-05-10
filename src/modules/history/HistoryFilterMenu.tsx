74620b1d2ab51d56c7311c7c4e009dc1f3bd3ca0
aschmidt20 <andrew@aave.com>
2023-05-10 01:16:46 -0500

    feat: progress on filter menu styling

--- src/modules/history/HistoryFilterMenu.tsx
+++ src/modules/history/HistoryFilterMenu.tsx
@@ -1,3 +1,4 @@
+import { XCircleIcon } from '@heroicons/react/solid';
 import { Trans } from '@lingui/macro';
 import { Check as CheckIcon, Sort as SortIcon } from '@mui/icons-material';
 import { Box, Button, Divider, Menu, MenuItem, SvgIcon, Typography, useTheme } from '@mui/material';
@@ -72,41 +73,71 @@ export const HistoryFilterMenu: React.FC<HistoryFilterMenuProps> = ({ onFilterCh
     if (selectedFilter.length === 0) {
       return <Trans>All transactions</Trans>;
     } else {
-      return <Trans>Borrow</Trans>;
+      const displayLimit = 3;
+      const hiddenCount = selectedFilter.length - displayLimit;
+      const displayedFilters = selectedFilter.slice(0, displayLimit).map((filter) => (
+        <React.Fragment key={filter}>
+          <FilterLabel filter={filter} />
+          {filter !== selectedFilter[displayLimit - 1] && ', '}
+        </React.Fragment>
+      ));
+
+      return (
+        <React.Fragment>
+          {displayedFilters}
+          {hiddenCount > 0 && <React.Fragment>... (+{hiddenCount})</React.Fragment>}
+        </React.Fragment>
+      );
     }
+  };
 
-    //   const selectedFiltersText = selectedFilters.join(', ');
-    //   const maxWidth = 320;
-    //    const ellipsis = '...';
-    //    const exceededText = selectedFiltersText.length > maxWidth
-    //        ? `${selectedFiltersText.slice(0, maxWidth - ellipsis.length)}${ellipsis}`
-    //        : selectedFiltersText;
-
-    //   return `TXs: ${exceededText}`;
+  const handleClearFilter = () => {
+    setSelectedFilters([]);
+    onFilterChange([]);
   };
 
   return (
     <Box>
       <Button
         sx={{
-          width: 170,
+          width: 148,
+          maxWidth: 320,
           display: 'flex',
-          justifyContent: 'center',
+          justifyContent: 'space-between',
           alignItems: 'center',
           height: 36,
           border: '1px solid',
           borderColor: 'divider',
           borderRadius: '4px',
           mr: 2,
+          pl: 2,
+          pr: 1,
         }}
         onClick={handleClick}
       >
-        <SvgIcon height={10} width={10} color="primary">
-          <SortIcon />
-        </SvgIcon>
-        <Typography variant="subheader1" color="text.primary" sx={{ ml: 2 }}>
-          <FilterButtonLabel />
-        </Typography>
+        <Box display="flex" alignItems="center" overflow="hidden">
+          <SvgIcon height={10} width={10} color="primary">
+            <SortIcon />
+          </SvgIcon>
+          <Typography
+            variant="subheader1"
+            color="text.primary"
+            sx={{ ml: 1, textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}
+          >
+            <FilterButtonLabel />
+          </Typography>
+        </Box>
+        {selectedFilter.length > 0 && (
+          <SvgIcon
+            height={10}
+            width={10}
+            sx={{ cursor: 'pointer' }}
+            color="primary"
+            onClick={handleClearFilter}
+          >
+            <XCircleIcon />
+          </SvgIcon>
+        )}
       </Button>
       <Menu
         anchorEl={anchorEl}
