7111c305b117b4194d6588fd563e227e33a6f735
aschmidt20 <andrew@aave.com>
2023-05-22 12:21:33 -0500

    chore: use userCooldownAmount as max value in UnstakeModalContent

--- src/components/transactions/UnStake/UnStakeModalContent.tsx
+++ src/components/transactions/UnStake/UnStakeModalContent.tsx
@@ -45,7 +45,7 @@ export const UnStakeModalContent = ({ stakeAssetName, icon }: UnStakeProps) => {
   const amountRef = useRef<string>();
 
   const walletBalance = normalize(
-    stakeUserResult?.[stakeAssetName as StakingType].stakeTokenRedeemableAmount || '0',
+    stakeUserResult?.[stakeAssetName as StakingType].userCooldownAmount || '0',
     18
   );
 
