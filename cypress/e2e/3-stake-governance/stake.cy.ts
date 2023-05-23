5aa67ba0c5862d5b95f6396455978bf285f8397d
Nikita <right2maresko@gmail.com>
2023-05-23 12:50:48 +0100

    test: added coverage for recool colldown

--- cypress/e2e/3-stake-governance/stake.cy.ts
+++ cypress/e2e/3-stake-governance/stake.cy.ts
@@ -1,19 +1,26 @@
 import assets from '../../fixtures/assets.json';
 import { skipState } from '../../support/steps/common';
 import { configEnvWithTenderlyMainnetFork } from '../../support/steps/configuration.steps';
-import { activateCooldown, claimReward, stake } from '../../support/steps/stake.steps';
+import {
+  activateCooldown,
+  claimReward,
+  stake,
+  reCallCooldown,
+} from '../../support/steps/stake.steps';
 
 const testCases = [
   {
     asset: assets.staking.AAVE,
-    amount: 1000,
-    checkAmount: '1,000.00',
+    amount: 500,
+    checkAmount: '500.00',
+    checkAmountFinal: '1,000.00',
     tabValue: 'aave',
   },
   {
     asset: assets.staking.ABPT,
-    amount: 1000,
-    checkAmount: '1,000.00',
+    amount: 500,
+    checkAmount: '500.00',
+    checkAmountFinal: '1,000.00',
     tabValue: 'bpt',
   },
 ];
@@ -36,6 +43,7 @@ testCases.forEach(
           amount: testCase.amount,
           checkAmount: testCase.checkAmount,
           tabValue: testCase.tabValue,
+          hasApproval: false,
         },
         skipTestState,
         true
@@ -54,113 +62,24 @@ testCases.forEach(
         skipTestState,
         false
       );
+      stake(
+        {
+          asset: testCase.asset,
+          amount: testCase.amount,
+          checkAmount: testCase.checkAmountFinal,
+          tabValue: testCase.tabValue,
+          hasApproval: true,
+        },
+        skipTestState,
+        true
+      );
+      reCallCooldown(
+        {
+          asset: testCase.asset,
+        },
+        skipTestState,
+        false
+      );
     });
   }
 );
-//
-// import assets from '../../fixtures/assets.json';
-// import { configEnvWithTenderlyMainnetFork } from '../../support/steps/configuration.steps';
-// import { doCloseModal } from '../../support/steps/main.steps';
-// import { claimReward, stake } from "../../support/steps/stake.steps";
-// import { skipState } from "../../support/steps/common";
-//
-// const testCases = [
-//   {
-//     assetName: assets.staking.AAVE,
-//     amount: 1000,
-//     checkAmount: '1,000.00',
-//     tabValue: 'aave',
-//   },
-//   {
-//     assetName: assets.staking.ABPT,
-//     amount: 1000,
-//     checkAmount: '1,000.00',
-//     tabValue: 'bpt',
-//   },
-// ];
-//
-// const waitStakedAmount = (assetName: string, checkAmount: string) => {
-//   cy.get(`[data-cy="stakedBox_${assetName}"]`)
-//     .find(`[data-cy="amountNative"]`)
-//     .then(($amount) => {
-//       cy.waitUntil(() => $amount.text() == checkAmount, {
-//         errorMsg: "staked amount wasn't updated",
-//         timeout: 70000,
-//         interval: 500,
-//       });
-//     });
-// };
-//
-// testCases.forEach(
-//   (testCase: {
-//     assetName: { fullName: string; shortName: string; address: string };
-//     amount: number;
-//     checkAmount: string;
-//     tabValue: string;
-//   }) => {
-//     describe(`STAKE INTEGRATION SPEC, ${testCase.assetName.shortName} V2 MARKET`, () => {
-//       configEnvWithTenderlyMainnetFork({
-//         tokens: [{ address: testCase.assetName.address }],
-//       });
-//       describe(`Stake amount`, () => {
-//         it(`Open staking page`, () => {
-//           cy.get('[data-cy="menuStake"]').click();
-//
-//           cy.get(`button[value="${testCase.tabValue}"]`).then(($clickable) => {
-//             if ($clickable.prop('disabled')) return;
-//             $clickable.click();
-//           });
-//         });
-//         it(`Open stake modal`, () => {
-//           cy.get(`button[data-cy="stakeBtn_${testCase.assetName.shortName}"]`)
-//             .should('not.be.disabled')
-//             .click();
-//         });
-//         it(`Set amount`, () => {
-//           cy.setAmount(testCase.amount, false);
-//           cy.doConfirm(false, 'Stake');
-//         });
-//         doCloseModal();
-//         it(`Check staked amount`, () => {
-//           waitStakedAmount(testCase.assetName.shortName, testCase.checkAmount);
-//           cy.get(`[data-cy="stakedBox_${testCase.assetName.shortName}"]`)
-//             .find(`[data-cy="amountNative"]`)
-//             .should('have.text', testCase.checkAmount);
-//           cy.get(`[data-cy="stakedBox_${testCase.assetName.shortName}"]`)
-//             .find(`[data-cy="amountUSD"]`)
-//             .should('not.have.text', '$ 0');
-//           cy.get(`[data-cy="rewardBox_${testCase.assetName.shortName}"]`)
-//             .find(`[data-cy="amountNative"]`)
-//             .should('not.have.text', '0');
-//           cy.get(`[data-cy="rewardBox_${testCase.assetName.shortName}"]`)
-//             .find(`[data-cy="amountUSD"]`)
-//             .should('not.have.text', ' $0');
-//         });
-//       });
-//       describe(`Claim reward`, () => {
-//         it(`Open claim popup`, () => {
-//           cy.get(`[data-cy="claimBtn_${testCase.assetName.shortName}"]`).click();
-//         });
-//         it(`Confirm`, () => {
-//           cy.doConfirm(true, `STAKE ${testCase.assetName.shortName}`);
-//         });
-//         doCloseModal();
-//       });
-//       describe(`Activate cooldown`, () => {
-//         it(`open activate cooldown`, () => {
-//           cy.get(`[data-cy="coolDownBtn_${testCase.assetName.shortName}"]`).click();
-//         });
-//         it(`Confirm`, () => {
-//           cy.get(`[data-cy="cooldownAcceptCheckbox"]`).click();
-//           cy.doConfirm(true, 'Cooldown to unstake');
-//         });
-//         doCloseModal();
-//         it(`Check cooldown activation`, () => {
-//           cy.get(`[data-cy="awaitCoolDownBtn_${testCase.assetName.shortName}"]`, {
-//             timeout: 70000,
-//           }).should('be.visible', { timeout: 50000 });
-//         });
-//       });
-//     });
-//   }
-// );
