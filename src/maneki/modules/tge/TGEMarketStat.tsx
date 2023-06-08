af6d6abb52f31a1f4a7ec2bddb2df9c6fe3f1e1b
Jason <jasonkwm13@gmail.com>
2023-06-08 12:44:33 +0800

    style: completed styles for tge market stats and price

--- src/maneki/modules/tge/TGEMarketStat.tsx
+++ src/maneki/modules/tge/TGEMarketStat.tsx
@@ -1,7 +1,8 @@
-import { Box } from '@mui/material';
+import { Trans } from '@lingui/macro';
+import { Box, Typography } from '@mui/material';
 import { BigNumber, Contract, utils } from 'ethers';
+import TGECylinderIcon from 'public/icons/maneki/tge-cylinder-icon.svg';
 import * as React from 'react';
-import { TopInfoPanelItem } from 'src/components/TopInfoPanel/TopInfoPanelItem';
 import EARLY_TOKEN_GENERATION_ABI from 'src/maneki/abi/earlyTokenGenerationABI';
 import ManekiLoadingPaper from 'src/maneki/components/ManekiLoadingPaper';
 
@@ -31,12 +32,81 @@ const TGEMarketStat = () => {
 
   if (loading) return <ManekiLoadingPaper description="Loading..." withCircle />;
   return (
-    <Box>
-      <TopInfoPanelItem title={'TGE PAW PRICE'}>
-        {utils.formatUnits(finalPAWPrice, 18)}
-      </TopInfoPanelItem>
-      <TopInfoPanelItem title={'Market Cap'}>{'$5M'}</TopInfoPanelItem>
-      <TopInfoPanelItem title={'Initial Supply / Total Supply'}>{'$2M / 100M'}</TopInfoPanelItem>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: { xs: 'column', xsm: 'row' },
+        gap: { xs: 3, xsm: 8 },
+        flexWrap: 'wrap',
+        width: '100%',
+        filter: 'drop-shadow(0px 4px 44px rgba(0, 0, 0, 0.1))',
+        mt: '45px',
+      }}
+    >
+      <TGEInfoPanelItem
+        icon={<TGECylinderIcon />}
+        title={<Trans>TGE PAW Price</Trans>}
+        content={`${utils.formatUnits(finalPAWPrice, 18)} BNB`}
+      />
+      <TGEInfoPanelItem
+        icon={<TGECylinderIcon />}
+        title={<Trans>Market Cap</Trans>}
+        content="$5M"
+      />
+      <TGEInfoPanelItem
+        icon={<TGECylinderIcon />}
+        title={<Trans>Initial Supply / Total Supply</Trans>}
+        content="$2M / 100M"
+      />
+    </Box>
+  );
+};
+
+interface TGEInfoPanelItemProps {
+  icon?: React.ReactNode;
+  title: React.ReactNode;
+  content: string;
+}
+
+const TGEInfoPanelItem = ({ icon, title, content }: TGEInfoPanelItemProps) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '20px',
+        gap: '8px',
+        width: { xs: '100%', xsm: '296px' },
+        backgroundColor: 'background.default',
+        borderRadius: '8px',
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '8px' }}>
+        {icon}
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: '400',
+            fontSize: { xs: '14px', xsm: '16px' },
+            lineHeight: '24px',
+          }}
+        >
+          {title}
+        </Typography>
+      </Box>
+      <Typography
+        sx={{
+          fontWeight: '600',
+          fontSize: '24px',
+          lineHeight: '36px',
+          ml: '12px',
+        }}
+      >
+        {content}
+      </Typography>
     </Box>
   );
 };
