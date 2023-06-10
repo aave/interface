f03f9da4bf6b7609769683397b77292daaf5e161
Josh DeCristi <joshdecristi@Joshs-MacBook-Pro.local>
2023-06-10 16:01:52 -0400

    fix: exhaustive-deps in proposalId.governance.tsx

--- pages/governance/proposal/[proposalId].governance.tsx
+++ pages/governance/proposal/[proposalId].governance.tsx
@@ -127,6 +127,7 @@ export default function ProposalPage({
   useEffect(() => {
     if (!proposal && initialProposal) setProposal(initialProposal);
     setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [initialProposal]);
 
   useEffect(() => {
