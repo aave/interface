3c307c8d82817c02fa35c793e3fcec243a6ee996
0xGraham <grahamnelson@live.com>
2023-06-12 13:58:58 +0200

    Feat/amm warning message (#1642)
    * feat: add hal to v3
    
    * Merge branch 'main' into feat/add-hal-to-v3-eth
    
    * feat: add AMM warning messages
    
    * fix: remove console log
    
    * test: turn off amm tests form ci
    
    ---------
    
    Co-authored-by: Nikita Boakrev <right2maresko@gmail.com>

--- src/modules/dashboard/lists/SupplyAssetsList/SupplyAssetsList.tsx
+++ src/modules/dashboard/lists/SupplyAssetsList/SupplyAssetsList.tsx
@@ -43,7 +43,7 @@ const head = [
 ];
 
 export const SupplyAssetsList = () => {
-  const { currentNetworkConfig, currentChainId } = useProtocolDataContext();
+  const { currentNetworkConfig, currentChainId, currentMarketData } = useProtocolDataContext();
   const {
     user,
     reserves,
@@ -233,6 +233,8 @@ export const SupplyAssetsList = () => {
               <MarketWarning marketName="Harmony" />
             ) : supplyDisabled && currentNetworkConfig.name === 'Fantom' ? (
               <MarketWarning marketName="Fantom" />
+            ) : supplyDisabled && currentMarketData.marketTitle === 'Ethereum AMM' ? (
+              <MarketWarning marketName="Ethereum AMM" />
             ) : user?.isInIsolationMode ? (
               <Warning severity="warning">
                 <Trans>
