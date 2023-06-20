d15a3dbf235606f432574dda06cf1a6c37c0f876
Mark Grothe <grothem@gmail.com>
2023-06-20 14:57:24 -0500

    fix: add gho addresses to market config

--- src/store/ghoSlice.ts
+++ src/store/ghoSlice.ts
@@ -1,49 +1,15 @@
 import { GhoService } from '@aave/contract-helpers';
 import { GhoReserveData, GhoUserData, normalize } from '@aave/math-utils';
 import { GHO_SUPPORTED_MARKETS } from 'src/utils/ghoUtilities';
-import {
-  CustomMarket,
-  ENABLE_TESTNET,
-  getProvider,
-  MarketDataType,
-  marketsData,
-  STAGING_ENV,
-} from 'src/utils/marketsAndNetworksConfig';
+import { getProvider } from 'src/utils/marketsAndNetworksConfig';
 import { StateCreator } from 'zustand';
 
 import { RootStore } from './root';
 
 interface GhoMarketConfig {
-  market: MarketDataType;
   ghoTokenAddress: string;
   uiGhoDataProviderAddress: string;
 }
-
-const getGoerliGhoConfig = (market: CustomMarket): GhoMarketConfig => {
-  return {
-    market: marketsData[market],
-    ghoTokenAddress: '0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211'.toLowerCase(),
-    uiGhoDataProviderAddress: '0xe9a16936340097B71370a973363176CA670D666C'.toLowerCase(),
-  };
-};
-
-const getSepoliGhoConfig = (market: CustomMarket): GhoMarketConfig => {
-  return {
-    market: marketsData[market],
-    ghoTokenAddress: '0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D'.toLowerCase(),
-    uiGhoDataProviderAddress: '0xCC3B9A84d5AbC04fb73B2a8Fa67Be4335d55D594'.toLowerCase(),
-  };
-};
-
-// TODO: update when GHO is launched on mainnet
-// NOTE: these addresses are Goerli addresses, and should be updated on launch
-const getMainnetGhoConfig = (market: CustomMarket): GhoMarketConfig => {
-  return {
-    market: marketsData[market],
-    ghoTokenAddress: '0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211'.toLowerCase(),
-    uiGhoDataProviderAddress: '0xE914D574975a1Cd273388035Db4413dda788c0E5'.toLowerCase(),
-  };
-};
 
 export interface GhoSlice {
   ghoReserveData: GhoReserveData;
@@ -51,7 +17,7 @@ export interface GhoSlice {
   ghoReserveDataFetched: boolean;
   ghoUserDataFetched: boolean;
   ghoUserQualifiesForDiscount: (futureBorrowAmount?: string) => boolean;
-  ghoMarketConfig: () => GhoMarketConfig;
+  ghoMarketConfig: () => GhoMarketConfig | undefined;
   refreshGhoData: () => Promise<void>;
 }
 
@@ -106,40 +72,30 @@ export const createGhoSlice: StateCreator<
       );
     },
     ghoMarketConfig: () => {
-      const currentMarket = get().currentMarket;
-
-      if (GHO_SUPPORTED_MARKETS.includes(currentMarket)) {
-        if (STAGING_ENV || ENABLE_TESTNET) {
-          if (currentMarket.endsWith('proto_sepolia_gho_v3')) {
-            return getSepoliGhoConfig(currentMarket);
-          } else {
-            return getGoerliGhoConfig(currentMarket);
-          }
-        } else {
-          return getMainnetGhoConfig(currentMarket);
-        }
-      } else {
-        if (STAGING_ENV || ENABLE_TESTNET) {
-          if (currentMarket === 'proto_sepolia_gho_v3') {
-            return getSepoliGhoConfig(CustomMarket.proto_sepolia_gho_v3);
-          } else {
-            return getGoerliGhoConfig(CustomMarket.proto_goerli_gho_v3);
-          }
-        } else {
-          return getMainnetGhoConfig(CustomMarket.proto_mainnet);
-        }
+      const market = get().currentMarket;
+      if (!GHO_SUPPORTED_MARKETS.includes(market)) {
+        return undefined;
       }
+
+      const { GHO_TOKEN_ADDRESS: ghoTokenAddress, GHO_UI_DATA_PROVIDER: uiGhoDataProviderAddress } =
+        get().currentMarketData.addresses;
+      if (!ghoTokenAddress || !uiGhoDataProviderAddress) {
+        return undefined;
+      }
+
+      return {
+        ghoTokenAddress,
+        uiGhoDataProviderAddress,
+      };
     },
     refreshGhoData: async () => {
       const ghoConfig = get().ghoMarketConfig();
       if (!ghoConfig) return;
-      // temporary to prevent app crashes when launching in production mode
-      if (!STAGING_ENV || ENABLE_TESTNET) return;
 
       const account = get().account;
 
       const ghoService = new GhoService({
-        provider: getProvider(ghoConfig.market.chainId),
+        provider: getProvider(get().currentMarketData.chainId),
         uiGhoDataProviderAddress: ghoConfig.uiGhoDataProviderAddress,
       });
 
--- src/ui-config/stakeConfig.ts
+++ src/ui-config/stakeConfig.ts
@@ -15,12 +15,12 @@ export interface StakeConfig {
 
 export const prodStakeConfig: StakeConfig = {
   chainId: ChainId.mainnet,
-  stakeDataProvider: '0x5E045cfb738F01bC73CEAFF783F4C16e8B14090b',
+  stakeDataProvider: '0xc57450af527d10Fe182521AB39C1AD23c1e1BaDE',
   tokens: {
     [Stake.aave]: {
       TOKEN_STAKING: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
       STAKING_REWARD_TOKEN: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
-      STAKING_HELPER: '0xaF630B2151b7b7F22dEf78a9830af5BED07208B3',
+      STAKING_HELPER: '0xce0424653fb2fd48ed1b621bdbd60db16b2e388a',
     },
     [Stake.bpt]: {
       TOKEN_STAKING: '0xa1116930326D21fB917d5A27F1E9943A9595fb47',
