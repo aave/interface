3fd6bd81d7bb062837ea73c423288065960ee2ea
Jason <jasonkwm13@gmail.com>
2023-06-21 13:32:50 +0800

    fix: copy text color for error modal

--- src/components/transactions/FlowCommons/Error.tsx
+++ src/components/transactions/FlowCommons/Error.tsx
@@ -57,7 +57,7 @@ export const TxErrorView = ({ txError }: { txError: TxErrorType }) => {
           variant="outlined"
           onClick={() => navigator.clipboard.writeText(txError.rawError.message.toString())}
           size="small"
-          sx={{ mt: 6, color: 'background.default' }}
+          sx={{ mt: 6 }}
         >
           <Trans>Copy error text</Trans>
 
