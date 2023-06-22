a54fbbd7660af929d67a83b967372aa18420e6cc
Mark Grothe <grothem@gmail.com>
2023-06-22 15:44:25 -0500

    fix: dynamically import reserve config components

--- src/modules/reserve-overview/ReserveConfigurationWrapper.tsx
+++ src/modules/reserve-overview/ReserveConfigurationWrapper.tsx
@@ -1,15 +1,21 @@
 import { Trans } from '@lingui/macro';
 import { Box, Paper, Typography, useMediaQuery, useTheme } from '@mui/material';
+import dynamic from 'next/dynamic';
 import { ComputedReserveData } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { isGhoAndSupported } from 'src/utils/ghoUtilities';
 
-import { GhoReserveConfiguration } from './Gho/GhoReserveConfiguration';
-import { ReserveConfiguration } from './ReserveConfiguration';
-
 type ReserveConfigurationProps = {
   reserve: ComputedReserveData;
 };
+
+const GhoReserveConfiguration = dynamic(() =>
+  import('./Gho/GhoReserveConfiguration').then((module) => module.GhoReserveConfiguration)
+);
+
+const ReserveConfiguration = dynamic(() =>
+  import('./ReserveConfiguration').then((module) => module.ReserveConfiguration)
+);
 
 export const ReserveConfigurationWrapper: React.FC<ReserveConfigurationProps> = ({ reserve }) => {
   const { currentMarket } = useProtocolDataContext();
