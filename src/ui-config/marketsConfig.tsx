f0cf27a712521c5dd06976b10be6f51b976d43bb
Mark Grothe <grothem@gmail.com>
2023-07-07 11:31:34 -0500

    feat: use latest fork contracts

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -132,7 +132,7 @@ export const marketsData: {
       UI_POOL_DATA_PROVIDER: AaveV3Ethereum.UI_POOL_DATA_PROVIDER,
       UI_INCENTIVE_DATA_PROVIDER: AaveV3Ethereum.UI_INCENTIVE_DATA_PROVIDER,
       COLLECTOR: AaveV3Ethereum.COLLECTOR,
-      DEBT_SWITCH_ADAPTER: '0xdf545dc1613cbab514ccf6cd6819306f82542ca2',
+      DEBT_SWITCH_ADAPTER: '0x4318f417a11639386382f20b477180abc7ee9dd4',
     },
     halIntegration: {
       URL: 'https://app.hal.xyz/recipes/aave-v3-track-health-factor',
@@ -162,7 +162,7 @@ export const marketsData: {
       UI_INCENTIVE_DATA_PROVIDER: AaveV2Ethereum.UI_INCENTIVE_DATA_PROVIDER,
       COLLECTOR: AaveV2Ethereum.COLLECTOR,
       V3_MIGRATOR: AaveV2Ethereum.MIGRATION_HELPER,
-      DEBT_SWITCH_ADAPTER: '0xdf545dc1613cbab514ccf6cd6819306f82542ca2',
+      DEBT_SWITCH_ADAPTER: '0xc317970223c39a3a7249119cdcdd8c38b56510a9',
     },
     halIntegration: {
       URL: 'https://app.hal.xyz/recipes/aave-track-your-health-factor',
