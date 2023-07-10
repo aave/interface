6014d586bc52aa4b34ca2d6504291fae5bcbc304
Jason <jasonkwm13@gmail.com>
2023-07-10 15:36:59 +0800

    fix: aave CORS error

--- src/hooks/useAddressAllowed.tsx
+++ src/hooks/useAddressAllowed.tsx
@@ -1,36 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { usePolling } from './usePolling';
+// import { usePolling } from './usePolling';
 
 export interface AddressAllowedResult {
   isAllowed: boolean;
 }
 
-const TWO_MINUTES = 2 * 60 * 1000;
+// const TWO_MINUTES = 2 * 60 * 1000;
+
+// export const useAddressAllowed = (address: string): AddressAllowedResult => {
+//   const [isAllowed, setIsAllowed] = useState(true);
+
+//   const screeningUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}/addresses/status`;
+//   const queryParams = `?address=${address}`;
+
+//   const getIsAddressAllowed = async () => {
+//     if (screeningUrl && address) {
+//       try {
+//         const response = await fetch(screeningUrl + queryParams);
+//         if (response.ok) {
+//           const data: { addressAllowed: boolean } = await response.json();
+//           setIsAllowed(data.addressAllowed);
+//         }
+//       } catch (e) {}
+//     } else {
+//       setIsAllowed(true);
+//     }
+//   };
+
+//   usePolling(getIsAddressAllowed, TWO_MINUTES, false, [address]);
+
+//   return {
+//     isAllowed,
+//   };
+// };
 
 export const useAddressAllowed = (address: string): AddressAllowedResult => {
   const [isAllowed, setIsAllowed] = useState(true);
 
-  const screeningUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}/addresses/status`;
-  const queryParams = `?address=${address}`;
+  useEffect(() => {
+    if (address) setIsAllowed(true);
+  }, []);
 
-  const getIsAddressAllowed = async () => {
-    if (screeningUrl && address) {
-      try {
-        const response = await fetch(screeningUrl + queryParams);
-        if (response.ok) {
-          const data: { addressAllowed: boolean } = await response.json();
-          setIsAllowed(data.addressAllowed);
-        }
-      } catch (e) {}
-    } else {
-      setIsAllowed(true);
-    }
-  };
-
-  usePolling(getIsAddressAllowed, TWO_MINUTES, false, [address]);
-
-  return {
-    isAllowed,
-  };
+  return { isAllowed };
 };
