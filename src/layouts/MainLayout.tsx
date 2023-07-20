e36e537ce33f56d8c3f8ac6280f6bb90bd23a3ad
Jason <jasonkwm13@gmail.com>
2023-07-20 12:12:04 +0800

    feat: add new tos component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,26 @@
 import { Box } from '@mui/material';
-import React, { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import React, { ReactNode, useEffect, useState } from 'react';
+import TosComponent from 'src/maneki/components/TosComponent';
 
 import { AppHeader } from './AppHeader';
 
 export function MainLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <AppHeader />
-      <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-        {children}
-      </Box>
-    </>
-  );
+  const route = useRouter();
+  const [tos, setTos] = useState(false);
+  useEffect(() => {
+    const manekiTOS = localStorage.getItem('manekiTOS');
+    manekiTOS !== 'agreed' ? setTos(false) : setTos(true);
+  }, [route]);
+  if (!tos) return <TosComponent />;
+  else {
+    return (
+      <>
+        <AppHeader />
+        <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+          {children}
+        </Box>
+      </>
+    );
+  }
 }
