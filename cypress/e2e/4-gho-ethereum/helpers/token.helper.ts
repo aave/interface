b1e1b569587dad27681aed423d6f732f35c758cb
Nikita <right2maresko@gmail.com>
2023-07-25 14:14:17 +0100

    test: test coverage for gho on mainnet market (#1701)
    * test: test coverage for gho on mainnet market
    
    * fix: config naming

--- cypress/e2e/4-gho-ethereum/helpers/token.helper.ts
+++ cypress/e2e/4-gho-ethereum/helpers/token.helper.ts
@@ -1,4 +1,4 @@
-import { TokenRequest } from '../../../../support/actions/tenderly.actions';
+import { TokenRequest } from '../../../support/actions/tenderly.actions';
 import donors from '../fixtures/donors.json';
 
 export const tokenSet = ({ stkAave = 0, aAAVE = 0, aDAI = 0 }) => {
