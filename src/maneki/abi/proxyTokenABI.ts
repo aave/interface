3dcbb95af5973dcc65bc3b95febebe5fae1ed976
Jason <jasonkwm13@gmail.com>
2023-07-26 16:36:06 +0800

    chore: add leverager abi and remove duplicate abi from proxy

--- src/maneki/abi/proxyTokenABI.ts
+++ src/maneki/abi/proxyTokenABI.ts
@@ -21,16 +21,6 @@ const PROXY_TOKEN_ABI = [
     type: 'event',
   },
   {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: 'address', name: 'from', type: 'address' },
-      { indexed: true, internalType: 'address', name: 'to', type: 'address' },
-      { indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
-    ],
-    name: 'Transfer',
-    type: 'event',
-  },
-  {
     inputs: [],
     name: 'DOMAIN_SEPARATOR',
     outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
