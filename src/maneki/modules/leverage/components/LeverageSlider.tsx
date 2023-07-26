cfd316cd2c6d9ea2209b5741cbb856d32f7e58d6
Jason <jasonkwm13@gmail.com>
2023-07-26 11:47:37 +0800

    feat: leverage slider

---
+++ src/maneki/modules/leverage/components/LeverageSlider.tsx
@@ -0,0 +1,78 @@
+import { Box, Slider } from '@mui/material';
+import { useLeverageContext } from 'src/maneki/hooks/leverage-data-provider/LeverageDataProvider';
+
+// interface IMarks {
+//   value: number;
+//   label: string;
+// }
+
+const marks = [
+  {
+    value: 2,
+    label: '2x',
+  },
+  {
+    value: 3,
+    label: '3x',
+  },
+  {
+    value: 4,
+    label: '4x',
+  },
+  {
+    value: 5,
+    label: '5x',
+  },
+  {
+    value: 6,
+    label: '6x',
+  },
+  {
+    value: 7,
+    label: '7x',
+  },
+  {
+    value: 8,
+    label: '8x',
+  },
+  {
+    value: 9,
+    label: '9x',
+  },
+  {
+    value: 10,
+    label: '10x',
+  },
+  {
+    value: 11,
+    label: '11x',
+  },
+  {
+    value: 12,
+    label: '12x',
+  },
+];
+
+export default function LeverageSlider() {
+  const { setLeverage } = useLeverageContext();
+  return (
+    <Box sx={{ width: '95%', m: 'auto' }}>
+      <Slider
+        min={2}
+        max={12}
+        marks={marks}
+        step={1}
+        defaultValue={2}
+        aria-label="Leverage"
+        getAriaValueText={(value: number) => {
+          setLeverage(value);
+          return `${value}`;
+        }}
+        sx={{
+          fontWeight: '800',
+          fontSize: '14px',
+        }}
+      />
+    </Box>
+  );
+}
