cfdbea6cef4cef5c57cf5fa0ca0a0ab995316baf
Jason <jasonkwm13@gmail.com>
2023-07-31 13:38:59 +0800

    chore: change usdc bridge to usdce

--- src/maneki/modules/leverage/utils/leverageActionHelper.ts
+++ src/maneki/modules/leverage/utils/leverageActionHelper.ts
@@ -13,12 +13,17 @@ export type reservesTokensTuple = [string, string];
 export function convertReserveTokens(
   reservesTokens: reservesTokensTuple[]
 ): collateralAssetsType[] {
-  const reservesTokensAddress: collateralAssetsType[] = reservesTokens.map(([token, address]) => ({
-    token,
-    address,
-    value: BigNumber.from(0),
-    balance: BigNumber.from(0),
-    decimals: 0,
-  }));
+  const reservesTokensAddress: collateralAssetsType[] = reservesTokens.map(([token, address]) => {
+    if (address.toLowerCase() === '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'.toLowerCase())
+      token = 'USDC.e';
+
+    return {
+      token,
+      address,
+      value: BigNumber.from(0),
+      balance: BigNumber.from(0),
+      decimals: 0,
+    };
+  });
   return reservesTokensAddress;
 }
