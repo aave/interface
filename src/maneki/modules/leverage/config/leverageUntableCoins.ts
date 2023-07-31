ab5ddb122e9607ae8eece36f3e52c922dc5e902c
Jason <jasonkwm13@gmail.com>
2023-07-31 22:49:31 +0800

    chore: change address in leverage coin config

--- src/maneki/modules/leverage/config/leverageUntableCoins.ts
+++ src/maneki/modules/leverage/config/leverageUntableCoins.ts
@@ -4,5 +4,5 @@ import { IleverageBorrowAsset } from '../../../hooks/leverage-data-provider/Leve
  * supported stable coin symbols borrows for leverage
  */
 export const LEVERAGE_UNSTABLE_COINS: IleverageBorrowAsset[] = [
-  { symbol: 'ETH', address: '0x1234' },
+  { symbol: 'ETH', address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1' },
 ];
