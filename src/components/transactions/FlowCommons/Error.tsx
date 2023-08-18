cc6df857bbe854c28f7d27b0f6031d556044fa5e
Mooncake Eater <mooncakefi@proton.me>
2023-08-18 18:59:19 +0300

    Added discord button

--- src/components/transactions/FlowCommons/Error.tsx
+++ src/components/transactions/FlowCommons/Error.tsx
@@ -41,8 +41,8 @@ export const TxErrorView = ({ txError }: { txError: TxErrorType }) => {
 
         <Typography>
           <Trans>
-            You can report incident to our <Link href="https://discord.gg/7kHKnkDEUf">Discord</Link>{' '}
-            or <Link href="https://github.com/aave/interface">Github</Link>.
+            You can report incident to our <Link href="https://discord.gg/fCcv8K6Uqy">Discord</Link>{' '}
+            or <Link href="https://github.com/mooncakefi/mooncake.fi">Github</Link>.
           </Trans>
         </Typography>
 
