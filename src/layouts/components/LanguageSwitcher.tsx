27406e646664119d5d2c329cdd56dec4cd14b649
Mark Grothe <grothem@gmail.com>
2023-09-18 14:47:10 -0500

    chore: upgrade lingui

--- src/layouts/components/LanguageSwitcher.tsx
+++ src/layouts/components/LanguageSwitcher.tsx
@@ -14,12 +14,14 @@ import React from 'react';
 
 import { dynamicActivateLanguage } from '../../libs/LanguageProvider';
 
-const langMap = {
-  en: t`English`,
-  es: t`Spanish`,
-  fr: t`French`,
-  el: t`Greek`,
-};
+function getLangMap(): Record<string, string> {
+  return {
+    en: t`English`,
+    es: t`Spanish`,
+    fr: t`French`,
+    el: t`Greek`,
+  };
+}
 
 interface LanguageListItemProps {
   component?: typeof MenuItem | typeof ListItem;
@@ -39,7 +41,7 @@ export const LanguageListItem = ({ component = ListItem, onClick }: LanguageList
         <Trans>Language</Trans>
       </ListItemText>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        {i18n._(langMap[i18n.locale as keyof typeof langMap])}{' '}
+        {i18n._(getLangMap()[i18n.locale])}{' '}
         <SvgIcon fontSize="small" sx={{ color: { xs: '#F1F1F3', md: 'text.primary' }, ml: 1 }}>
           <ChevronRightIcon />
         </SvgIcon>
@@ -76,7 +78,7 @@ export const LanguagesList = ({ component = ListItem, onClick }: LanguageListIte
         </ListItemText>
       </Box>
 
-      {Object.keys(langMap).map((lang) => (
+      {Object.keys(getLangMap()).map((lang) => (
         <Box
           component={component}
           key={lang}
@@ -92,7 +94,7 @@ export const LanguagesList = ({ component = ListItem, onClick }: LanguageListIte
           >
             <img src={`/icons/flags/${lang}.svg`} width="100%" height="100%" alt={`${lang} icon`} />
           </ListItemIcon>
-          <ListItemText>{i18n._(langMap[lang as keyof typeof langMap])}</ListItemText>
+          <ListItemText>{i18n._(getLangMap()[lang])}</ListItemText>
           {lang === i18n.locale && (
             <ListItemIcon sx={{ m: 0 }}>
               <SvgIcon fontSize="small" sx={{ color: { xs: '#F1F1F3', md: 'text.primary' } }}>
