105f1adb9086a5d90f17e686714d927824d014a4
Mark Grothe <grothem@gmail.com>
2023-09-21 15:41:59 -0500

    fix: hooking up connect kit

--- src/components/WalletConnection/ConnectWalletButton.tsx
+++ src/components/WalletConnection/ConnectWalletButton.tsx
@@ -1,23 +1,36 @@
-import { Trans } from '@lingui/macro';
-import { Button } from '@mui/material';
-import { useWalletModalContext } from 'src/hooks/useWalletModal';
-import { useRootStore } from 'src/store/root';
-import { AUTH } from 'src/utils/mixPanelEvents';
+// import { Trans } from '@lingui/macro';
+import { useTheme } from '@mui/material';
+import { ConnectKitButton } from 'connectkit';
+// import { useWalletModalContext } from 'src/hooks/useWalletModal';
+// import { useRootStore } from 'src/store/root';
+// import { AUTH } from 'src/utils/mixPanelEvents';
 
 // import { WalletModal } from './WalletModal';
-import { WatchWalletModal } from './WatchWalletModal';
+// import { WatchWalletModal } from './WatchWalletModal';
 
 export interface ConnectWalletProps {
   funnel?: string;
 }
 
 export const ConnectWalletButton: React.FC<ConnectWalletProps> = ({ funnel }) => {
-  const { setWalletModalOpen } = useWalletModalContext();
-  const trackEvent = useRootStore((store) => store.trackEvent);
+  // TODO
+  // const trackEvent = useRootStore((store) => store.trackEvent);
+  console.log(funnel);
+
+  const theme = useTheme();
 
   return (
     <>
-      <Button
+      <ConnectKitButton
+        customTheme={{
+          '--ck-connectbutton-background': theme.palette.gradients.aaveGradient,
+          '--ck-connectbutton-hover-background': theme.palette.gradients.aaveGradient,
+          '--ck-connectbutton-active-background': theme.palette.gradients.aaveGradient,
+          '--ck-connectbutton-border-radius': '4px',
+          '--ck-connectbutton-font-size': '0.875rem',
+        }}
+      />
+      {/* <Button
         variant="gradient"
         onClick={() => {
           trackEvent(AUTH.CONNECT_WALLET, { funnel: funnel });
@@ -25,9 +38,9 @@ export const ConnectWalletButton: React.FC<ConnectWalletProps> = ({ funnel }) =>
         }}
       >
         <Trans>Connect wallet</Trans>
-      </Button>
+      </Button> */}
       {/* <WalletModal /> */}
-      <WatchWalletModal />
+      {/* <WatchWalletModal /> */}
     </>
   );
 };
