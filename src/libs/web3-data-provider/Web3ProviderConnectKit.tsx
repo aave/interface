60f4eee1399a5664fdb0132ec9d2b6538ef6a07d
Mark Grothe <grothem@gmail.com>
2023-09-21 11:03:42 -0500

    fix: test WC id

--- src/libs/web3-data-provider/Web3ProviderConnectKit.tsx
+++ src/libs/web3-data-provider/Web3ProviderConnectKit.tsx
@@ -1,12 +1,12 @@
-import { WagmiConfig, createConfig } from 'wagmi';
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from 'connectkit';
+import { ConnectKitButton, ConnectKitProvider, getDefaultConfig } from 'connectkit';
 import React, { ReactElement } from 'react';
+import { createConfig, WagmiConfig } from 'wagmi';
 
 const config = createConfig(
   getDefaultConfig({
     // Required API Keys
-    alchemyId: process.env.ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
+    // alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: '5e8e618c68a73b7fb035c833dbf210b4', // process.env.WALLETCONNECT_PROJECT_ID,
 
     // Required
     appName: 'Your App Name',
