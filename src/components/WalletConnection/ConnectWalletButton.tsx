7807d1876f35f8f5a2265d0cbb7fb7785fce4081
Mark Grothe <grothem@gmail.com>
2023-09-22 08:17:43 -0500

    fix: track event on connect

--- src/components/WalletConnection/ConnectWalletButton.tsx
+++ src/components/WalletConnection/ConnectWalletButton.tsx
@@ -1,27 +1,26 @@
 // import { Trans } from '@lingui/macro';
 import { useTheme } from '@mui/material';
 import { ConnectKitButton } from 'connectkit';
-// import { useWalletModalContext } from 'src/hooks/useWalletModal';
-// import { useRootStore } from 'src/store/root';
-// import { AUTH } from 'src/utils/mixPanelEvents';
-
-// import { WalletModal } from './WalletModal';
-// import { WatchWalletModal } from './WatchWalletModal';
+import { useRootStore } from 'src/store/root';
+import { AUTH } from 'src/utils/mixPanelEvents';
 
 export interface ConnectWalletProps {
   funnel?: string;
 }
 
 export const ConnectWalletButton: React.FC<ConnectWalletProps> = ({ funnel }) => {
-  // TODO
-  // const trackEvent = useRootStore((store) => store.trackEvent);
-  console.log(funnel);
-
+  const trackEvent = useRootStore((store) => store.trackEvent);
   const theme = useTheme();
+
+  const onClick = (open: () => void) => {
+    trackEvent(AUTH.CONNECT_WALLET, { funnel });
+    open();
+  };
 
   return (
     <>
       <ConnectKitButton
+        onClick={onClick}
         customTheme={{
           '--ck-connectbutton-background': theme.palette.gradients.aaveGradient,
           '--ck-connectbutton-hover-background': theme.palette.gradients.aaveGradient,
@@ -30,17 +29,6 @@ export const ConnectWalletButton: React.FC<ConnectWalletProps> = ({ funnel }) =>
           '--ck-connectbutton-font-size': '0.875rem',
         }}
       />
-      {/* <Button
-        variant="gradient"
-        onClick={() => {
-          trackEvent(AUTH.CONNECT_WALLET, { funnel: funnel });
-          setWalletModalOpen(true);
-        }}
-      >
-        <Trans>Connect wallet</Trans>
-      </Button> */}
-      {/* <WalletModal /> */}
-      {/* <WatchWalletModal /> */}
     </>
   );
 };
