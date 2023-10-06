2d04c950e8560e1431004a5289952f37ec950e38
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-10-06 15:18:24 +0100

    chore: move tx history locations (#1795)
    * chore: move tx history locations
    
    * chore: handle mobile
    
    * chore: fix duplicate
    
    * chore: handle position

--- src/modules/dashboard/DashboardContentWrapper.tsx
+++ src/modules/dashboard/DashboardContentWrapper.tsx
@@ -1,6 +1,11 @@
 import { ChainId } from '@aave/contract-helpers';
-import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { Trans } from '@lingui/macro';
+import { Box, Button, useMediaQuery, useTheme } from '@mui/material';
+import { useRouter } from 'next/router';
+import { ROUTES } from 'src/components/primitives/Link';
+import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
 import { useRootStore } from 'src/store/root';
+import { AUTH } from 'src/utils/mixPanelEvents';
 
 import { BorrowAssetsList } from './lists/BorrowAssetsList/BorrowAssetsList';
 import { BorrowedPositionsList } from './lists/BorrowedPositionsList/BorrowedPositionsList';
@@ -13,9 +18,18 @@ interface DashboardContentWrapperProps {
 
 export const DashboardContentWrapper = ({ isBorrow }: DashboardContentWrapperProps) => {
   const { breakpoints } = useTheme();
+  const { currentAccount } = useWeb3Context();
+  const router = useRouter();
+  const trackEvent = useRootStore((store) => store.trackEvent);
+
   const currentMarketData = useRootStore((store) => store.currentMarketData);
   const isDesktop = useMediaQuery(breakpoints.up('lg'));
   const paperWidth = isDesktop ? 'calc(50% - 8px)' : '100%';
+
+  const downToLg = useMediaQuery(breakpoints.down('lg'));
+
+  const upFromSm = useMediaQuery(breakpoints.up('xsm'));
+
   return (
     <Box>
       {currentMarketData.chainId === ChainId.polygon && !currentMarketData.v3}
@@ -26,12 +40,71 @@ export const DashboardContentWrapper = ({ isBorrow }: DashboardContentWrapperPro
           alignItems: 'flex-start',
         }}
       >
-        <Box sx={{ display: { xs: isBorrow ? 'none' : 'block', lg: 'block' }, width: paperWidth }}>
+        <Box
+          sx={{
+            position: 'relative',
+
+            display: { xs: isBorrow ? 'none' : 'block', lg: 'block' },
+            width: paperWidth,
+          }}
+        >
+          {currentAccount && !isBorrow && downToLg && (
+            <Box>
+              <Button
+                sx={{
+                  position: 'absolute',
+                  top: upFromSm ? '-60px' : '-90px',
+                  right: '0px',
+                }}
+                onClick={() => {
+                  router.push(ROUTES.history);
+                  trackEvent(AUTH.VIEW_TX_HISTORY);
+                }}
+                component="a"
+                variant="surface"
+                size="small"
+              >
+                <Trans>View Transactions</Trans>
+              </Button>
+            </Box>
+          )}
+
           <SuppliedPositionsList />
           <SupplyAssetsList />
         </Box>
 
-        <Box sx={{ display: { xs: !isBorrow ? 'none' : 'block', lg: 'block' }, width: paperWidth }}>
+        <Box
+          sx={{
+            position: 'relative',
+
+            display: { xs: !isBorrow ? 'none' : 'block', lg: 'block' },
+            width: paperWidth,
+          }}
+        >
+          {currentAccount && (
+            <Box
+              sx={{
+                position: 'absolute',
+
+                top: upFromSm ? '-60px' : '-90px',
+
+                right: '0px',
+              }}
+            >
+              <Button
+                onClick={() => {
+                  router.push(ROUTES.history);
+                  trackEvent(AUTH.VIEW_TX_HISTORY);
+                }}
+                component="a"
+                variant="surface"
+                size="small"
+              >
+                <Trans>View Transactions</Trans>
+              </Button>
+            </Box>
+          )}
+
           <BorrowedPositionsList />
           <BorrowAssetsList />
         </Box>
