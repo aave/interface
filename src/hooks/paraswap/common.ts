0edb9b483e181031626f2f6efa82ba1477712021
Joaquin Battilana <battilanajn@gmail.com>
2023-10-20 15:15:40 +0100

    feat: changed to take surplus

--- src/hooks/paraswap/common.ts
+++ src/hooks/paraswap/common.ts
@@ -350,7 +350,7 @@ export const ExactInSwapper = (chainId: ChainId) => {
           priceRoute: route,
           userAddress: user,
           partnerAddress: FEE_CLAIMER_ADDRESS,
-          positiveSlippageToUser: false,
+          takeSurplus: true,
         },
         { ignoreChecks: true }
       );
@@ -423,7 +423,7 @@ const ExactOutSwapper = (chainId: ChainId) => {
           priceRoute: route,
           userAddress: user,
           partnerAddress: FEE_CLAIMER_ADDRESS,
-          positiveSlippageToUser: false,
+          takeSurplus: true,
           srcDecimals,
           destDecimals,
         },
