7626a3b1349abb73af454302c20a0bebc5a56da7
Mark Grothe <grothem@gmail.com>
2023-11-02 09:15:24 -0500

    feat: wrapped config

--- src/ui-config/wrappedTokenConfig.ts
+++ src/ui-config/wrappedTokenConfig.ts
@@ -25,5 +25,16 @@ export const wrappedTokenConfig: { [chainId: number]: WrappedTokenConfig[] } = {
       },
       tokenWrapperContractAddress: '0x437f428930669cd06adab2df4a8d4b203ac729c6',
     },
+    {
+      tokenIn: {
+        underlyingAsset: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'.toLowerCase(),
+        symbol: 'stETH',
+      },
+      tokenOut: {
+        underlyingAsset: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0'.toLowerCase(),
+        symbol: 'wstETH',
+      },
+      tokenWrapperContractAddress: '0x14aa09449fac437b5c0110614be2c08610e38f62',
+    },
   ],
 };
