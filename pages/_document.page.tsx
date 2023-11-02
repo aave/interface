156657fb742f77554f2497fcb64b5d7b7464bbb5
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-11-02 19:56:27 +0000

    fix: favicon

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -11,13 +11,13 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content="#1B2030" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
-          <link
-            rel="apple-touch-icon"
-            href="/apple-icon?<generated>"
-            type="image/<generated>"
-            sizes="<generated>"
-          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="16x16" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="32x32" href="/favicon32.png" />
+          <link rel="shortcut icon" sizes="32x32" href="/favicon32.ico" />
+          <link rel="apple-touch-icon" sizes="64x64" href="/favicon64.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/aave_icon180.png" />
+
           <link rel="manifest" href="/manifest.json" />
           {
             // Inject MUI styles first to match with the prepend: true configuration.
