c29cd06c05c5b37a82125e8e6718c56d816feba1
Mark Hinschberger <markhinschberger@gmail.com>
2023-11-04 17:55:02 +0000

    chore: market notification

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React, { ReactNode } from 'react';
 import AnalyticsConsent from 'src/components/Analytics/AnalyticsConsent';
+import TopBarNotify from 'src/layouts/TopBarNotify';
 import { FORK_ENABLED } from 'src/utils/marketsAndNetworksConfig';
 
 import { AppFooter } from './AppFooter';
@@ -9,6 +10,11 @@ import { AppHeader } from './AppHeader';
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
+      <TopBarNotify
+        notifyText="An issue in a certain feature of the Aave Protocol was identified. Some markets are temporarily paused on Aave V2. No funds are at risk"
+        learnMoreLink="https://google.com"
+      />
+
       <AppHeader />
       <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
         {children}
