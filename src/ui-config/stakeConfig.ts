1dfb0d95cf41408e6377da8615da19596de38e24
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-11-24 16:19:33 +0000

    chore: update oracle

--- src/ui-config/stakeConfig.ts
+++ src/ui-config/stakeConfig.ts
@@ -1,4 +1,5 @@
 import { ChainId, Stake } from '@aave/contract-helpers';
+import { AaveSafetyModule, AaveV2Ethereum } from '@bgd-labs/aave-address-book';
 
 export interface StakeConfig {
   chainId: ChainId;
@@ -17,13 +18,13 @@ export const stakeConfig: StakeConfig = {
   stakeDataProvider: '0x5E045cfb738F01bC73CEAFF783F4C16e8B14090b',
   tokens: {
     [Stake.aave]: {
-      TOKEN_STAKING: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
-      STAKING_REWARD_TOKEN: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+      TOKEN_STAKING: AaveSafetyModule.STK_AAVE,
+      STAKING_REWARD_TOKEN: AaveV2Ethereum.ASSETS.AAVE.UNDERLYING,
       STAKING_HELPER: '0xce0424653fb2fd48ed1b621bdbd60db16b2e388a',
     },
     [Stake.bpt]: {
-      TOKEN_STAKING: '0xa1116930326D21fB917d5A27F1E9943A9595fb47',
-      STAKING_REWARD_TOKEN: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+      TOKEN_STAKING: AaveSafetyModule.STK_ABPT,
+      STAKING_REWARD_TOKEN: AaveSafetyModule.STK_ABPT_ORACLE,
     },
   },
 };
