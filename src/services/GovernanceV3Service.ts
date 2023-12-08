7e3a19699ee412882c8e0bbcf8c647e9322d905a
Mark Grothe <grothem@gmail.com>
2023-12-08 15:50:01 -0600

    feat: latest utils

--- src/services/GovernanceV3Service.ts
+++ src/services/GovernanceV3Service.ts
@@ -1,4 +1,4 @@
-import { ChainId, GovernanceDataHelperService } from '@aave/contract-helpers';
+import { AccessLevel, ChainId, GovernanceDataHelperService } from '@aave/contract-helpers';
 import { getProvider } from 'src/utils/marketsAndNetworksConfig';
 
 const govCoreAddressSepolia = '0xc4ABF658C3Dda84225cF8A07d7D5Bb6Aa41d9E59';
@@ -11,9 +11,23 @@ export class GovernanceV3Service {
     return new GovernanceDataHelperService(govDataHelperSepolia, provider);
   }
 
-  async getProposalsData() {
+  async getProposalsData(from = 0, to = 0, limit = 10) {
     const dataHelperService = this.getDataHelperService();
-    const proposals = await dataHelperService.getProposalsData(govCoreAddressSepolia, 0, 0, 10);
+    const proposals = await dataHelperService.getProposalsData(
+      govCoreAddressSepolia,
+      from,
+      to,
+      limit
+    );
     return proposals;
+  }
+
+  async getVotingConfig() {
+    const dataHelperService = this.getDataHelperService();
+    const votingConfig = await dataHelperService.getConstants(govCoreAddressSepolia, [
+      AccessLevel.Short_Executor,
+      AccessLevel.Long_Executor,
+    ]);
+    return votingConfig;
   }
 }
