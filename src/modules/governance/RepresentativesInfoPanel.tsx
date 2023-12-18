6fba25c44a9d2046fad31fcfbeb5f524a91de801
Mark Grothe <grothem@gmail.com>
2023-12-18 14:49:20 -0600

    feat: representing view

--- src/modules/governance/RepresentativesInfoPanel.tsx
+++ src/modules/governance/RepresentativesInfoPanel.tsx
@@ -1,3 +1,4 @@
+import { Representative, Rpresented } from '@aave/contract-helpers';
 import { PlusIcon } from '@heroicons/react/outline';
 import { ExternalLinkIcon } from '@heroicons/react/solid';
 import { Trans } from '@lingui/macro';
@@ -12,8 +13,11 @@ import { networkConfigs } from 'src/ui-config/networksConfig';
 
 import { ZERO_ADDRESS } from './utils/formatProposal';
 
-// const userWithRepChosen = '0xAd9A211D227d2D9c1B5573f73CDa0284b758Ac0C';
+// Setting up a representative is only useful for smart contract wallets.
+// If connected account is an EOA, this section is hidden.
 
+// If an account has representatives, then we assume that there is no need to set up a representative,
+// and it will show the addresses that have selected the current account to represent them.
 export const RepresentativesInfoPanel = () => {
   const { openGovRepresentatives } = useModalContext();
   const account = useRootStore((state) => state.account);
@@ -25,11 +29,13 @@ export const RepresentativesInfoPanel = () => {
 
   console.log(isContractAddress, fetchingIsContractAddress);
 
-  console.log(data);
-
   const isAddressSelectedAsRepresentative = data?.Represented.some(
     (r) => r.votersRepresented.length > 0
   );
+
+  // if (!isContractAddress) {
+  //   return null;
+  // }
 
   return (
     <Paper sx={{ mt: 2 }}>
@@ -49,73 +55,26 @@ export const RepresentativesInfoPanel = () => {
         <Stack gap={8} sx={{ mt: 2 }}>
           <Stack direction="column">
             <Typography variant="description" color="text.secondary">
-              <Trans>
-                Representative smart contract wallet (ie. Safe) addresses on other chains.
-              </Trans>
+              {isAddressSelectedAsRepresentative ? (
+                <Trans>
+                  Representing smart contract wallet (ie. Safe) addresses on other chains.
+                </Trans>
+              ) : (
+                <Trans>
+                  Representative smart contract wallet (ie. Safe) addresses on other chains.
+                </Trans>
+              )}
             </Typography>
           </Stack>
           <Stack alignItems="start" gap={6}>
-            {data?.Representatives.map((representative, i) => (
-              <Stack gap={4} key={i} direction="column" alignItems="self-start">
-                <Stack direction="row" alignItems="center" gap={2}>
-                  <img
-                    src={networkConfigs[representative.chainId].networkLogoPath}
-                    height="16px"
-                    width="16px"
-                    alt="network logo"
-                  />
-                  <Typography variant="subheader1">
-                    {networkConfigs[representative.chainId].name}
-                  </Typography>
-                </Stack>
-                {representative.representative === ZERO_ADDRESS ? (
-                  <Stack direction="row" gap={1} alignItems="center">
-                    <IconButton
-                      sx={(theme) => ({
-                        height: '24px',
-                        width: '24px',
-                        background: theme.palette.background.disabled,
-                      })}
-                      onClick={() => openGovRepresentatives(data?.Representatives || [])}
-                    >
-                      <SvgIcon sx={{ p: 1 }}>
-                        <PlusIcon />
-                      </SvgIcon>
-                    </IconButton>
-                    <Typography variant="subheader1" color="text.muted">
-                      <Trans>Connect</Trans>
-                    </Typography>
-                  </Stack>
-                ) : (
-                  <Link
-                    href={`${networkConfigs[representative.chainId].explorerLink}/address/${
-                      representative.representative
-                    }`}
-                  >
-                    <Stack direction="row" alignItems="center" gap={1}>
-                      <CompactableTypography
-                        variant="subheader1"
-                        compactMode={CompactMode.MD}
-                        compact
-                        sx={{ ml: 4 }}
-                      >
-                        {representative.representative}
-                      </CompactableTypography>
-                      <SvgIcon
-                        sx={(theme) => ({
-                          width: 14,
-                          height: 14,
-                          ml: 0.5,
-                          color: theme.palette.text.muted,
-                        })}
-                      >
-                        <ExternalLinkIcon />
-                      </SvgIcon>
-                    </Stack>
-                  </Link>
-                )}
-              </Stack>
-            ))}
+            {isAddressSelectedAsRepresentative ? (
+              <Representing representing={data?.Represented || []} />
+            ) : (
+              <Representatives
+                representatives={data?.Representatives || []}
+                onOpenRepresentatives={() => openGovRepresentatives(data?.Representatives || [])}
+              />
+            )}
           </Stack>
         </Stack>
       </Box>
@@ -123,6 +82,117 @@ export const RepresentativesInfoPanel = () => {
   );
 };
 
-// const Representing = () => {
+const Representatives = ({
+  representatives,
+  onOpenRepresentatives,
+}: {
+  representatives: Representative[];
+  onOpenRepresentatives: () => void;
+}) => {
+  return (
+    <>
+      {representatives.map((representative, i) => (
+        <Stack gap={4} key={i} direction="column" alignItems="self-start">
+          <Network
+            networkLogoPath={networkConfigs[representative.chainId].networkLogoPath}
+            networkName={networkConfigs[representative.chainId].name}
+          />
+          {representative.representative === ZERO_ADDRESS ? (
+            <Stack direction="row" gap={1} alignItems="center">
+              <IconButton
+                sx={(theme) => ({
+                  height: '24px',
+                  width: '24px',
+                  background: theme.palette.background.disabled,
+                })}
+                onClick={onOpenRepresentatives}
+              >
+                <SvgIcon sx={{ p: 1 }}>
+                  <PlusIcon />
+                </SvgIcon>
+              </IconButton>
+              <Typography variant="subheader1" color="text.muted">
+                <Trans>Connect</Trans>
+              </Typography>
+            </Stack>
+          ) : (
+            <AddressLink
+              explorerLink={networkConfigs[representative.chainId].explorerLink}
+              address={representative.representative}
+            />
+          )}
+        </Stack>
+      ))}
+    </>
+  );
+};
 
-// }
+const Representing = ({ representing }: { representing: Rpresented[] }) => {
+  return (
+    <>
+      {representing.map((representing, i) => (
+        <Stack gap={4} key={i} direction="column" alignItems="self-start">
+          <Network
+            networkLogoPath={networkConfigs[representing.chainId].networkLogoPath}
+            networkName={networkConfigs[representing.chainId].name}
+          />
+          {representing.votersRepresented.length === 0 ? (
+            <Typography sx={{ ml: 4 }} color="text.secondary">
+              <Trans>None</Trans>
+            </Typography>
+          ) : (
+            representing.votersRepresented.map((voter, i) => (
+              <AddressLink
+                key={i}
+                explorerLink={networkConfigs[representing.chainId].explorerLink}
+                address={voter}
+              />
+            ))
+          )}
+        </Stack>
+      ))}
+    </>
+  );
+};
+
+const Network = ({
+  networkLogoPath,
+  networkName,
+}: {
+  networkLogoPath: string;
+  networkName: string;
+}) => {
+  return (
+    <Stack direction="row" alignItems="center" gap={2}>
+      <img src={networkLogoPath} height="16px" width="16px" alt="network logo" />
+      <Typography variant="subheader1">{networkName}</Typography>
+    </Stack>
+  );
+};
+
+const AddressLink = ({ explorerLink, address }: { explorerLink: string; address: string }) => {
+  return (
+    <Link href={`${explorerLink}/address/${address}`}>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <CompactableTypography
+          variant="subheader1"
+          compactMode={CompactMode.MD}
+          compact
+          sx={{ ml: 4 }}
+        >
+          {address}
+        </CompactableTypography>
+        <SvgIcon
+          sx={(theme) => ({
+            width: 14,
+            height: 14,
+            ml: 0.5,
+            color: theme.palette.text.muted,
+          })}
+        >
+          <ExternalLinkIcon />
+        </SvgIcon>
+      </Stack>
+    </Link>
+  );
+};
