0b51f2a260eb92b3a5bd63da2543cae3d0c78c34
Mark Grothe <grothem@gmail.com>
2023-12-18 12:14:23 -0600

    fix: revert

--- src/ui-config/menu-items/index.tsx
+++ src/ui-config/menu-items/index.tsx
@@ -37,10 +37,10 @@ export const navigation: Navigation[] = [
     link: ROUTES.governance,
     title: t`Governance`,
     dataCy: 'menuGovernance',
-    // isVisible: () =>
-    //   process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true' &&
-    //   process.env.NEXT_PUBLIC_ENV === 'prod' &&
-    //   !ENABLE_TESTNET,
+    isVisible: () =>
+      process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true' &&
+      process.env.NEXT_PUBLIC_ENV === 'prod' &&
+      !ENABLE_TESTNET,
   },
   {
     link: ROUTES.faucet,
