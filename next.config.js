3c50cd76af21fafcd9a8c6e4426bc6541405bef3
Mark Grothe <grothem@gmail.com>
2023-12-18 13:26:17 -0600

    fix: deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const pageExtensions = ['page.tsx'];
-if (process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx');
+// if (process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx'); // TODO: put back in
+pageExtensions.push('governance.tsx');
 if (process.env.NEXT_PUBLIC_ENABLE_STAKING === 'true') pageExtensions.push('staking.tsx');
 
 /** @type {import('next').NextConfig} */
