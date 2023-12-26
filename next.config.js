7500cec96a2e3fb75267abde8bbf7746ca074387
JoaquinBattilana <battilanajn@gmail.com>
2023-12-26 11:26:53 -0300

    feat: next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const pageExtensions = ['page.tsx'];
-// if (process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx'); // TODO: put back in
+if (process.env.NEXT_PUBLIC_ENABLE_GOVERNANCE === 'true') pageExtensions.push('governance.tsx');
 pageExtensions.push('governance.tsx');
 if (process.env.NEXT_PUBLIC_ENABLE_STAKING === 'true') pageExtensions.push('staking.tsx');
 
