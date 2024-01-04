32985d17c5e984c28715b476b6d06e32d846bd7b
Nikita <right2maresko@gmail.com>
2024-01-04 20:14:15 +0000

    Feat/refactor test coverage (#1890)

--- cypress/e2e/0-v2-markets/2-polygon-v2-market/0-assets/dai.polygon-v2.cy.ts
+++ cypress/e2e/0-v2-markets/2-polygon-v2-market/0-assets/dai.polygon-v2.cy.ts
@@ -4,13 +4,13 @@ import { skipState } from '../../../../support/steps/common';
 import { configEnvWithTenderlyPolygonFork } from '../../../../support/steps/configuration.steps';
 import { borrow, repay, supply, withdraw } from '../../../../support/steps/main.steps';
 import { dashboardAssetValuesVerification } from '../../../../support/steps/verification.steps';
+import { RequestedTokens, tokenSet } from '../../../4-gho-ethereum/helpers/token.helper';
+
+const tokensToRequest: RequestedTokens = {
+  aMATICPolygonV2: 800,
+};
 
 const testData = {
-  depositBaseAmount: {
-    asset: assets.polygonMarket.MATIC,
-    amount: 800,
-    hasApproval: true,
-  },
   testCases: {
     borrow: {
       asset: assets.polygonMarket.DAI,
@@ -59,9 +59,7 @@ const testData = {
 
 describe('DAI INTEGRATION SPEC, POLYGON V2 MARKET', () => {
   const skipTestState = skipState(false);
-  configEnvWithTenderlyPolygonFork({});
-
-  supply(testData.depositBaseAmount, skipTestState, true);
+  configEnvWithTenderlyPolygonFork({ tokens: tokenSet(tokensToRequest) });
   borrow(testData.testCases.borrow, skipTestState, true);
   supply(testData.testCases.deposit, skipTestState, true);
   repay(testData.testCases.repay, skipTestState, false);
