b9826ea50841ed8d0e18c6be46ac36e0ad321870
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-03-11 15:46:36 +0000

    chore: favicons (#1823)

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -10,7 +10,14 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content="#1B2030" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="16x16" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="32x32" href="/favicon32.png" />
+          <link rel="shortcut icon" sizes="32x32" href="/favicon32.ico" />
+          <link rel="apple-touch-icon" sizes="64x64" href="/favicon64.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/aave_icon180.png" />
+
           <link rel="manifest" href="/manifest.json" />
           {
             // Inject MUI styles first to match with the prepend: true configuration.
