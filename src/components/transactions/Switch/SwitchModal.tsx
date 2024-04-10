51b43430f74d4d95fea8c4f58006591e9d773554
Joaquin Battilana <battilanajn@gmail.com>
2024-04-10 15:55:02 +0100

    feat: added support for connecting directly to wallet network

--- src/components/transactions/Switch/SwitchModal.tsx
+++ src/components/transactions/Switch/SwitchModal.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { ConnectWalletButton } from 'src/components/WalletConnection/ConnectWalletButton';
 import { TokenInfoWithBalance, useTokensBalance } from 'src/hooks/generic/useTokensBalance';
 import { ModalType, useModalContext } from 'src/hooks/useModal';
+import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
 import { useRootStore } from 'src/store/root';
 import { queryKeysFactory } from 'src/ui-config/queries';
 import { TOKEN_LIST, TokenInfo } from 'src/ui-config/TokenList';
@@ -122,6 +123,7 @@ export const SwitchModal = () => {
   } = useModalContext();
 
   const currentChainId = useRootStore((store) => store.currentChainId);
+  const { chainId: connectedChainId } = useWeb3Context();
   const user = useRootStore((store) => store.account);
 
   const [selectedChainId, setSelectedChainId] = useState(() => {
@@ -134,12 +136,20 @@ export const SwitchModal = () => {
     // Passing chainId as prop will set default network for switch modal
     if (chainId && supportedNetworksWithEnabledMarket.find((elem) => elem.chainId === chainId)) {
       setSelectedChainId(chainId);
+    } else if (
+      connectedChainId &&
+      supportedNetworksWithEnabledMarket.find((elem) => elem.chainId === connectedChainId)
+    ) {
+      const supportedFork = supportedNetworksWithEnabledMarket.find(
+        (elem) => elem.underlyingChainId === connectedChainId
+      );
+      setSelectedChainId(supportedFork ? supportedFork.chainId : connectedChainId);
     } else if (supportedNetworksWithEnabledMarket.find((elem) => elem.chainId === currentChainId)) {
       setSelectedChainId(currentChainId);
     } else {
       setSelectedChainId(defaultNetwork.chainId);
     }
-  }, [currentChainId, chainId]);
+  }, [currentChainId, chainId, connectedChainId]);
 
   return (
     <BasicModal open={type === ModalType.Switch} setOpen={close}>
