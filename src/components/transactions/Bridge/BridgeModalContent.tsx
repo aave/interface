eac38167f4e7ff4db1977dd3f76a80316b66a8b1
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-05-30 16:08:11 +0100

    Feat/bridge limit (#2064)
    Co-authored-by: Mark Grothe <grothem@gmail.com>
    Co-authored-by: miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>

--- src/components/transactions/Bridge/BridgeModalContent.tsx
+++ src/components/transactions/Bridge/BridgeModalContent.tsx
@@ -4,9 +4,11 @@ import { Box, Button, IconButton, SvgIcon, Typography } from '@mui/material';
 import BigNumber from 'bignumber.js';
 import { constants } from 'ethers';
 import React, { useEffect, useState } from 'react';
+import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
 import { Link, ROUTES } from 'src/components/primitives/Link';
 import { NoData } from 'src/components/primitives/NoData';
 import { Row } from 'src/components/primitives/Row';
+import { Warning } from 'src/components/primitives/Warning';
 import {
   DetailsNumberLine,
   TxModalDetails,
@@ -29,6 +31,7 @@ import { TxModalTitle } from '../FlowCommons/TxModalTitle';
 import { ChangeNetworkWarning } from '../Warnings/ChangeNetworkWarning';
 import { BridgeActions } from './BridgeActions';
 import { supportedNetworksWithBridgeMarket, SupportedNetworkWithChainId } from './common';
+import { useBridgingValues, useRateLimit } from './useGetBridgeLimits';
 import { useGetBridgeMessage } from './useGetBridgeMessage';
 
 // const defaultNetwork = marketsData[CustomMarket.proto_mainnet_v3];
@@ -124,12 +127,25 @@ export const BridgeModalContent = () => {
     bridgeFee,
     bridgeFeeFormatted,
     loading: loadingBridgeMessage,
+    latestAnswer: bridgeFeeUSD,
   } = useGetBridgeMessage({
     sourceChainId: sourceNetworkObj.chainId,
     destinationChainId: destinationNetworkObj?.chainId || 0,
     amount,
     sourceTokenAddress: sourceTokenInfo.address || '',
   });
+
+  const bridgingValues = useBridgingValues(sourceNetworkObj.chainId);
+
+  const rateLimit = useRateLimit({
+    destinationChainId: destinationNetworkObj?.chainId || 0,
+    sourceChainId: sourceNetworkObj.chainId,
+  });
+
+  const bridgeLimitExceeded =
+    bridgingValues &&
+    bridgingValues.currentAmountBridged + parseInt(amount, 10) >= bridgingValues.maxAmountBridged;
+  const rateLimitExceeded = rateLimit !== 0 && parseInt(amount, 10) >= (rateLimit || 0);
 
   const handleSelectedNetworkChange =
     (networkAction: string) => (network: SupportedNetworkWithChainId) => {
@@ -185,7 +201,7 @@ export const BridgeModalContent = () => {
     isWrongNetwork,
     // poolAddress: GHO.underlying,
     symbol: 'GHO',
-    blocked: loadingBridgeMessage,
+    blocked: loadingBridgeMessage || bridgeLimitExceeded || rateLimitExceeded,
     decimals: 18,
     isWrappedBaseAsset: false,
     message,
@@ -201,20 +217,9 @@ export const BridgeModalContent = () => {
       <TxSuccessView
         customAction={
           <Box mt={5}>
-            {/* <Button
-                component="a"
-                target="_blank"
-                href={`https://ccip.chain.link/tx/${bridgeTxState.txHash}`}
-                variant="gradient"
-                size="medium"
-              >
-                <Trans>See Transaction status on CCIP</Trans>
-              </Button> */}
-
             <Button
               component={Link}
               href={ROUTES.bridge}
-              // sx={{ mr: 8, mb: '24px' }}
               variant="outlined"
               size="small"
               onClick={close}
@@ -355,13 +360,25 @@ export const BridgeModalContent = () => {
                   value={amount}
                 />
                 {message || loadingBridgeMessage ? (
-                  <DetailsNumberLine
-                    description={<Trans>Fee</Trans>}
-                    iconSymbol={'ETH'}
-                    symbol={'ETH'}
-                    value={bridgeFeeFormatted}
-                    loading={loadingBridgeMessage}
-                  />
+                  <>
+                    <DetailsNumberLine
+                      description={<Trans>Fee</Trans>}
+                      iconSymbol={'ETH'}
+                      symbol={'ETH'}
+                      value={bridgeFeeFormatted}
+                      loading={loadingBridgeMessage}
+                      customMb={0}
+                    />
+                    <Box display={'flex'} justifyContent={'flex-end'}>
+                      <FormattedNumber
+                        value={bridgeFeeUSD}
+                        variant="helperText"
+                        compact
+                        symbol="USD"
+                        color="text.secondary"
+                      />
+                    </Box>
+                  </>
                 ) : (
                   <Row caption={<Trans>Fee</Trans>} captionVariant="description" mb={4}>
                     <NoData variant="secondary14" color="text.secondary" />
@@ -370,8 +387,31 @@ export const BridgeModalContent = () => {
               </TxModalDetails>
             </Box>
           </Box>
-
           {txError && <GasEstimationError txError={txError} />}
+
+          {bridgeLimitExceeded && (
+            <Warning severity="error" sx={{ mt: 4 }} icon={false}>
+              <Typography variant="caption">
+                <Trans>
+                  The selected amount is not available to bridge due to the bridge limit of
+                </Trans>{' '}
+                {bridgingValues.maxAmountBridged - bridgingValues.currentAmountBridged}.{' '}
+                <Trans>Please try again later or reduce the amount to bridge</Trans>
+              </Typography>
+            </Warning>
+          )}
+
+          {rateLimitExceeded && (
+            <Warning severity="error" sx={{ mt: 4 }} icon={false}>
+              <Typography variant="caption">
+                <Trans>
+                  The selected amount is not available to bridge due to CCIP rate limit of
+                </Trans>
+                <Trans>Please try again later or reduce the amount to bridge to less than</Trans>{' '}
+                {rateLimit}
+              </Typography>
+            </Warning>
+          )}
 
           <BridgeActions {...bridgeActionsProps} />
         </>
---
+++ src/components/transactions/Bridge/useGetBridgeLimits.tsx
@@ -0,0 +1,93 @@
+import { Contract } from 'ethers';
+import { formatUnits } from 'ethers/lib/utils';
+import { useEffect, useState } from 'react';
+import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
+
+import { getChainSelectorFor, laneConfig } from './BridgeConfig';
+// NOTE: lightweight ABI
+import TokenPoolAbi from './Tokenpool-abi.json';
+
+export const useBridgingValues = (sourceChainId: number) => {
+  const { provider } = useWeb3Context();
+
+  const [bridgingValues, setBridgingValues] = useState({
+    currentAmountBridged: 0,
+    maxAmountBridged: 0,
+  });
+
+  useEffect(() => {
+    if (!provider) {
+      throw Error('Provider is not available');
+    }
+
+    const sourceLaneConfig = laneConfig.find((config) => config.sourceChainId === sourceChainId);
+    if (!sourceLaneConfig) {
+      throw Error('No sourceLaneConfig found');
+    }
+
+    const tokenPoolAddress = sourceLaneConfig.tokenPool;
+    const signer = provider.getSigner();
+    const tokenPool = new Contract(tokenPoolAddress, TokenPoolAbi, signer);
+
+    async function fetchBridgingValues() {
+      try {
+        const maxAmountBridged = await tokenPool.getBridgeLimit();
+        const currentAmountBridged = await tokenPool.getCurrentBridgedAmount();
+        setBridgingValues({
+          maxAmountBridged: parseInt(formatUnits(maxAmountBridged, 18), 10),
+          currentAmountBridged: parseInt(formatUnits(currentAmountBridged, 18), 10),
+        });
+      } catch (error) {
+        console.error('Error fetching bridging values:', error);
+      }
+    }
+
+    fetchBridgingValues();
+  }, [sourceChainId, provider]);
+
+  return bridgingValues;
+};
+
+// NOTE: Currently useRateLimit is not set on governance
+interface RateLimitProps {
+  destinationChainId: number;
+  sourceChainId: number;
+}
+export const useRateLimit = ({ destinationChainId, sourceChainId }: RateLimitProps) => {
+  const [rateLimit, setRateLimit] = useState(0);
+
+  const { provider } = useWeb3Context();
+
+  useEffect(() => {
+    if (!provider) {
+      console.error('Provider is not available');
+      return;
+    }
+
+    const sourceLaneConfig = laneConfig.find((config) => config.sourceChainId === sourceChainId);
+    if (!sourceLaneConfig) {
+      throw Error('No sourceLaneConfig found');
+    }
+
+    const tokenPoolAddress = sourceLaneConfig.tokenPool;
+    const signer = provider.getSigner();
+    const tokenPool = new Contract(tokenPoolAddress, TokenPoolAbi, signer);
+    const destinationChainSelector = getChainSelectorFor(destinationChainId);
+
+    async function fetchRateLimit() {
+      try {
+        const [rate, , isEnabled, ,] = await tokenPool.getCurrentOutboundRateLimiterState(
+          destinationChainSelector
+        );
+        if (!isEnabled) rate = 0;
+        setRateLimit(parseInt(rate.toString(), 10));
+      } catch (error) {
+        console.error('Error fetching rate limit:', error);
+      }
+    }
+
+    fetchRateLimit();
+  }, [destinationChainId, sourceChainId, provider]);
+
+  return rateLimit;
+};
