83947f503cd0f1c11a61a2b38009238a248c5230
Mark Grothe <grothem@gmail.com>
2024-06-06 11:02:13 -0500

    fix: add default finality time

--- src/components/transactions/Bridge/useGetFinalityTime.tsx
+++ src/components/transactions/Bridge/useGetFinalityTime.tsx
@@ -1,6 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { getProvider } from 'src/utils/marketsAndNetworksConfig';
+
+dayjs.extend(relativeTime);
+
+// Used in case there's an error fetching the latest finalized block, it should give a close estimate for Arb or Eth source chains
+const DEFAULT_FINALITY_TIME = 1080; // 18 minutes
 
 export const useTimeToDestination = (sourceChainId: number) => {
   return useQuery({
@@ -10,12 +16,11 @@ export const useTimeToDestination = (sourceChainId: number) => {
         const block = await provider.send('eth_getBlockByNumber', ['finalized', false]);
         const timestamp = parseInt(block.timestamp, 16);
         const now = dayjs().unix();
-        console.log('timestamp', timestamp);
         const estimatedTimeToDestination = dayjs.unix(now + (now - timestamp) + 120).fromNow();
         return estimatedTimeToDestination;
       } catch (error) {
         console.error('Error fetching finality time', error);
-        return 'TODO'; // need to add some reasonable defaults as a fallback
+        return dayjs.unix(dayjs().unix() + DEFAULT_FINALITY_TIME).fromNow();
       }
     },
     queryKey: ['getFinalityTime', sourceChainId],
