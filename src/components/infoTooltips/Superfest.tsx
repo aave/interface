dbf0752e1a729463da886401632f2cfbc1ee69c1
Nandy Bâ <nandyba@yahoo.fr>
2024-07-08 21:41:08 +0200

    fix: change Image for html img component

--- src/components/infoTooltips/Superfest.tsx
+++ src/components/infoTooltips/Superfest.tsx
@@ -1,5 +1,4 @@
 import { Link, Tooltip } from '@mui/material';
-import Image from 'next/image';
 
 export const SuperfestTooltip = () => (
   <Tooltip title={`This position is eligible for Superfest`} arrow placement="top">
@@ -9,7 +8,7 @@ export const SuperfestTooltip = () => (
       onClick={(e) => e.stopPropagation()}
       style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
     >
-      <Image src={'/icons/other/superfest.svg'} height={25} width={25} />
+      <img src={'/icons/other/superfest.svg'} height={25} width={25} />
     </Link>
   </Tooltip>
 );
