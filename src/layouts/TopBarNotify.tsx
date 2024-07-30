886be09aea610299db4872e0107e61702ab6bd59
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-07-30 19:41:41 +0100

    Feat/update banner logic (#2128)

--- src/layouts/TopBarNotify.tsx
+++ src/layouts/TopBarNotify.tsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { ReactNode, useEffect, useState } from 'react';
+import { MarketLogo } from 'src/components/MarketSwitcher';
 import { Link } from 'src/components/primitives/Link';
 import { useRootStore } from 'src/store/root';
 
@@ -14,22 +15,37 @@ interface TopBarNotifyProps {
   notifyText: ReactNode;
   learnMoreLink?: string;
   buttonText?: string;
+  bannerVersion: string;
+  icon?: string;
 }
 
-export default function TopBarNotify({ notifyText, learnMoreLink, buttonText }: TopBarNotifyProps) {
+export default function TopBarNotify({
+  notifyText,
+  learnMoreLink,
+  buttonText,
+  bannerVersion,
+  icon,
+}: TopBarNotifyProps) {
   const { breakpoints } = useTheme();
   const md = useMediaQuery(breakpoints.down('md'));
+  const sm = useMediaQuery(breakpoints.down('sm'));
 
   const [showWarning, setShowWarning] = useState(true);
 
   const [mobileDrawerOpen] = useRootStore((state) => [state.mobileDrawerOpen]);
 
   useEffect(() => {
+    const storedBannerVersion = localStorage.getItem('bannerVersion');
     const warningBarOpen = localStorage.getItem('warningBarOpen');
-    if (warningBarOpen && warningBarOpen === 'false') {
+
+    if (storedBannerVersion !== bannerVersion) {
+      localStorage.setItem('bannerVersion', bannerVersion);
+      setShowWarning(true);
+      localStorage.setItem('warningBarOpen', 'true');
+    } else if (warningBarOpen === 'false') {
       setShowWarning(false);
     }
-  }, []);
+  }, [bannerVersion]);
 
   const handleClose = () => {
     localStorage.setItem('warningBarOpen', 'false');
@@ -64,8 +80,13 @@ export default function TopBarNotify({ notifyText, learnMoreLink, buttonText }: 
           variant="dense"
         >
           <Box sx={{ padding: md ? '20px 10px' : '', paddingRight: 0 }}>
-            <Typography component="div">
+            <Typography
+              sx={{ display: 'flex', alignContent: 'center', alignItems: 'center' }}
+              component="div"
+            >
               <Trans>{notifyText}</Trans>
+
+              {icon && !sm ? <MarketLogo sx={{ ml: 2 }} size={32} logo={icon} /> : ''}
 
               {learnMoreLink && md ? (
                 <Link
