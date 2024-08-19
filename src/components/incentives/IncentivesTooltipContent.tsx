86086f079a280b038f86e35bf845810e0297a371
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-08-19 12:12:25 -0400

    Fix/avax fixes [skip cypress] (#2159)

--- src/components/incentives/IncentivesTooltipContent.tsx
+++ src/components/incentives/IncentivesTooltipContent.tsx
@@ -57,6 +57,7 @@ export const IncentivesTooltipContent = ({
 
   const getSymbolMap = (incentive: ReserveIncentiveResponse) => {
     const rewardTokenSymbol = incentive.rewardTokenSymbol;
+
     return IncentivesSymbolMap[rewardTokenSymbol]
       ? {
           ...IncentivesSymbolMap[rewardTokenSymbol],
@@ -112,7 +113,7 @@ export const IncentivesTooltipContent = ({
         ))}
 
         {incentives.length > 1 && (
-          <Box sx={(theme) => ({ pt: 1, mt: 1, border: `1px solid ${theme.palette.divider}` })}>
+          <Box sx={() => ({ pt: 1, mt: 1 })}>
             <Row caption={<Trans>Net APR</Trans>} height={32}>
               <Number incentiveAPR={incentivesNetAPR} />
             </Row>
