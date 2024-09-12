11d570f14e5412aeddbe7fb35e65f5b5a41e9d65
QuyTran <168699946+Sotatek-QuyTran2@users.noreply.github.com>
2024-09-12 17:15:03 +0700

    check disabled when borrow

--- src/modules/dashboard/lists/BorrowAssetsList/BorrowAssetsListItem.tsx
+++ src/modules/dashboard/lists/BorrowAssetsList/BorrowAssetsListItem.tsx
@@ -15,6 +15,7 @@ import { ListAPRColumn } from '../ListAPRColumn';
 import { ListButtonsColumn } from '../ListButtonsColumn';
 import { ListItemWrapper } from '../ListItemWrapper';
 import { ListValueColumn } from '../ListValueColumn';
+import { useAssetCaps } from 'src/hooks/useAssetCaps';
 
 export const BorrowAssetsListItem = ({
   symbol,
@@ -38,7 +39,9 @@ export const BorrowAssetsListItem = ({
   const theme = useTheme();
   const { isConnectedTonWallet } = useTonConnectContext();
 
-  const disableBorrow = isFreezed || Number(availableBorrows) <= 0;
+  const assetCaps = useAssetCaps();
+
+  const disableBorrow = isFreezed || Number(availableBorrows) <= 0 || assetCaps.borrowCap.isMaxed;
 
   const trackEvent = useRootStore((store) => store.trackEvent);
 
