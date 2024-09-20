44ac38f8b427bea7379631a5c192edce455bcf48
Nandy Bâ <nandyba@yahoo.fr>
2024-09-20 17:05:09 +0900

    feat: temporary disable whitelist (#2)
    access forbiden to access the API

--- src/hooks/useAddressAllowed.tsx
+++ src/hooks/useAddressAllowed.tsx
@@ -11,21 +11,8 @@ const TWO_MINUTES = 2 * 60 * 1000;
 export const useAddressAllowed = (address: string): AddressAllowedResult => {
   const [isAllowed, setIsAllowed] = useState(true);
 
-  const screeningUrl = `https://aave.com/addresses/status`;
-  const queryParams = `?address=${address}`;
-
   const getIsAddressAllowed = async () => {
-    if (screeningUrl && address) {
-      try {
-        const response = await fetch(screeningUrl + queryParams);
-        if (response.ok) {
-          const data: { addressAllowed: boolean } = await response.json();
-          setIsAllowed(data.addressAllowed);
-        }
-      } catch (e) {}
-    } else {
-      setIsAllowed(true);
-    }
+    setIsAllowed(true);
   };
 
   usePolling(getIsAddressAllowed, TWO_MINUTES, false, [address]);
