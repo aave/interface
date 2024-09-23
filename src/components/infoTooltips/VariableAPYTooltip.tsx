94a5dc8887b0967fc59ddce9894f35649a32af3f
Nandy Bâ <nandyba@yahoo.fr>
2024-09-23 23:15:06 +0900

    feat: update variable rate info tooltip (#2185)

--- src/components/infoTooltips/VariableAPYTooltip.tsx
+++ src/components/infoTooltips/VariableAPYTooltip.tsx
@@ -6,8 +6,7 @@ export const VariableAPYTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        Variable interest rate will <b>fluctuate</b> based on the market conditions. Recommended for
-        short-term positions.
+        Variable interest rate will <b>fluctuate</b> based on the market conditions.
       </Trans>
     </TextWithTooltip>
   );
