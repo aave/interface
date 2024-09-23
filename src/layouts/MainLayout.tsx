4dfb4279f7ea32cbbb2034093bafc94b0c1f890f
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-09-23 16:53:48 +0200

    chore: market banner [skip cypress] (#2186)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,16 +9,16 @@ import { AppHeader } from './AppHeader';
 import TopBarNotify from './TopBarNotify';
 
 export function MainLayout({ children }: { children: ReactNode }) {
-  const APP_BANNER_VERSION = '2.0.0';
+  const APP_BANNER_VERSION = '3.0.0';
 
   return (
     <>
       <TopBarNotify
-        learnMoreLink="/markets/?marketName=proto_etherfi_v3"
+        learnMoreLink="/markets/?marketName=proto_zksync_v3"
         buttonText="View Market"
-        notifyText="Aave Governance has deployed a new EtherFi market on Ethereum V3"
+        notifyText="Aave Governance has deployed a new ZkSync market"
         bannerVersion={APP_BANNER_VERSION}
-        icon={'/icons/markets/etherfi.svg'}
+        icon={'/icons/networks/zksync.svg'}
       />
       <AppHeader />
       <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
