4e34a3165ba9044b3ee3f4a8e180a6db3f0b50ec
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-17 12:32:25 -0300

    fix: update metadata

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -11,16 +11,16 @@ type MetaProps = {
 export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
   return (
     <Head>
-      <title>Aave - {title}</title>
+      <title>Zeebu - {title}</title>
       <meta name="description" content={description} key="description" />
-      <meta property="og:title" content={`Aave - ${title}`} key="title" />
+      <meta property="og:title" content={`Zeebu - ${title}`} key="title" />
       <meta property="og:description" content={description} key="ogdescription" />
       {imageUrl && <meta property="og:image" content={imageUrl} key="ogimage" />}
       {imageUrl && <meta name="twitter:image" content={imageUrl} key="twitterimage" />}
       {imageUrl && (
         <meta name="twitter:image:alt" content={`aave governance image`} key="twitteralt" />
       )}
-      <meta name="twitter:site" content="@AaveAave" key="twittersite" />
+      <meta name="twitter:site" content="@zeebuofficial" key="twittersite" />
       <meta
         property="twitter:card"
         content={imageUrl ? 'summary_large_image' : 'summary'}
@@ -32,7 +32,7 @@ export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
       <meta
         name="keywords"
         key="keywords"
-        content="Decentralized Finance, DeFi, lending, borrowing, stablecoins, Ethereum, assets, erc-20, smart contracts, open finance, trustless"
+        content="Decentralized Finance, Zeebu, DeFi, lending, borrowing, stablecoins, Ethereum, assets, erc-20, smart contracts, open finance, trustless"
       />
       <link rel="apple-touch-icon" href="/aave_icon180.png" />
       <meta name="apple-mobile-web-app-title" content={`Aave`} />
