df11ed952e5324fb708878565f43a2a13aec26e8
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-10-23 15:47:49 +0100

    fix: typo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -15,7 +15,7 @@ export function MainLayout({ children }: { children: ReactNode }) {
     <>
       <TopBarNotify
         learnMoreLink="/markets/?marketName=proto_mainnet_v3"
-        notifyText="Merit incentives are available for users who both supply chBTC and borrow USDC."
+        notifyText="Merit incentives are available for users who both supply cbBTC and borrow USDC."
         bannerVersion={APP_BANNER_VERSION}
       />
       <AppHeader />
