a971fef560e61c159bb7eb620b40e78971ec5ed8
Mark Grothe <grothem@gmail.com>
2024-10-30 12:19:24 -0500

    fix: use legacy providers for testnets (#2234)

--- src/services/UIPoolService.ts
+++ src/services/UIPoolService.ts
@@ -7,6 +7,7 @@ import {
 } from '@aave/contract-helpers';
 import { Provider } from '@ethersproject/providers';
 import { MarketDataType } from 'src/ui-config/marketsConfig';
+import { ENABLE_TESTNET } from 'src/utils/marketsAndNetworksConfig';
 
 export type UserReservesDataHumanized = {
   userReserves: UserReserveDataHumanized[];
@@ -35,6 +36,7 @@ export class UiPoolService {
 
   private useLegacyUiPoolDataProvider(marketData: MarketDataType) {
     if (
+      ENABLE_TESTNET ||
       !marketData.v3 ||
       marketData.marketTitle === 'Fantom' ||
       marketData.marketTitle === 'Harmony'
