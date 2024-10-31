b6de0bea4c8fd4a4dfd3bbbf5dd1cd8ec26fafd7
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-10-31 15:08:28 +0000

    fix: undefined in usetokenbalance

--- src/hooks/generic/useTokensBalance.ts
+++ src/hooks/generic/useTokensBalance.ts
@@ -19,59 +19,57 @@ export const useTokensBalance = (tokenList: TokenInfo[], chainId: number, user: 
       const tokensWithoutNative = tokenList.filter((elem) => !elem.extensions?.isNative);
       const nativeToken = tokenList.find((token) => token.extensions?.isNative);
       const multicall = new Multicall({
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
         ethersProvider: provider as unknown as providers.Provider,
         tryAggregate: true,
         multicallCustomContractAddress: '0xcA11bde05977b3631167028862bE2a173976CA11',
       });
-      const contractCallContext = tokensWithoutNative.map((token) => {
-        return {
-          reference: token.address,
-          contractAddress: token.address,
-          abi: [
-            {
-              name: 'balanceOf',
-              type: 'function',
-              stateMutability: 'view',
-              inputs: [{ name: 'account', type: 'address' }],
-              outputs: [{ name: 'balance', type: 'uint256' }],
-            },
-          ],
-          calls: [
-            { reference: 'balanceOfCall', methodName: 'balanceOf', methodParameters: [user] },
-          ],
-        };
-      });
+      const contractCallContext = tokensWithoutNative.map((token) => ({
+        reference: token.address,
+        contractAddress: token.address,
+        abi: [
+          {
+            name: 'balanceOf',
+            type: 'function',
+            stateMutability: 'view',
+            inputs: [{ name: 'account', type: 'address' }],
+            outputs: [{ name: 'balance', type: 'uint256' }],
+          },
+        ],
+        calls: [{ reference: 'balanceOfCall', methodName: 'balanceOf', methodParameters: [user] }],
+      }));
+
       if (!nativeToken) {
         const { results } = await multicall.call(contractCallContext);
         return tokenList
-          .map((elem, index) => ({
-            ...elem,
-            balance: formatUnits(
-              results[index].callsReturnContext[0].returnValues[0],
-              elem.decimals
-            ),
-          }))
+          .map((elem, index) => {
+            const balanceValue = results[index]?.callsReturnContext[0]?.returnValues[0];
+            return {
+              ...elem,
+              balance: balanceValue ? formatUnits(balanceValue, elem.decimals) : '0',
+            };
+          })
           .sort((a, b) => Number(b.balance) - Number(a.balance));
       }
+
       const [balanceResult, multicallResult] = await Promise.all([
         provider.getBalance(user),
         multicall.call(contractCallContext),
       ]);
+
       return tokenList
         .map((elem) => {
+          const balanceValue = elem.extensions?.isNative
+            ? balanceResult
+            : multicallResult.results[elem.address]?.callsReturnContext[0]?.returnValues[0];
           return {
             ...elem,
-            balance: elem.extensions?.isNative
-              ? formatUnits(balanceResult, elem.decimals)
-              : formatUnits(
-                  multicallResult.results[elem.address].callsReturnContext[0].returnValues[0],
-                  elem.decimals
-                ),
+            balance: balanceValue ? formatUnits(balanceValue, elem.decimals) : '0',
           };
         })
         .sort((a, b) => Number(b.balance) - Number(a.balance));
+    },
+    onError: (error) => {
+      console.error('Error fetching token balances:', error);
     },
   });
 };
