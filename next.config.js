4881d9bcb72264c99c1939fe74cb3349ee9b7181
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-12-03 10:09:39 +0000

    fix: safe local

--- next.config.js
+++ next.config.js
@@ -31,4 +31,26 @@ module.exports = withBundleAnalyzer({
   // assetPrefix: "./",
   trailingSlash: true,
   pageExtensions,
+  // NOTE: Needed for SAFE testing locally
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/manifest.json',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'X-Requested-With, content-type, Authorization',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 });
