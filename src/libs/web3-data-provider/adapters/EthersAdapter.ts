71f8fe2551f4bd7b6d9c409bf2a4b5013f70fea3
Mark Grothe <grothem@gmail.com>
2024-12-11 09:49:31 -0600

    fix: connect kit/wagmi fixes

--- src/libs/web3-data-provider/adapters/EthersAdapter.ts
+++ src/libs/web3-data-provider/adapters/EthersAdapter.ts
@@ -5,17 +5,7 @@ import { Config, useClient, useConnectorClient } from 'wagmi';
 
 export function clientToProvider(client: Client<Transport, Chain>) {
   const { chain, transport } = client;
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  };
-  // if (transport.type === 'fallback')
-  //   return new providers.FallbackProvider(
-  //     (transport.transports as ReturnType<Transport>[]).map(
-  //       ({ value }) => new providers.JsonRpcProvider(value?.url, network)
-  //     )
-  //   );
+  const network = getNetwork(chain);
   return new providers.JsonRpcProvider(transport.url, network);
 }
 
@@ -27,11 +17,7 @@ export function useEthersProvider({ chainId }: { chainId?: number | undefined } 
 
 export function clientToSigner(client: Client<Transport, Chain, Account>) {
   const { account, chain, transport } = client;
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  };
+  const network = getNetwork(chain);
   const provider = new providers.Web3Provider(transport, network);
   const signer = provider.getSigner(account.address);
   return signer;
@@ -41,4 +27,14 @@ export function clientToSigner(client: Client<Transport, Chain, Account>) {
 export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
   const { data: client } = useConnectorClient<Config>({ chainId });
   return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
+}
+
+function getNetwork(chain: Chain) {
+  // if chain is undefined, it means that the client is connected to a chain that is not supported by the app
+  const network = {
+    chainId: chain?.id || -1,
+    name: chain?.name || 'unknown',
+    ensAddress: chain?.contracts?.ensRegistry?.address || undefined,
+  };
+  return network;
 }
