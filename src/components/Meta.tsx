b8f2c71aa2ffe85b2054dc1f5eb3cb90452ee288
Mark Hinschberger <foodaka@users.noreply.github.com>
2025-01-07 11:30:17 +0000

    Fix/update icon (#2307)

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -35,7 +35,7 @@ export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
         key="keywords"
         content="Decentralized Finance, DeFi, lending, borrowing, stablecoins, Ethereum, assets, erc-20, smart contracts, open finance, trustless"
       />
-      <link rel="apple-touch-icon" href="/aave_icon180.png" />
+      <link rel="apple-touch-icon" href="/aave_180.png" />
       <meta name="apple-mobile-web-app-title" content={`Aave`} />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
