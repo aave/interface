7a4423988d126963741801650e620d859db6acba
defispartan <andrew@aave.com>
2025-01-14 21:41:25 -0600

    feat: dynamic query for all assets

--- src/modules/markets/index-current-query.ts
+++ src/modules/markets/index-current-query.ts
@@ -1,14 +1,31 @@
-export const INDEX_CURRENT = `
-query IndexCurrent {
-  reserveParamsHistoryItems(orderBy: timestamp, orderDirection: desc){
-    liquidityIndex
-    liquidityRate
-    variableBorrowRate
-    variableBorrowIndex
-    timestamp
-    reserve{
-		  underlyingAsset
+import { generateAliases } from "src/utils/generateSubgraphQueryAlias";
+
+export const constructIndexCurrentQuery = (underlyingAssets: string[]): string => {
+  const aliases = generateAliases(underlyingAssets.length);
+
+  const queries = underlyingAssets.map(
+    (asset, index) => `
+      ${aliases[index]}: reserveParamsHistoryItems(
+        orderBy: timestamp,
+        orderDirection: desc,
+        first: 1,
+        where: { reserve_: { underlyingAsset: "${asset.toLowerCase()}" } }
+      ) {
+        liquidityIndex
+        liquidityRate
+        variableBorrowRate
+        variableBorrowIndex
+        timestamp
+        reserve {
+          underlyingAsset
+        }
+      }
+    `
+  );
+
+  return `
+    query IndexCurrent {
+      ${queries.join("\n")}
     }
-  }
-}
-`;
+  `;
+};
--- src/modules/markets/index-history-query.ts
+++ src/modules/markets/index-history-query.ts
@@ -1,14 +1,31 @@
-export const INDEX_HISTORY = `
-query IndexHistory($timestamp: Int!) {
-  reserveParamsHistoryItems(where:{timestamp_lt: $timestamp}, orderBy: timestamp, orderDirection: desc){
-    liquidityIndex
-    liquidityRate
-    variableBorrowRate
-    variableBorrowIndex
-    timestamp
-    reserve{
-		  underlyingAsset
+import { generateAliases } from "src/utils/generateSubgraphQueryAlias";
+
+export const constructIndexHistoryQuery = (underlyingAssets: string[]): string => {
+  const aliases = generateAliases(underlyingAssets.length);
+
+  const queries = underlyingAssets.map(
+    (asset, index) => `
+      ${aliases[index]}: reserveParamsHistoryItems(
+        where: { timestamp_lt: $timestamp, reserve_: { underlyingAsset: "${asset.toLowerCase()}" } },
+        orderBy: timestamp,
+        orderDirection: desc,
+        first: 1
+      ) {
+        liquidityIndex
+        liquidityRate
+        variableBorrowRate
+        variableBorrowIndex
+        timestamp
+        reserve {
+          underlyingAsset
+        }
+      }
+    `
+  );
+
+  return `
+    query IndexHistory($timestamp: Int!) {
+      ${queries.join("\n")}
     }
-  }
-}
-`;
+  `;
+};
