43ef57e38448491c79da2c8cd417093231c1ab79
Mark Grothe <grothem@gmail.com>
2025-01-17 08:42:51 -0600

    feat: multi tokens

--- src/modules/umbrella/helpers/Helpers.tsx
+++ src/modules/umbrella/helpers/Helpers.tsx
@@ -6,7 +6,8 @@ import { ContentWithTooltip } from 'src/components/ContentWithTooltip';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
 import { Row } from 'src/components/primitives/Row';
 
-import { TokenIcon } from '../../../components/primitives/TokenIcon';
+import { MultiTokenIcon, TokenIcon } from '../../../components/primitives/TokenIcon';
+import { MultiIcon, MultiIconWithTooltip } from './MultiIcon';
 
 export const UmbrellaAssetBreakdown = ({
   market,
@@ -26,14 +27,15 @@ export const UmbrellaAssetBreakdown = ({
   underlyingTokenDecimals: number;
   // protocolAction?: ProtocolAction;
 }) => {
-  const [open, setOpen] = useState(false);
   // const { data: zkSyncIgniteIncentives } = useZkSyncIgniteIncentives(params);
 
   return (
-    <ContentWithTooltip
+    <MultiIconWithTooltip
+      icons={[
+        { src: symbol, aToken: false },
+        { src: symbol, aToken: true },
+      ]}
       tooltipContent={
-        // <ZkSyncIgniteIncentivesTooltipContent zkSyncIgniteIncentives={zkSyncIgniteIncentives} />
-
         <Box
           sx={{
             display: 'flex',
@@ -135,33 +137,139 @@ export const UmbrellaAssetBreakdown = ({
           </Box>
         </Box>
       }
-      withoutHover
-      setOpen={setOpen}
-      open={open}
-    >
-      <Box
-        sx={(theme) => ({
-          p: { xs: '0 4px', xsm: '2px 4px' },
-          //   border: `1px solid ${open ? theme.palette.action.disabled : theme.palette.divider}`,
-          //   borderRadius: '4px',
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          transition: 'opacity 0.2s ease',
-          bgcolor: open ? 'action.hover' : 'transparent',
-          '&:hover': {
-            bgcolor: 'action.hover',
-            borderColor: 'action.disabled',
-          },
-        })}
-        onClick={() => {
-          // TODO: How to handle this for event props?
-          setOpen(!open);
-        }}
-      >
-        <TokenIcon symbol={`${symbol}_${symbol}`} aTokens={[true, false]} fontSize="medium" />
-      </Box>
-    </ContentWithTooltip>
+    />
   );
+};
+
+export const AssetsAvaialbeToStake = ({
+  symbol,
+  underlyingIsWaToken,
+  underlying,
+  underlyingTokenBalance,
+  waTokenUnderlying,
+  waTokenUnderlyingBalance,
+  waTokenAToken,
+  waTokenATokenBalance,
+}: {
+  symbol: string;
+  underlyingIsWaToken: boolean;
+  underlying: string;
+  underlyingTokenBalance: string;
+  waTokenUnderlying: string;
+  waTokenUnderlyingBalance: string;
+  waTokenAToken: string;
+  waTokenATokenBalance: string;
+}) => {
+  if (underlyingIsWaToken) {
+    return (
+      <MultiIconWithTooltip
+        icons={[
+          { src: 'USDC', aToken: false },
+          { src: 'USDT', aToken: true },
+          { src: 'AAVE', aToken: false },
+        ]}
+        tooltipContent={
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            <Typography variant="caption" color="text.secondary" mb={3}>
+              <Trans>
+                Participating in staking gives annualized rewards. Your wallet balance is the sum of
+                your aTokens and underlying assets. The breakdown to stake is below
+              </Trans>
+            </Typography>
+            <Box sx={{ width: '100%' }}>
+              <Row
+                height={32}
+                caption={
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      // mb: incentives.length > 1 ? 2 : 0,
+                    }}
+                  >
+                    <TokenIcon aToken={false} symbol={symbol} sx={{ fontSize: '20px', mr: 1 }} />
+                  </Box>
+                }
+                width="100%"
+              >
+                <FormattedNumber
+                  value={normalize(Number(underlyingTokenBalance), underlyingTokenDecimals)}
+                  compact
+                  variant="main16"
+                  values={underlyingTokenBalance}
+                />
+              </Row>
+              <Row
+                height={32}
+                caption={
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      // mb: incentives.length > 1 ? 2 : 0,
+                    }}
+                  >
+                    <TokenIcon aToken={true} symbol={symbol} sx={{ fontSize: '20px', mr: 1 }} />
+                  </Box>
+                }
+                width="100%"
+              >
+                <FormattedNumber
+                  value={normalize(Number(underlyingWaTokenBalance), underlyingTokenDecimals)}
+                  compact
+                  variant="main16"
+                  values={underlyingWaTokenBalance}
+                />
+              </Row>
+              <Row
+                height={32}
+                caption={
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      // mb: incentives.length > 1 ? 2 : 0,
+                    }}
+                  >
+                    <TokenIcon aToken={true} symbol={symbol} sx={{ fontSize: '20px', mr: 1 }} />
+                  </Box>
+                }
+                width="100%"
+              >
+                <FormattedNumber
+                  value={normalize(Number(underlyingWaTokenATokenBalance), underlyingTokenDecimals)}
+                  compact
+                  variant="main16"
+                  values={underlyingWaTokenATokenBalance}
+                />
+              </Row>
+              <Box sx={() => ({ pt: 1, mt: 1 })}>
+                <Row caption={<Trans>Total</Trans>} height={32}>
+                  <FormattedNumber
+                    value={normalize(
+                      Number(underlyingTokenBalance) +
+                        Number(underlyingWaTokenBalance) +
+                        Number(underlyingWaTokenATokenBalance),
+                      underlyingTokenDecimals
+                    )}
+                    compact
+                    variant="main16"
+                  />
+                </Row>
+              </Box>
+            </Box>
+          </Box>
+        }
+      />
+    );
+  } else {
+    return <Box>hi</Box>;
+  }
 };
