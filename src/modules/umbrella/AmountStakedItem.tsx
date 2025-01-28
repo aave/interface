8d4cf4e6a296ef1e3e9a469ccd7e1b9571875a13
Mark Grothe <grothem@gmail.com>
2025-01-27 21:25:30 -0600

    feat: cooldown and unstake time

--- src/modules/umbrella/AmountStakedItem.tsx
+++ src/modules/umbrella/AmountStakedItem.tsx
@@ -1,23 +1,22 @@
-import { Stack } from '@mui/material';
-import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
-import { ReserveSubheader } from 'src/components/ReserveSubheader';
+import { Trans } from '@lingui/macro';
+import { Stack, Typography } from '@mui/material';
 import { MergedStakeData } from 'src/hooks/stake/useUmbrellaSummary';
-// import { BigNumber } from 'ethers';
+import { useCurrentTimestamp } from 'src/hooks/useCurrentTimestamp';
 
-// import { SecondsToString } from '../staking/StakingPanel';
+import { ListValueColumn } from '../dashboard/lists/ListValueColumn';
+import { SecondsToString } from '../staking/StakingPanel';
 
 export const AmountStakedItem = ({ stakeData }: { stakeData: MergedStakeData }) => {
-  // const now = useCurrentTimestamp(1);
+  const now = useCurrentTimestamp(1);
   const { stakeTokenBalance, stakeTokenBalanceUSD } = stakeData.formattedBalances;
 
-  // const cooldownSeconds = stakeData?.cooldownSeconds || 0;
-  // const endOfCooldown = stakeData?.cooldownData.endOfCooldown || 0;
-  // const unstakeWindow = stakeData?.cooldownData.withdrawalWindow || 0;
-  // const cooldownTimeRemaining = endOfCooldown - now;
-  // const unstakeTimeRemaining = endOfCooldown + unstakeWindow - now;
+  const endOfCooldown = stakeData?.cooldownData.endOfCooldown || 0;
+  const unstakeWindow = stakeData?.cooldownData.withdrawalWindow || 0;
+  const cooldownTimeRemaining = endOfCooldown - now;
 
-  // const isCooldownActive = cooldownTimeRemaining > 0;
-  // const isUnstakeWindowActive = endOfCooldown < now && now < endOfCooldown + unstakeWindow;
+  const isCooldownActive = cooldownTimeRemaining > 0;
+  const isUnstakeWindowActive = endOfCooldown < now && now < endOfCooldown + unstakeWindow;
+  const unstakeTimeRemaining = endOfCooldown + unstakeWindow - now;
 
   // const availableToReactivateCooldown =
   //   isCooldownActive &&
@@ -28,21 +27,52 @@ export const AmountStakedItem = ({ stakeData }: { stakeData: MergedStakeData }) 
   // console.log('TODO: availableToReactivateCooldown', availableToReactivateCooldown);
   return (
     <Stack direction="column" alignItems="center" justifyContent="center">
-      <FormattedNumber compact value={stakeTokenBalance} variant="main16" />
-      <ReserveSubheader value={stakeTokenBalanceUSD} />
-      {/* <Stack direction="column" alignItems="center" justifyContent="center" minWidth={150}>
-        <Button fullWidth variant="outlined" size="medium" disabled={stakeTokenBalance === '0'}>
-          <Trans>Cooldown</Trans>
-        </Button>
-        <Stack width={'100%'} direction="row" alignItems="center" justifyContent="space-between">
-          <Typography variant="helperText">
-            <Trans>Cooldown period</Trans>
-          </Typography>
-          <Typography variant="helperText">
-            <SecondsToString seconds={stakeData.cooldownSeconds} />
-          </Typography>
-        </Stack>
-      </Stack> */}
+      <ListValueColumn
+        value={stakeTokenBalance}
+        subValue={stakeTokenBalanceUSD}
+        withTooltip
+        disabled={stakeTokenBalance === '0'}
+      />
+      {isCooldownActive && <Cooldown cooldownTimeRemaining={cooldownTimeRemaining} />}
+      {isUnstakeWindowActive && <UnstakeWindow unstakeTimeRemaining={unstakeTimeRemaining} />}
+    </Stack>
+  );
+};
+
+const Cooldown = ({ cooldownTimeRemaining }: { cooldownTimeRemaining: number }) => {
+  return (
+    <Stack
+      gap={1}
+      minWidth={'125px'}
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Typography variant="helperText">
+        <Trans>Remaining cooldown</Trans>
+      </Typography>
+      <Typography variant="helperText">
+        <SecondsToString seconds={cooldownTimeRemaining} />
+      </Typography>
+    </Stack>
+  );
+};
+
+const UnstakeWindow = ({ unstakeTimeRemaining }: { unstakeTimeRemaining: number }) => {
+  return (
+    <Stack
+      gap={1}
+      minWidth={'125px'}
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Typography variant="helperText">
+        <Trans>Remaining time to unstake</Trans>
+      </Typography>
+      <Typography variant="helperText">
+        <SecondsToString seconds={unstakeTimeRemaining} />
+      </Typography>
     </Stack>
   );
 };
