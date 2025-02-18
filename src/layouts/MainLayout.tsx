c7fc498a57122911422e1b34442bb3eee82f888b
Mark Hinschberger <foodaka@users.noreply.github.com>
2025-02-18 10:09:32 +0000

    chore: update banner (#2367)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,23 +2,43 @@ import { Box } from '@mui/material';
 import React, { ReactNode } from 'react';
 import AnalyticsConsent from 'src/components/Analytics/AnalyticsConsent';
 import { FeedbackModal } from 'src/layouts/FeedbackDialog';
+import { useRootStore } from 'src/store/root';
 import { FORK_ENABLED } from 'src/utils/marketsAndNetworksConfig';
 
 import { AppFooter } from './AppFooter';
 import { AppHeader } from './AppHeader';
-// import TopBarNotify from './TopBarNotify';
+import TopBarNotify from './TopBarNotify';
 
+const SwitchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    style={{ marginLeft: '8px', width: '24px', height: '24px' }}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+    />
+  </svg>
+);
 export function MainLayout({ children }: { children: ReactNode }) {
-  // const APP_BANNER_VERSION = '5.0.0';
-
+  const APP_BANNER_VERSION = '6.0.0';
+  const currentMarket = useRootStore((state) => state.currentMarket);
   return (
     <>
-      {/* <TopBarNotify
-        learnMoreLink="https://oh7vm38ynd2.typeform.com/to/Fnw3rMyw"
-        notifyText="Looking to provide product feedback? We'd love to hear from you! 👉"
-        bannerVersion={APP_BANNER_VERSION}
-        buttonText="Share feedback"
-      /> */}
+      {currentMarket === 'proto_base_v3' && (
+        <TopBarNotify
+          // learnMoreLink="https://oh7vm38ynd2.typeform.com/to/Fnw3rMyw"
+          notifyText="Debt swaps are now live on Base!"
+          bannerVersion={APP_BANNER_VERSION}
+          customIcon={<SwitchIcon />}
+        />
+      )}
+
       <AppHeader />
       <Box component="main" sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
         {children}
