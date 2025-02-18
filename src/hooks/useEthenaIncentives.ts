4cd14776be4ad229fd70c201783446b40fceefce
MartinGbz <martin.grabarz@outlook.fr>
2025-02-18 16:40:30 +0100

    ✨Add new Ethena Rewards tooltip [skip cypress] (#2365)

---
+++ src/hooks/useEthenaIncentives.ts
@@ -0,0 +1,18 @@
+import { AaveV3Ethereum, AaveV3EthereumLido } from '@bgd-labs/aave-address-book';
+
+const getEthenaData = (assetAddress: string): number | undefined =>
+  ETHENA_DATA_MAP.get(assetAddress);
+
+const ETHENA_DATA_MAP: Map<string, number> = new Map([
+  [AaveV3Ethereum.ASSETS.USDe.A_TOKEN, 25],
+  [AaveV3Ethereum.ASSETS.sUSDe.A_TOKEN, 5],
+  [AaveV3EthereumLido.ASSETS.sUSDe.A_TOKEN, 5],
+]);
+
+export const useEthenaIncentives = (rewardedAsset?: string) => {
+  if (!rewardedAsset) {
+    return undefined;
+  }
+
+  return getEthenaData(rewardedAsset);
+};
