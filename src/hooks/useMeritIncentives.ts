1efdc008309f81c9d59620031269dc69cd73e00e
Nandy Bâ <nandyba@yahoo.fr>
2025-02-19 15:24:23 +0300

    fix: merit API url

--- src/hooks/useMeritIncentives.ts
+++ src/hooks/useMeritIncentives.ts
@@ -45,8 +45,7 @@ export type ExtendedReserveIncentiveResponse = ReserveIncentiveResponse & {
   customForumLink: string;
 };
 
-// const url = 'https://apps.aavechan.com/api/merit/aprs';
-const url = 'http://localhost:3000/api/merit/aprs';
+const url = 'https://apps.aavechan.com/api/merit/aprs';
 
 export type MeritReserveIncentiveData = Omit<ReserveIncentiveResponse, 'incentiveAPR'> & {
   action: MeritAction;
