da6ac23c8bd3c8aa9b90a36aaa1e936a29ece560
Nandy Bâ <nandyba@yahoo.fr>
2025-02-20 17:19:57 +0300

    feat: add borrow gho Ethena incentive (#2370)

--- src/hooks/useEthenaIncentives.ts
+++ src/hooks/useEthenaIncentives.ts
@@ -7,6 +7,8 @@ const ETHENA_DATA_MAP: Map<string, number> = new Map([
   [AaveV3Ethereum.ASSETS.USDe.A_TOKEN, 25],
   [AaveV3Ethereum.ASSETS.sUSDe.A_TOKEN, 5],
   [AaveV3EthereumLido.ASSETS.sUSDe.A_TOKEN, 5],
+  [AaveV3Ethereum.ASSETS.GHO.V_TOKEN, 5],
+  [AaveV3EthereumLido.ASSETS.GHO.V_TOKEN, 5],
 ]);
 
 export const useEthenaIncentives = (rewardedAsset?: string) => {
