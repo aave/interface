3f603541c23878d74f83fe8f66b92f1477ebef5e
MartinGbz <martin.grabarz@outlook.fr>
2025-03-07 18:36:17 +0100

    chore: use aave address book for sonic addresses

--- src/hooks/usePointsIncentives.tsx
+++ src/hooks/usePointsIncentives.tsx
@@ -6,14 +6,15 @@ import {
   AaveV3EthereumLido,
   AaveV3Linea,
   AaveV3Scroll,
+  AaveV3Sonic,
   AaveV3ZkSync,
 } from '@bgd-labs/aave-address-book';
 import { CustomMarket } from 'src/ui-config/marketsConfig';
 
 const SONIC_DATA_MAP: Map<string, number> = new Map([
-  [`${CustomMarket.proto_sonic_v3}-${'0xe18Ab82c81E7Eecff32B8A82B1b7d2d23F1EcE96'}`, 4], // AaveV3Sonic.ASSETS.WETH.A_TOKEN
-  [`${CustomMarket.proto_sonic_v3}-${'0x578Ee1ca3a8E1b54554Da1Bf7C583506C4CD11c6'}`, 10], // AaveV3Sonic.ASSETS.USDC.e.A_TOKEN
-  [`${CustomMarket.proto_sonic_v3}-${`0x6C5E14A212c1C3e4Baf6f871ac9B1a969918c131`}`, 8], // AaveV3Sonic.ASSETS.ws.A_TOKEN
+  [`${CustomMarket.proto_sonic_v3}-${AaveV3Sonic.ASSETS.WETH}`, 4],
+  [`${CustomMarket.proto_sonic_v3}-${AaveV3Sonic.ASSETS.USDCe}`, 10],
+  [`${CustomMarket.proto_sonic_v3}-${AaveV3Sonic.ASSETS.wS}`, 8],
 ]);
 
 const ETHENA_DATA_MAP: Map<string, number> = new Map([
