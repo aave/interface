b32c186dfb862570b88a87e3f4ddb33ddafcf8fe
Mark Grothe <grothem@gmail.com>
2025-03-12 14:17:08 +0000

    feat: celo market

--- src/ui-config/marketsConfig.tsx
+++ src/ui-config/marketsConfig.tsx
@@ -9,6 +9,7 @@ import {
   AaveV3Avalanche,
   AaveV3Base,
   AaveV3BNB,
+  AaveV3Celo,
   AaveV3Ethereum,
   AaveV3EthereumEtherFi,
   AaveV3EthereumLido,
@@ -96,6 +97,7 @@ export enum CustomMarket {
   proto_etherfi_v3 = 'proto_etherfi_v3',
   proto_linea_v3 = 'proto_linea_v3',
   proto_sonic_v3 = 'proto_sonic_v3',
+  proto_celo_v3 = 'proto_celo_v3',
   // v2
   proto_mainnet = 'proto_mainnet',
   proto_avalanche = 'proto_avalanche',
@@ -715,6 +717,20 @@ export const marketsData: {
       UI_POOL_DATA_PROVIDER: AaveV3Sonic.UI_POOL_DATA_PROVIDER,
       UI_INCENTIVE_DATA_PROVIDER: AaveV3Sonic.UI_INCENTIVE_DATA_PROVIDER,
       COLLECTOR: AaveV3Sonic.COLLECTOR,
+    },
+  },
+  [CustomMarket.proto_celo_v3]: {
+    marketTitle: 'Celo',
+    market: CustomMarket.proto_celo_v3,
+    chainId: ChainId.celo,
+    v3: true,
+    addresses: {
+      LENDING_POOL_ADDRESS_PROVIDER: AaveV3Celo.POOL_ADDRESSES_PROVIDER,
+      LENDING_POOL: AaveV3Celo.POOL,
+      WALLET_BALANCE_PROVIDER: AaveV3Celo.WALLET_BALANCE_PROVIDER,
+      UI_POOL_DATA_PROVIDER: AaveV3Celo.UI_POOL_DATA_PROVIDER,
+      UI_INCENTIVE_DATA_PROVIDER: AaveV3Celo.UI_INCENTIVE_DATA_PROVIDER,
+      COLLECTOR: AaveV3Celo.COLLECTOR,
     },
   },
 } as const;
