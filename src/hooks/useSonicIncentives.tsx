910c432f2367f9dd811f60cc3aa32eac9e6a7c14
MartinGbz <martin.grabarz@outlook.fr>
2025-03-24 10:53:53 +0100

    fix: use aave-address-book

--- src/hooks/useSonicIncentives.tsx
+++ src/hooks/useSonicIncentives.tsx
@@ -1,11 +1,11 @@
-// import { AaveV3Sonic } from '@bgd-labs/aave-address-book';
+import { AaveV3Sonic } from '@bgd-labs/aave-address-book';
 
 const getSonicData = (assetAddress: string): number | undefined => SONIC_DATA_MAP.get(assetAddress);
 
 const SONIC_DATA_MAP: Map<string, number> = new Map([
-  ['0xe18Ab82c81E7Eecff32B8A82B1b7d2d23F1EcE96', 4], //AaveV3Sonic.ASSETS.WETH.A_TOKEN
-  ['0x578Ee1ca3a8E1b54554Da1Bf7C583506C4CD11c6', 12], // AaveV3Sonic.ASSETS.USDC.e.A_TOKEN
-  ['0x6C5E14A212c1C3e4Baf6f871ac9B1a969918c131', 8], // AaveV3Sonic.ASSETS.ws.A_TOKEN
+  [AaveV3Sonic.ASSETS.WETH.A_TOKEN, 4], //
+  [AaveV3Sonic.ASSETS.USDCe.A_TOKEN, 12], //
+  [AaveV3Sonic.ASSETS.wS.A_TOKEN, 8], // AaveV3Sonic.ASSETS.ws.A_TOKEN
 ]);
 
 export const useSonicIncentives = (rewardedAsset?: string) => {
