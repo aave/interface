e5ee2e9ba9f285f3885148bffe174507d4d4e73c
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2025-04-08 09:40:35 +0300

    fix: reactivated faucet link for testnet

--- src/ui-config/menu-items/index.tsx
+++ src/ui-config/menu-items/index.tsx
@@ -1,5 +1,6 @@
 import { t } from '@lingui/macro';
 import { ROUTES } from 'src/components/primitives/Link';
+import { ENABLE_TESTNET } from 'src/utils/marketsAndNetworksConfig';
 
 import { MarketDataType } from '../marketsConfig';
 
@@ -30,5 +31,10 @@ export const navigation: Navigation[] = [
     link: ROUTES.playground,
     title: t`Playground`,
     dataCy: 'menuPlayground',
+  },
+  {
+    link: ROUTES.faucet,
+    title: t`Faucet`,
+    isVisible: () => process.env.NEXT_PUBLIC_ENV === 'staging' || ENABLE_TESTNET,
   },
 ];
