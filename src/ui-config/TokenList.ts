cd929aabba8d0ccd8cb3f7a388c6b72b7a37557f
xxxbarry <127599710+xxxbarry@users.noreply.github.com>
2025-04-17 15:11:28 -0400

    Update TokenList.ts

--- src/ui-config/TokenList.ts
+++ src/ui-config/TokenList.ts
@@ -51,6 +51,363 @@ export interface TokenList {
 
 const NETWORK_ASSETS: TokenInfo[] = [
   {
+    "address": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    "name": "USD Coin",
+    "symbol": "USDC",
+    "decimals": 5
+  },
+  {
+    "address": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    "name": "USD Coin",
+    "symbol": "USDC",
+    "decimals": 6
+  },
+  {
+    "address": "0x9e90296a1343258388a6476db1c12d76dd04504e",
+    "name": "USDC",
+    "symbol": "USDC",
+    "decimals": 18
+  },
+  {
+    "address": "0xb2785e03e542161d3dfa3ce3ddad4a26d892cd6a",
+    "name": "ZAP TOKEN",
+    "symbol": "ZAP",
+    "decimals": 18
+  },
+  {
+    "address": "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a",
+    "name": "GMX",
+    "symbol": "GMX",
+    "decimals": 18
+  },
+  {
+    "address": "0x5d3a1ff2b6bab83b63cd9ad0787074081a52ef34",
+    "name": "USDe",
+    "symbol": "USDe",
+    "decimals": 18
+  },
+  {
+    "address": "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+    "name": "ChainLink Token",
+    "symbol": "LINK",
+    "decimals": 18
+  },
+  {
+    "address": "0xb41877593e1eec5a380f8c22d0abfa17b0865527",
+    "name": "Arbitrum",
+    "symbol": "ARB",
+    "decimals": 8
+  },
+  {
+    "address": "0x513c7e3a9c69ca3e22550ef58ac1c0088e918fff",
+    "name": "Aave Arbitrum wstETH",
+    "symbol": "aArbwstETH",
+    "decimals": 18
+  },
+  {
+    "address": "0x5979d7b546e38e414f7e9822514be443a4800529",
+    "name": "Wrapped liquid staked Ether 2.0",
+    "symbol": "wstETH",
+    "decimals": 18
+  },
+  {
+    "address": "0x7e6f08984feac6244aec5dce3bd66387e82b9630",
+    "name": "Pendle",
+    "symbol": "PENDLE",
+    "decimals": 18
+  },
+  {
+    "address": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+    "name": "USD₮0",
+    "symbol": "USD₮0",
+    "decimals": 6
+  },
+  {
+    "address": "0xf31e1ae27e7cd057c1d6795a5a083e0453d39b50",
+    "name": "USDC",
+    "symbol": "USDC",
+    "decimals": 0
+  },
+  {
+    "address": "0xf67ec0a483a1c80eb1e92d58e46f1c10f8383b8d",
+    "name": "Ryan",
+    "symbol": "RYAN",
+    "decimals": 2
+  },
+  {
+    "address": "0x187acd192822aed86d89ad516e4a3c3bb1929574",
+    "name": "SushiSwap LP Token",
+    "symbol": "SLP",
+    "decimals": 18
+  },
+  {
+    "address": "0x912ce59144191c1204e64559fe8253a0e49e6548",
+    "name": "Arbitrum",
+    "symbol": "ARB",
+    "decimals": 18
+  },
+  {
+    "address": "0x7b6c14ccca515d6d1f234054254bdced56ac173e",
+    "name": "WBARBITRUM",
+    "symbol": "WBARB",
+    "decimals": 18
+  },
+  {
+    "address": "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+    "name": "Wrapped BTC",
+    "symbol": "WBTC",
+    "decimals": 8
+  },
+  {
+    "address": "0xf810fc6501a91e3e7ff43f65fd81335238bbbd5e",
+    "name": "Ethers",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  {
+    "address": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+    "name": "Wrapped Ether",
+    "symbol": "WETH",
+    "decimals": 18
+  },
+  {
+    "address": "0x94f1747558e1287a89bf4be3abb6e736d8b5fc38",
+    "name": "ETHERFAST",
+    "symbol": "ETHERFAST",
+    "decimals": 18
+  },
+  {
+    "address": "0x3082cc23568ea640225c2467653db90e9250aaa0",
+    "name": "Radiant",
+    "symbol": "RDNT",
+    "decimals": 18
+  },
+  {
+    "address": "0xba5ddd1f9d7f570dc94a51479a000e3bce967196",
+    "name": "Aave Token",
+    "symbol": "AAVE",
+    "decimals": 18
+  },
+  {
+    "address": "0x7189fb5b6504bbff6a852b13b7b82a3c118fdc27",
+    "name": "ether.fi governance token",
+    "symbol": "ETHFI",
+    "decimals": 18
+  },
+  {
+    "address": "0xaf0807b1bfb6c9a6a000c393fa152322a67bb34b",
+    "name": "USDCT",
+    "symbol": "USDCT",
+    "decimals": 18
+  },
+  {
+    "address": "0xf224dbc383874ecf3d8c1a63736fd914c30448a3",
+    "name": "TRUMP",
+    "symbol": "gettrumps",
+    "decimals": 10
+  },
+  {
+    "address": "0x050d4e9ca843a9640e8f92731833805c650f5e3d",
+    "name": "Dai Stablecoin",
+    "symbol": "DAI",
+    "decimals": 18
+  },
+  {
+    "address": "0x422e3b839e0bcf7c9dbc7df39329e9380096149c",
+    "name": "Tether USD",
+    "symbol": "USDT",
+    "decimals": 18
+   },
+   {
+      "address": "0x14d44f4457de9bd6435b566543d0a58754ba27b3",
+      "symbol": "FYDE",
+      "name": "FYDE Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x8707cdf0767a4a956d6ebd7728a2ad483b334968",
+      "symbol": "PEPE",
+      "name": "PEPE Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x422e3b839e0bcf7c9dbc7df39329e9380096149c",
+      "symbol": "USDT",
+      "name": "Tether",
+      "decimals": 6,
+      "chainId": 42161
+    },
+    {
+      "address": "0xcbc79972198fd73ddb8fdee6dcdec1e6270be357",
+      "symbol": "ETH",
+      "name": "Ether",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xf8d72c9cd50607bdd6fbdce53d6929794c99c959",
+      "symbol": "eth",
+      "name": "Ethereum",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x1db4E24A2747D0E7b209E9BB4EFe0b7d3Df806B0",
+      "symbol": "AAVE",
+      "name": "Aave Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x7b6bcfbb49199e16b5c2cd9f1b627575e2fadf6f",
+      "symbol": "CRV",
+      "name": "Curve DAO Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xece654d50a41c1f562e46ecee9334a45a57b918d",
+      "symbol": "WBTC",
+      "name": "Wrapped Bitcoin",
+      "decimals": 8,
+      "chainId": 42161
+    },
+    {
+      "address": "0x4dA65b79d0AD30f64e4E06Bc3c7FCa7D263cA8aC",
+      "symbol": "TokenV",
+      "name": "TokenV",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x10c9cc8aba8c0d9289bee510f3147cc38774b8b0",
+      "symbol": "WBTC",
+      "name": "Wrapped Bitcoin",
+      "decimals": 8,
+      "chainId": 42161
+    },
+    {
+      "address": "0xb2785e03e542161d3dfa3ce3ddad4a26d892cd6a",
+      "symbol": "ZAP",
+      "name": "ZAP Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xb29870524503b0D83AB8c57d79e7849883479346",
+      "symbol": "AsoRaD",
+      "name": "AsoRaD Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xe3c861bd8b5deb9ec15e74b96b600db119f765a1",
+      "symbol": "ZAPUSD",
+      "name": "ZAPUSD Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x00699FE7d5b786d42297670C7Ba7F02373B4F0e3",
+      "symbol": "$SHARBI",
+      "name": "$SHARBI Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xbe3e51fbbcd6e0da945941f4452339bd2f1259e5",
+      "symbol": "TokenV",
+      "name": "TokenV",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xcceea600520f56982e389e97ac6ac1ffc68c3b9d",
+      "symbol": "DAI",
+      "name": "Dai Stablecoin",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xcc9841c942ec72e6ac74da8fb2fb45cf3b1a099c",
+      "symbol": "USDT",
+      "name": "Tether",
+      "decimals": 6,
+      "chainId": 42161
+    },
+    {
+      "address": "0x2d1352df75836a2681f8be063571c8391c04b0dc",
+      "symbol": "ZAP-WETH",
+      "name": "ZAP-WETH",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x7243bd9e4ace21dbeb4c11e7916505b89ad6b107",
+      "symbol": "ARBWART",
+      "name": "ARBWART Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xb41877593e1eec5a380f8c22d0abfa17b0865527",
+      "symbol": "ARB",
+      "name": "Arbitrum",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xa4cd3c7f8e71115c856dad1dbe042b17722f7807",
+      "symbol": "ARB",
+      "name": "Arbitrum",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xe4251b067c541f12c5032198884306b66122e44e",
+      "symbol": "ARBITRUM",
+      "name": "Arbitrum Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x2519f2598897d2a93d0ccf368fa565fd3053de1e",
+      "symbol": "GMT",
+      "name": "GMT Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0x7b6c14ccca515d6d1f234054254bdced56ac173e",
+      "symbol": "WBARB",
+      "name": "WBARB Token",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xaed3a2d0aaaaf5e6f54285a004af5036f3fe0eda",
+      "symbol": "ODOD_LP",
+      "name": "ODOD LP",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    {
+      "address": "0xaed3a2d0aaaaf5e6f54285a004af5036f3fe0eda",
+      "symbol": "WBTC",
+      "name": "Wrapped Bitcoin",
+      "decimals": 18,
+      "chainId": 42161
+    },
+    { 
+      "address": "0xAE63B6DC1E399c7257E7Aa4e20e334CAA3bab6cc",
+      "symbol": "ETH",
+      "name": "Ether",
+      "decimals": 18,
+      "chainId": 1
+    } 
+  {
     name: 'Ethereum',
     symbol: 'ETH',
     decimals: 18,
