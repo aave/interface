04dfdb23153ed91034e36884fa09ff3f61a2e4df
Mark Grothe <grothem@gmail.com>
2025-05-01 08:56:55 -0500

    feat: added incentives st stS

--- src/hooks/useSonicIncentives.tsx
+++ src/hooks/useSonicIncentives.tsx
@@ -3,9 +3,10 @@ import { AaveV3Sonic } from '@bgd-labs/aave-address-book';
 const getSonicData = (assetAddress: string): number | undefined => SONIC_DATA_MAP.get(assetAddress);
 
 const SONIC_DATA_MAP: Map<string, number> = new Map([
-  [AaveV3Sonic.ASSETS.WETH.A_TOKEN, 4], //
-  [AaveV3Sonic.ASSETS.USDCe.A_TOKEN, 12], //
-  [AaveV3Sonic.ASSETS.wS.A_TOKEN, 8], // AaveV3Sonic.ASSETS.ws.A_TOKEN
+  [AaveV3Sonic.ASSETS.WETH.A_TOKEN, 4],
+  [AaveV3Sonic.ASSETS.USDCe.A_TOKEN, 12],
+  [AaveV3Sonic.ASSETS.wS.A_TOKEN, 8],
+  ['0xeAa74D7F42267eB907092AF4Bc700f667EeD0B8B', 8], // stS aToken
 ]);
 
 export const useSonicIncentives = (rewardedAsset?: string) => {
