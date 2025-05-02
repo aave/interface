537a85764d8230764a22350fc2875fd1267fcc53
Martin Grabina <mgrabina@itba.edu.ar>
2025-05-02 10:54:19 -0300

    nits

--- src/components/transactions/Switch/BaseSwitchModalContent.tsx
+++ src/components/transactions/Switch/BaseSwitchModalContent.tsx
@@ -321,7 +321,8 @@ export const BaseSwitchModalContent = ({
         <Warning severity="info" icon={false} sx={{ mt: 2, mb: 2 }}>
           <Typography variant="caption">
             You have {cowSwitchsInProgress} pending order{cowSwitchsInProgress > 1 ? 's' : ''} for
-            this asset pair. You can track their status in your{' '}
+            this asset pair. You can track {cowSwitchsInProgress > 1 ? 'their' : 'its'} status in
+            your{' '}
             <Link target="_blank" href="/history">
               transaction history
             </Link>
@@ -441,6 +442,14 @@ export const BaseSwitchModalContent = ({
               {swapDetailsComponent}
 
               {txError && <ParaswapErrorDisplay txError={txError} />}
+              {switchProvider === 'cowprotocol' && (
+                <Warning severity="info" icon={false} sx={{ mt: 4 }}>
+                  <Typography variant="caption">
+                    This action doesn't require gas as it's not an onchain transaction.
+                  </Typography>
+                </Warning>
+              )}
+
               <SwitchActions
                 isWrongNetwork={isWrongNetwork.isWrongNetwork}
                 inputAmount={debounceInputAmount}
