4a903c505dd6cf1c4ea39547cfbe4a84d0764061
Nandy Bâ <nandyba@yahoo.fr>
2025-05-13 22:40:40 +0900

    feat: add Ethena PT points (#2466)

--- src/hooks/useEthenaIncentives.ts
+++ src/hooks/useEthenaIncentives.ts
@@ -9,6 +9,8 @@ const ETHENA_DATA_MAP: Map<string, number> = new Map([
   [AaveV3EthereumLido.ASSETS.sUSDe.A_TOKEN, 5],
   [AaveV3Ethereum.ASSETS.GHO.V_TOKEN, 5],
   [AaveV3EthereumLido.ASSETS.GHO.V_TOKEN, 5],
+  [AaveV3Ethereum.ASSETS.PT_eUSDE_29MAY2025.A_TOKEN, 2],
+  [AaveV3Ethereum.ASSETS.PT_sUSDE_31JUL2025.A_TOKEN, 1],
 ]);
 
 export const useEthenaIncentives = (rewardedAsset?: string) => {
