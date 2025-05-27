013556a861fa7655fbdceb6fad0f567e0a858506
Mark Hinschberger <foodaka@users.noreply.github.com>
2025-05-27 17:40:38 +0100

    fix: transaction event

--- src/components/TransactionEventHandler.tsx
+++ src/components/TransactionEventHandler.tsx
@@ -1,62 +1,59 @@
-// import { useEffect, useState } from 'react';
-// import { useRootStore } from 'src/store/root';
-// import { selectSuccessfulTransactions } from 'src/store/transactionsSelectors';
-// import { getNetworkConfig } from 'src/utils/marketsAndNetworksConfig';
-// import { GENERAL } from 'src/utils/mixPanelEvents';
-// import { useShallow } from 'zustand/shallow';
+import { useEffect, useRef } from 'react';
+import { useRootStore } from 'src/store/root';
+import { GENERAL } from 'src/utils/events';
+import { getNetworkConfig } from 'src/utils/marketsAndNetworksConfig';
 
 export const TransactionEventHandler = () => {
-  // const [postedTransactions, setPostedTransactions] = useState<{ [chainId: string]: string[] }>({});
+  const postedTransactionsRef = useRef<{ [chainId: string]: string[] }>({});
 
-  // const trackEvent = useRootStore((store) => store.trackEvent);
-  // const successfulTransactions = useRootStore(useShallow(selectSuccessfulTransactions));
+  const trackEvent = useRootStore((store) => store.trackEvent);
+  const transactions = useRootStore((store) => store.transactions);
 
   //tx's currently tracked: supply, borrow, withdraw, repay, repay with coll, collateral switch
 
-  // useEffect(() => {
-  //   Object.keys(successfulTransactions).forEach((chainId) => {
-  //     const chainIdNumber = +chainId;
-  //     const networkConfig = getNetworkConfig(chainIdNumber);
-  //     Object.keys(successfulTransactions[chainIdNumber]).forEach((txHash) => {
-  //       if (!postedTransactions[chainIdNumber]?.includes(txHash)) {
-  //         const tx = successfulTransactions[chainIdNumber][txHash];
-  //         // const event = actionToEvent(tx.action);
-  //         trackEvent(GENERAL.TRANSACTION, {
-  //           transactionType: tx.action,
-  //           tokenAmount: tx.amount,
-  //           assetName: tx.assetName,
-  //           asset: tx.asset,
-  //           market: tx.market === null ? undefined : tx.market,
-  //           txHash: txHash,
-  //           proposalId: tx.proposalId,
-  //           support: tx.support,
-  //           previousState: tx.previousState,
-  //           newState: tx.newState,
-  //           outAsset: tx.outAsset,
-  //           outAmount: tx.outAmount,
-  //           outAssetName: tx.outAssetName,
-  //           amountUsd: tx.amountUsd,
-  //           outAmountUsd: tx.outAmountUsd,
-  //           chainId: chainIdNumber,
-  //           chainName: networkConfig.displayName || networkConfig.name,
-  //         });
+  useEffect(() => {
+    Object.keys(transactions).forEach((chainId) => {
+      const chainIdNumber = +chainId;
+      const networkConfig = getNetworkConfig(chainIdNumber);
 
-  //         // update local state
-  //         if (postedTransactions[chainIdNumber]) {
-  //           setPostedTransactions((prev) => ({
-  //             ...prev,
-  //             [chainIdNumber]: [...prev[chainIdNumber], txHash],
-  //           }));
-  //         } else {
-  //           setPostedTransactions((prev) => ({
-  //             ...prev,
-  //             [+chainId]: [txHash],
-  //           }));
-  //         }
-  //       }
-  //     });
-  //   });
-  // }, [trackEvent, postedTransactions, successfulTransactions]);
+      Object.keys(transactions[chainIdNumber] || {}).forEach((txHash) => {
+        const tx = transactions[chainIdNumber][txHash];
+
+        // Only process successful transactions that haven't been posted yet
+        if (
+          tx.txState === 'success' &&
+          !postedTransactionsRef.current[chainIdNumber]?.includes(txHash)
+        ) {
+          trackEvent(GENERAL.TRANSACTION, {
+            transactionType: tx.action,
+            tokenAmount: tx.amount,
+            assetName: tx.assetName,
+            asset: tx.asset,
+            market: tx.market === null ? undefined : tx.market,
+            txHash: txHash,
+            proposalId: tx.proposalId,
+            support: tx.support,
+            previousState: tx.previousState,
+            newState: tx.newState,
+            outAsset: tx.outAsset,
+            outAmount: tx.outAmount,
+            outAssetName: tx.outAssetName,
+            amountUsd: tx.amountUsd,
+            outAmountUsd: tx.outAmountUsd,
+            chainId: chainIdNumber,
+            chainName: networkConfig.displayName || networkConfig.name,
+          });
+
+          // update ref
+          if (postedTransactionsRef.current[chainIdNumber]) {
+            postedTransactionsRef.current[chainIdNumber].push(txHash);
+          } else {
+            postedTransactionsRef.current[chainIdNumber] = [txHash];
+          }
+        }
+      });
+    });
+  }, [trackEvent, transactions]);
 
   return null;
 };
