c7299ceb065c31dae2d60e340b88fd0a7a52c8bf
Martin Grabina <mgrabina@itba.edu.ar>
2025-06-05 11:31:59 -0300

    feat: forcing a minimum slippage for cow eth-flow orders

--- src/components/transactions/Switch/validation.helpers.ts
+++ src/components/transactions/Switch/validation.helpers.ts
@@ -12,40 +12,58 @@ export interface ValidationData {
 
 export const validateSlippage = (
   slippage: string,
+  chainId: number,
+  isNativeToken = false,
   provider?: SwitchProvider
 ): ValidationData | undefined => {
   try {
+    const numberSlippage = Number(slippage);
     if (provider === 'cowprotocol') {
+      if (isNativeToken) {
+        if (chainId === 1) {
+          if (numberSlippage < 2) {
+            return {
+              message: 'Slippage lower than 2% may result in failed transactions',
+              severity: ValidationSeverity.ERROR,
+            };
+          }
+        } else {
+          if (numberSlippage < 0.5) {
+            return {
+              message: 'Slippage lower than 0.5% may result in failed transactions',
+              severity: ValidationSeverity.ERROR,
+            };
+          }
+        }
+      }
+    } else {
+      if (Number.isNaN(numberSlippage))
+        return {
+          message: 'Invalid slippage',
+          severity: ValidationSeverity.ERROR,
+        };
+      if (numberSlippage > 30)
+        return {
+          message: 'Slippage must be lower 30%',
+          severity: ValidationSeverity.ERROR,
+        };
+      if (numberSlippage < 0)
+        return {
+          message: 'Slippage must be positive',
+          severity: ValidationSeverity.ERROR,
+        };
+      if (numberSlippage > 10)
+        return {
+          message: 'High slippage',
+          severity: ValidationSeverity.WARNING,
+        };
+      if (numberSlippage < 0.1)
+        return {
+          message: 'Slippage lower than 0.1% may result in failed transactions',
+          severity: ValidationSeverity.WARNING,
+        };
       return undefined;
     }
-
-    const numberSlippage = Number(slippage);
-    if (Number.isNaN(numberSlippage))
-      return {
-        message: 'Invalid slippage',
-        severity: ValidationSeverity.ERROR,
-      };
-    if (numberSlippage > 30)
-      return {
-        message: 'Slippage must be lower 30%',
-        severity: ValidationSeverity.ERROR,
-      };
-    if (numberSlippage < 0)
-      return {
-        message: 'Slippage must be positive',
-        severity: ValidationSeverity.ERROR,
-      };
-    if (numberSlippage > 10)
-      return {
-        message: 'High slippage',
-        severity: ValidationSeverity.WARNING,
-      };
-    if (numberSlippage < 0.1)
-      return {
-        message: 'Slippage lower than 0.1% may result in failed transactions',
-        severity: ValidationSeverity.WARNING,
-      };
-    return undefined;
   } catch {
     return { message: 'Invalid slippage', severity: ValidationSeverity.ERROR };
   }
