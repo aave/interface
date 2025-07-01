8febd7e98cf3ebd64135429ea9d2d9bf0b96d9ab
Mark Grothe <grothem@gmail.com>
2025-07-01 08:18:46 -0500

    chore: cleanup

--- src/utils/ghoUtilities.tsx
+++ src/utils/ghoUtilities.tsx
@@ -18,64 +18,6 @@ export const getGhoReserve = (reserves: ComputedReserveData[]) => {
 };
 
 /**
- * Calculates the weighted average APY
- * @param baseVariableBorrowRate - The base variable borrow rate, normalized
- * @param totalBorrowAmount - The total amount of the asset that is being borrowed
- * @param discountableAmount - The amount that can be discounted for the user
- * @param borrowRateAfterDiscount - The borrow rate after the discount is applied
- * @returns
- */
-export const weightedAverageAPY = (
-  baseVariableBorrowRate: number,
-  totalBorrowAmount: number,
-  discountableAmount: number,
-  borrowRateAfterDiscount: number
-) => {
-  if (discountableAmount === 0) return baseVariableBorrowRate;
-  if (totalBorrowAmount <= discountableAmount) return borrowRateAfterDiscount;
-
-  const nonDiscountableAmount = totalBorrowAmount - discountableAmount;
-
-  return (
-    (nonDiscountableAmount * baseVariableBorrowRate +
-      discountableAmount * borrowRateAfterDiscount) /
-    totalBorrowAmount
-  );
-};
-
-/**
- * This helps display the discountable amount of GHO based off of how much is being borrowed and how much is discountable.
- * This is used in both the borrow modal and the discount rate calculator.
- * @param discountableGhoAmount - The amount of GHO that is discountable
- * @param amountGhoBeingBorrowed - The amount of GHO requesting to be borrowed
- * @returns The amount of discountable GHO as a number in a display-friendly form
- */
-export const displayDiscountableAmount = (
-  discountableGhoAmount: number,
-  amountGhoBeingBorrowed: number
-): number => {
-  return discountableGhoAmount >= amountGhoBeingBorrowed
-    ? amountGhoBeingBorrowed
-    : discountableGhoAmount;
-};
-
-/**
- * This helps display the non-discountable amount of GHO based off of how much is being borrowed and how much is discountable.
- * This is used in both the borrow modal and the discount rate calculator.
- * @param discountableGhoAmount - The amount of GHO that is discountable
- * @param amountGhoBeingBorrowed - The amount of GHO requesting to be borrowed
- * @returns The amount of non-discountable GHO as a number in a display-friendly form
- */
-export const displayNonDiscountableAmount = (
-  discountableGhoAmount: number,
-  amountGhoBeingBorrowed: number
-): number => {
-  return discountableGhoAmount >= amountGhoBeingBorrowed
-    ? 0
-    : amountGhoBeingBorrowed - discountableGhoAmount;
-};
-
-/**
  * Determines if the given symbol is GHO and the market supports minting new GHO
  */
 export const displayGhoForMintableMarket = ({
