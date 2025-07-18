6da476a44315f1009ddc332869c2945bf4f9e698
Mark Hinschberger <foodaka@users.noreply.github.com>
2025-07-18 17:19:36 +0100

    fix: remove staking info gho unconnected wallet

--- src/modules/staking/StakingPanelNoWallet.tsx
+++ src/modules/staking/StakingPanelNoWallet.tsx
@@ -3,9 +3,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { MeritIncentivesButton } from 'src/components/incentives/IncentivesButton';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
-import { Link } from 'src/components/primitives/Link';
 import { TokenIcon } from 'src/components/primitives/TokenIcon';
-import { TextWithTooltip } from 'src/components/TextWithTooltip';
 import { StakeTokenFormatted, useGeneralStakeUiData } from 'src/hooks/stake/useGeneralStakeUiData';
 import { useRootStore } from 'src/store/root';
 import { CustomMarket } from 'src/ui-config/marketsConfig';
@@ -38,8 +36,6 @@ export const StakingPanelNoWallet: React.FC<StakingPanelNoWalletProps> = ({
   if (stakedToken == 'ABPT') stakingAPY = stkBpt?.stakeApy || '0';
   if (stakedToken == 'GHO') stakingAPY = stkGho?.stakeApy || '0';
   if (stakedToken == 'ABPT V2') stakingAPY = stkBptV2?.stakeApy || '0';
-
-  const distributionEnded = Date.now() / 1000 > Number(stkGho?.distributionEnd);
 
   return (
     <Box
@@ -94,29 +90,13 @@ export const StakingPanelNoWallet: React.FC<StakingPanelNoWalletProps> = ({
           alignItems: 'center',
         }}
       >
-        <Box display={'flex'} alignItems={'center'}>
-          <Typography variant="subheader2" color="text.secondary">
-            <Trans>Staking APR</Trans>
-          </Typography>
-
-          {distributionEnded && stakedToken === 'GHO' && (
-            <TextWithTooltip wrapperProps={{ marginBottom: '1px' }} iconColor="warning.main">
-              <Trans>
-                The current incentives period, decided on by the Aave community, has ended.
-                Governance is in the process on renewing, check for updates.{' '}
-                <Link
-                  href="https://governance.aave.com"
-                  sx={{ textDecoration: 'underline' }}
-                  variant="caption"
-                  color="text.secondary"
-                >
-                  Learn more
-                </Link>
-                .
-              </Trans>
-            </TextWithTooltip>
-          )}
-        </Box>
+        {stakedToken !== 'GHO' && (
+          <Box display={'flex'} alignItems={'center'}>
+            <Typography variant="subheader2" color="text.secondary">
+              <Trans>Staking APR</Trans>
+            </Typography>
+          </Box>
+        )}
 
         <FormattedNumber
           value={parseFloat(stakingAPY || '0') / 10000}
