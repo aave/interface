0aafa7a92fe5d6bfe423df2bd1ee9925f4427242
Nandy Bâ <nandyba@yahoo.fr>
2025-07-30 16:22:36 +0200

    feat: update Ethena's USDe points from 25 to 5 (#2554)

--- src/hooks/useEthenaIncentives.ts
+++ src/hooks/useEthenaIncentives.ts
@@ -4,7 +4,7 @@ const getEthenaData = (assetAddress: string): number | undefined =>
   ETHENA_DATA_MAP.get(assetAddress);
 
 const ETHENA_DATA_MAP: Map<string, number> = new Map([
-  [AaveV3Ethereum.ASSETS.USDe.A_TOKEN, 25],
+  [AaveV3Ethereum.ASSETS.USDe.A_TOKEN, 5],
   [AaveV3Ethereum.ASSETS.sUSDe.A_TOKEN, 5],
   [AaveV3EthereumLido.ASSETS.sUSDe.A_TOKEN, 5],
   [AaveV3Ethereum.ASSETS.GHO.V_TOKEN, 5],
