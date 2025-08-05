cbc1b548c53afc5cfebf65122b975e08e9e02165
Mark Grothe <grothem@gmail.com>
2025-08-05 11:42:00 -0500

    feat: gho ccip gnosis

--- src/components/transactions/Bridge/BridgeConfig.ts
+++ src/components/transactions/Bridge/BridgeConfig.ts
@@ -6,6 +6,7 @@ import {
   AaveV3Base,
   AaveV3BaseSepolia,
   AaveV3Ethereum,
+  AaveV3Gnosis,
   AaveV3Sepolia,
   GhoArbitrum,
   GhoBase,
@@ -173,6 +174,30 @@ const prodConfig: Config[] = [
         chainId: ChainId.avalanche,
         logoURI:
           'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+        extensions: {
+          isNative: true,
+        },
+        balance: '0',
+      },
+    ],
+  },
+  {
+    sourceChainId: ChainId.xdai,
+    chainSelector: '465200170687744372',
+    burnMintTokenPool: '0xDe6539018B095353A40753Dc54C91C68c9487D4E',
+    router: '0x4aAD6071085df840abD9Baf1697d5D5992bDadce',
+    tokenOracle: '0x360d8aa8F6b09B7BC57aF34db2Eb84dD87bf4d12',
+    wrappedNativeOracle: AaveV3Gnosis.ASSETS.WXDAI.ORACLE,
+    subgraphUrl: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_SUBGRAPH_API_KEY}/subgraphs/id/CFjU1G9iUtFDqEBTzSePRiPjghjUzQeFX5C67DGSK2Ao`,
+    feeTokens: [
+      {
+        name: 'XDAI',
+        symbol: 'XDAI',
+        decimals: 18,
+        address: constants.AddressZero, // Use zero address for network token ccip
+        chainId: ChainId.xdai,
+        logoURI:
+          '/icons/tokens/wxdai.svg',
         extensions: {
           isNative: true,
         },
