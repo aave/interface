35cef7836a39943f9318cd767292e85bf2f2cfaf
Alejandro <95312462+AGMASO@users.noreply.github.com>
2025-08-26 09:52:16 +0200

    feat: implement Figma design for asset category filter buttons

--- src/components/MarketAssetCategoryFilter.tsx
+++ src/components/MarketAssetCategoryFilter.tsx
@@ -1,59 +1,87 @@
 import { Trans } from '@lingui/macro';
-import { Box, Button, ButtonGroup } from '@mui/material';
+import { SxProps, Theme, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { AssetCategory } from 'src/modules/markets/utils/assetCategories';
 
 interface MarketAssetCategoryFiltersProps {
   selectedCategory: AssetCategory;
   onCategoryChange: (category: AssetCategory) => void;
+  sx?: {
+    buttonGroup?: SxProps<Theme>;
+    button?: SxProps<Theme>;
+  };
 }
+const categoryLabels = {
+  [AssetCategory.ALL]: <Trans>All</Trans>,
+  [AssetCategory.STABLECOINS]: <Trans>Stablecoins</Trans>,
+  [AssetCategory.ETH_CORRELATED]: <Trans>ETH Correlated</Trans>,
+} as const;
+const categories = [
+  AssetCategory.ALL,
+  AssetCategory.STABLECOINS,
+  AssetCategory.ETH_CORRELATED,
+] as const;
 
 export const MarketAssetCategoryFilter = ({
   selectedCategory,
   onCategoryChange,
+  ...props
 }: MarketAssetCategoryFiltersProps) => {
+  const handleChange = (_event: React.MouseEvent<HTMLElement>, newCategory: AssetCategory) => {
+    if (newCategory !== null) {
+      onCategoryChange(newCategory);
+    }
+  };
+
   return (
-    <Box>
-      <ButtonGroup
-        sx={{
-          width: '100%',
-          '& .MuiButton-root': {
-            flex: 1,
-            textTransform: 'none',
-            whiteSpace: 'nowrap',
-            minWidth: 'auto',
-            p: 2,
-            fontSize: '0.875rem',
-            border: '1px solid transparent',
-            '&:hover': {
-              borderColor: 'primary.main',
-              borderWidth: '1px',
-              borderStyle: 'solid',
-              zIndex: 1,
+    <ToggleButtonGroup
+      value={selectedCategory}
+      exclusive
+      onChange={handleChange}
+      aria-label="Asset category"
+      sx={{
+        width: '100%',
+        height: '36px',
+        '&.MuiToggleButtonGroup-grouped': {
+          borderRadius: 'unset',
+        },
+        ...props.sx?.buttonGroup,
+      }}
+    >
+      {categories.map((category) => (
+        <ToggleButton
+          key={category}
+          value={category}
+          disableRipple
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          sx={(theme): SxProps<Theme> | undefined => ({
+            flex: { xs: 1, xsm: 1, sm: 'auto' },
+            '&.MuiToggleButtonGroup-grouped:not(.Mui-selected), &.MuiToggleButtonGroup-grouped&.Mui-disabled':
+              {
+                border: '1px solid transparent',
+                backgroundColor: 'background.surface',
+                color: 'action.disabled',
+              },
+            '&.MuiToggleButtonGroup-grouped&.Mui-selected': {
+              borderRadius: '4px',
+              border: `1px solid ${theme.palette.divider}`,
+              boxShadow: '0px 2px 1px rgba(0, 0, 0, 0.05), 0px 0px 1px rgba(0, 0, 0, 0.25)',
+              backgroundColor: 'background.paper',
             },
-          },
-        }}
-        variant="outlined"
-        size="small"
-      >
-        <Button
-          variant={selectedCategory === AssetCategory.ALL ? 'contained' : 'outlined'}
-          onClick={() => onCategoryChange(AssetCategory.ALL)}
+            ...props.sx?.button,
+          })}
         >
-          <Trans>All</Trans>
-        </Button>
-        <Button
-          variant={selectedCategory === AssetCategory.STABLECOINS ? 'contained' : 'outlined'}
-          onClick={() => onCategoryChange(AssetCategory.STABLECOINS)}
-        >
-          <Trans>Stablecoins</Trans>
-        </Button>
-        <Button
-          variant={selectedCategory === AssetCategory.ETH_CORRELATED ? 'contained' : 'outlined'}
-          onClick={() => onCategoryChange(AssetCategory.ETH_CORRELATED)}
-        >
-          <Trans>ETH Correlated</Trans>
-        </Button>
-      </ButtonGroup>
-    </Box>
+          <Typography
+            variant="buttonM"
+            sx={{
+              fontSize: '0.875rem',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            {categoryLabels[category]}
+          </Typography>
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
