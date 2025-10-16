78dfa157c543abecfaa1502a688964733e7e7c3b
mark hinschberger <markhinschberger@gmail.com>
2025-10-16 11:13:38 +0100

    chore: banner

--- src/components/MarketSwitcher.tsx
+++ src/components/MarketSwitcher.tsx
@@ -187,6 +187,7 @@ export const MarketSwitcher = () => {
   };
 
   const marketBlurbs: { [key: string]: JSX.Element } = {
+    proto_ink_v3: <Trans>The Ink instance is governed by the Ink Foundation</Trans>,
     proto_mainnet_v3: (
       <Trans>Main Ethereum market with the largest selection of assets and yield options</Trans>
     ),
