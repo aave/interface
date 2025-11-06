af1ff4d5e4ca9340d98d24b366d85141d5935e21
mark hinschberger <markhinschberger@gmail.com>
2025-11-06 08:20:47 +0000

    feat: list vbill

--- src/modules/dashboard/lists/constants.ts
+++ src/modules/dashboard/lists/constants.ts
@@ -1,9 +1,9 @@
 import { CustomMarket } from 'src/ui-config/marketsConfig';
 
 export const HIDDEN_ASSETS: Partial<Record<CustomMarket, string[]>> = {
-  [CustomMarket.proto_horizon_v3]: [
-    '0x2255718832bc9fd3be1caf75084f4803da14ff01'.toLowerCase(), // VBILL
-  ],
+  // [CustomMarket.proto_horizon_v3]: [
+  //   '0x2255718832bc9fd3be1caf75084f4803da14ff01'.toLowerCase(), // VBILL
+  // ],
 };
 
 export const isAssetHidden = (market: CustomMarket, underlyingAsset: string) => {
