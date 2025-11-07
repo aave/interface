0c7d0fbadd7efe601f719a51e32dc0dc806f6757
Alejandro <95312462+AGMASO@users.noreply.github.com>
2025-11-07 17:38:58 +0100

    fix: new rpc url for optimism

---
+++ src/shims/baseAccountConstants.ts
@@ -0,0 +1,5 @@
+// interface/src/shims/baseAccountConstants.ts
+export const CB_KEYS_URL = 'https://keys.coinbase.com/connect';
+export const CB_WALLET_RPC_URL = 'https://rpc.wallet.coinbase.com';
+export const PACKAGE_NAME = '@base-org/account';
+export const PACKAGE_VERSION = '2.4.0';
