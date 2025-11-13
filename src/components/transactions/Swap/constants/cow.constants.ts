0ed0099500bc1739328981419b6b2bf4315a34f7
Martin Grabina <mgrabina@itba.edu.ar>
2025-11-13 13:04:32 -0300

    turn on all

--- src/components/transactions/Swap/constants/cow.constants.ts
+++ src/components/transactions/Swap/constants/cow.constants.ts
@@ -65,76 +65,74 @@ export const DUST_PROTECTION_MULTIPLIER = 1.001;
 export const COW_UNSUPPORTED_ASSETS: Partial<
   Record<SwapType | 'ALL', Partial<Record<SupportedChainId, string[] | 'ALL'>>>
 > = {
-  // For adapters we start supporting only base
-  [SwapType.DebtSwap]: {
-    [SupportedChainId.ARBITRUM_ONE]: 'ALL',
-    [SupportedChainId.AVALANCHE]: 'ALL',
-    [SupportedChainId.BNB]: 'ALL',
-    [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
-    [SupportedChainId.MAINNET]: 'ALL',
-    [SupportedChainId.POLYGON]: 'ALL',
-    [SupportedChainId.SEPOLIA]: 'ALL',
-    [SupportedChainId.BASE]: 'ALL', // Base is not supported
-  },
-  [SwapType.CollateralSwap]: {
-    [SupportedChainId.ARBITRUM_ONE]: 'ALL',
-    [SupportedChainId.AVALANCHE]: 'ALL',
-    [SupportedChainId.BNB]: 'ALL',
-    [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
-    [SupportedChainId.MAINNET]: 'ALL',
-    [SupportedChainId.POLYGON]: 'ALL',
-    [SupportedChainId.SEPOLIA]: 'ALL',
-    // Base is supported
-  },
-  [SwapType.RepayWithCollateral]: {
-    [SupportedChainId.ARBITRUM_ONE]: 'ALL',
-    [SupportedChainId.AVALANCHE]: 'ALL',
-    [SupportedChainId.BNB]: 'ALL',
-    [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
-    [SupportedChainId.MAINNET]: 'ALL',
-    [SupportedChainId.POLYGON]: 'ALL',
-    [SupportedChainId.SEPOLIA]: 'ALL',
-    [SupportedChainId.BASE]: 'ALL', // Base is not supported
-  },
+  // // For adapters we start supporting only base
+  // [SwapType.DebtSwap]: {
+  //   [SupportedChainId.ARBITRUM_ONE]: 'ALL',
+  //   [SupportedChainId.AVALANCHE]: 'ALL',
+  //   [SupportedChainId.BNB]: 'ALL',
+  //   [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
+  //   [SupportedChainId.MAINNET]: 'ALL',
+  //   [SupportedChainId.POLYGON]: 'ALL',
+  //   [SupportedChainId.SEPOLIA]: 'ALL',
+  //   // Base is supported
+  // },
+  // [SwapType.CollateralSwap]: {
+  //   [SupportedChainId.ARBITRUM_ONE]: 'ALL',
+  //   [SupportedChainId.AVALANCHE]: 'ALL',
+  //   [SupportedChainId.BNB]: 'ALL',
+  //   [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
+  //   [SupportedChainId.MAINNET]: 'ALL',
+  //   [SupportedChainId.POLYGON]: 'ALL',
+  //   [SupportedChainId.SEPOLIA]: 'ALL',
+  //   // Base is supported
+  // },
+  // [SwapType.RepayWithCollateral]: {
+  //   [SupportedChainId.ARBITRUM_ONE]: 'ALL',
+  //   [SupportedChainId.AVALANCHE]: 'ALL',
+  //   [SupportedChainId.BNB]: 'ALL',
+  //   [SupportedChainId.GNOSIS_CHAIN]: 'ALL',
+  //   [SupportedChainId.MAINNET]: 'ALL',
+  //   [SupportedChainId.POLYGON]: 'ALL',
+  //   [SupportedChainId.SEPOLIA]: 'ALL',
+  //   // Base is supported
+  // },
 
-  // Specific assets that are not supported for certain chains across all swap types
-  ['ALL']: {
-    [SupportedChainId.POLYGON]: [
-      '0x8eb270e296023e9d92081fdf967ddd7878724424'.toLowerCase(), // aPOLGHST not supported
-      '0x38d693ce1df5aadf7bc62595a37d667ad57922e5'.toLowerCase(), // aPolEURS not supported
-      '0xea1132120ddcdda2f119e99fa7a27a0d036f7ac9'.toLowerCase(), // aPolSTMATIC not supported
-      '0x6533afac2e7bccb20dca161449a13a32d391fb00'.toLowerCase(), // aPolJEUR not supported
-      '0x513c7e3a9c69ca3e22550ef58ac1c0088e918fff'.toLowerCase(), // aPolCRV not supported
-      '0xebe517846d0f36eced99c735cbf6131e1feb775d'.toLowerCase(), // aPolMIMATIC not supported
-      '0xc45a479877e1e9dfe9fcd4056c699575a1045daa'.toLowerCase(), // aPolSUSHI not supported
-      '0x8437d7c167dfb82ed4cb79cd44b7a32a1dd95c77'.toLowerCase(), // aPolAGEUR not supported
-      '0x724dc807b04555b71ed48a6896b6f41593b8c637'.toLowerCase(), // aPolDPI not supported
-      '0x8ffdf2de812095b1d19cb146e4c004587c0a0692'.toLowerCase(), // aPolBAL not supported
-    ],
-    [SupportedChainId.AVALANCHE]: [
-      '0x8eb270e296023e9d92081fdf967ddd7878724424'.toLowerCase(), // AVaMAI not supported
-      '0x078f358208685046a11c85e8ad32895ded33a249'.toLowerCase(), // aVaWBTC not supported
-      '0xc45a479877e1e9dfe9fcd4056c699575a1045daa'.toLowerCase(), // aVaFRAX not supported
-    ],
-    [SupportedChainId.GNOSIS_CHAIN]: [
-      '0xedbc7449a9b594ca4e053d9737ec5dc4cbccbfb2'.toLowerCase(), // EURe USD Price not supported
-    ],
-    [SupportedChainId.ARBITRUM_ONE]: [
-      '0x62fC96b27a510cF4977B59FF952Dc32378Cc221d'.toLowerCase(), // atBTC does not have good solver liquidity
-    ],
-    [SupportedChainId.BASE]: [
-      '0x90072A4aA69B5Eb74984Ab823EFC5f91e90b3a72'.toLowerCase(), // alBTC does not have good solver liquidity
-      '0x6Bb7a212910682DCFdbd5BCBb3e28FB4E8da10Ee'.toLowerCase(), // GHO does not have good solver liquidity
-      '0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42'.toLowerCase(), // EURC does not have good solver liquidity
-    ],
-    [SupportedChainId.MAINNET]: [
-      '0x00907f9921424583e7ffBfEdf84F92B7B2Be4977'.toLowerCase(), // aGHO not supported
-      '0x18eFE565A5373f430e2F809b97De30335B3ad96A'.toLowerCase(), // aGHO not supported
-    ],
-    [SupportedChainId.SEPOLIA]: [
-      '0xd190eF37dB51Bb955A680fF1A85763CC72d083D4'.toLowerCase(), // aGHO not supported
-    ],
-  },
+  // // Specific assets that are not supported for certain chains across all swap types
+  // ['ALL']: {
+  //   [SupportedChainId.POLYGON]: [
+  //     '0x8eb270e296023e9d92081fdf967ddd7878724424'.toLowerCase(), // aPOLGHST not supported
+  //     '0x38d693ce1df5aadf7bc62595a37d667ad57922e5'.toLowerCase(), // aPolEURS not supported
+  //     '0xea1132120ddcdda2f119e99fa7a27a0d036f7ac9'.toLowerCase(), // aPolSTMATIC not supported
+  //     '0x6533afac2e7bccb20dca161449a13a32d391fb00'.toLowerCase(), // aPolJEUR not supported
+  //     '0x513c7e3a9c69ca3e22550ef58ac1c0088e918fff'.toLowerCase(), // aPolCRV not supported
+  //     '0xebe517846d0f36eced99c735cbf6131e1feb775d'.toLowerCase(), // aPolMIMATIC not supported
+  //     '0xc45a479877e1e9dfe9fcd4056c699575a1045daa'.toLowerCase(), // aPolSUSHI not supported
+  //     '0x8437d7c167dfb82ed4cb79cd44b7a32a1dd95c77'.toLowerCase(), // aPolAGEUR not supported
+  //     '0x724dc807b04555b71ed48a6896b6f41593b8c637'.toLowerCase(), // aPolDPI not supported
+  //     '0x8ffdf2de812095b1d19cb146e4c004587c0a0692'.toLowerCase(), // aPolBAL not supported
+  //   ],
+  //   [SupportedChainId.AVALANCHE]: [
+  //     '0x8eb270e296023e9d92081fdf967ddd7878724424'.toLowerCase(), // AVaMAI not supported
+  //     '0x078f358208685046a11c85e8ad32895ded33a249'.toLowerCase(), // aVaWBTC not supported
+  //     '0xc45a479877e1e9dfe9fcd4056c699575a1045daa'.toLowerCase(), // aVaFRAX not supported
+  //   ],
+  //   [SupportedChainId.GNOSIS_CHAIN]: [
+  //     '0xedbc7449a9b594ca4e053d9737ec5dc4cbccbfb2'.toLowerCase(), // EURe USD Price not supported
+  //   ],
+  //   [SupportedChainId.ARBITRUM_ONE]: [
+  //     '0x62fC96b27a510cF4977B59FF952Dc32378Cc221d'.toLowerCase(), // atBTC does not have good solver liquidity
+  //   ],
+  //   [SupportedChainId.BASE]: [
+  //     '0x90072A4aA69B5Eb74984Ab823EFC5f91e90b3a72'.toLowerCase(), // alBTC does not have good solver liquidity
+  //   ],
+  //   [SupportedChainId.MAINNET]: [
+  //     '0x00907f9921424583e7ffBfEdf84F92B7B2Be4977'.toLowerCase(), // aGHO not supported
+  //     '0x18eFE565A5373f430e2F809b97De30335B3ad96A'.toLowerCase(), // aGHO not supported
+  //   ],
+  //   [SupportedChainId.SEPOLIA]: [
+  //     '0xd190eF37dB51Bb955A680fF1A85763CC72d083D4'.toLowerCase(), // aGHO not supported
+  //   ],
+  // },
 };
 
 export const CoWProtocolSupportedNetworks = [
