709d74c218cf8905155fd60a9d4f5ee0e6c5e86c
Martin Grabina <mgrabina@itba.edu.ar>
2025-11-18 19:48:05 -0300

    feat: activate repay-with-collateral on base cow adapters

--- src/components/transactions/Swap/constants/cow.constants.ts
+++ src/components/transactions/Swap/constants/cow.constants.ts
@@ -90,7 +90,7 @@ export const COW_UNSUPPORTED_ASSETS: Partial<
     [SupportedChainId.MAINNET]: 'ALL',
     [SupportedChainId.POLYGON]: 'ALL',
     [SupportedChainId.SEPOLIA]: 'ALL',
-    [SupportedChainId.BASE]: 'ALL', // Base is not supported
+    // Base is supported
   },
 
   // Specific assets that are not supported for certain chains across all swap types
