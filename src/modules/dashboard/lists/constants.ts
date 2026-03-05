602712d327ee7f7256aa4a9fc666ed9965b69322
Mark Hinschberger <foodaka@users.noreply.github.com>
2026-03-05 17:53:48 +0000

    chore: acred listing (#2891)

--- src/modules/dashboard/lists/constants.ts
+++ src/modules/dashboard/lists/constants.ts
@@ -1,9 +1,9 @@
 import { CustomMarket } from 'src/ui-config/marketsConfig';
 
 export const HIDDEN_ASSETS: Partial<Record<CustomMarket, string[]>> = {
-  // [CustomMarket.proto_horizon_v3]: [
-  //   '0x2255718832bc9fd3be1caf75084f4803da14ff01'.toLowerCase(), // VBILL
-  // ],
+  [CustomMarket.proto_horizon_v3]: [
+    '0x17418038ecf73ba4026c4f428547bf099706f27b'.toLowerCase(), // aCRED
+  ],
 };
 
 export const isAssetHidden = (market: CustomMarket, underlyingAsset: string) => {
