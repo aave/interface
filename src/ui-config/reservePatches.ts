add529e7ad27c23b43990017aa73a5b763667b46
Mark Grothe <grothem@gmail.com>
2026-04-09 02:05:56 -0500

    chore: pt strata token icon (#2926)

--- src/ui-config/reservePatches.ts
+++ src/ui-config/reservePatches.ts
@@ -255,6 +255,11 @@ export function fetchIconSymbolAndName({ underlyingAsset, symbol, name }: IconSy
       name: 'PT sUSDe June 2026',
       iconSymbol: 'ptsusde',
     },
+    '0x619d75e3b790ebc21c289f2805bb7177a7d732e2': {
+      symbol: 'PT Strata Senior USDe June 25th 2026',
+      name: 'PT Strata Senior USDe June 2026',
+      iconSymbol: 'ptsrusde',
+    },
 
     '0xa693B19d2931d498c5B318dF961919BB4aee87a5': { iconSymbol: 'UST', name: 'UST (Wormhole)' },
     '0x59a19d8c652fa0284f44113d0ff9aba70bd46fb4': { iconSymbol: 'BPT_BAL_WETH' },
