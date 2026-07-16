97dc2cd119e64cfa138a60df4d3fbec258f07da3
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2026-07-17 07:35:59 +0900

    style(modal): apply H4 spec to modal titles

--- src/components/transactions/FlowCommons/TxModalTitle.tsx
+++ src/components/transactions/FlowCommons/TxModalTitle.tsx
@@ -8,7 +8,16 @@ export type TxModalTitleProps = {
 
 export const TxModalTitle = ({ title, symbol }: TxModalTitleProps) => {
   return (
-    <Typography variant="h2" sx={{ mb: 6 }}>
+    <Typography
+      variant="h2"
+      sx={(theme) => ({
+        mb: 6,
+        color: theme.palette.fig['fg-1'],
+        fontSize: '1rem',
+        fontWeight: 500,
+        lineHeight: '1.5rem',
+      })}
+    >
       {title} {symbol ?? ''}
     </Typography>
   );
