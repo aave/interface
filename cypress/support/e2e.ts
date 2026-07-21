6c33e183b560714461944398ae870e3f1bb19102
Mark Grothe <grothem@gmail.com>
2026-07-21 15:34:13 -0500

    fix: compliance stub

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -5,3 +5,15 @@ Cypress.on('uncaught:exception', (err) => {
     return false;
   }
 });
+
+beforeEach(() => {
+  // Stub the compliance check. A test that specifically covers
+  // the modal can override this with its own later cy.intercept if needed
+  cy.intercept('GET', /\/api\/preflight-compliance/, {
+    statusCode: 200,
+    body: {
+      result: true,
+      nextCheck: new Date(Date.now() + 60 * 60 * 1000).toISOString(),
+    },
+  });
+});
