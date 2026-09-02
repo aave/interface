28f295eccc714d40ff9afd19882fddd127228f52
Alejandro <95312462+AGMASO@users.noreply.github.com>
2026-09-03 00:46:25 +0900

    fix: tooltip netAPY (#3095)

--- src/components/infoTooltips/NetAPYTooltip.tsx
+++ src/components/infoTooltips/NetAPYTooltip.tsx
@@ -6,8 +6,9 @@ export const NetAPYTooltip = ({ ...rest }: TextWithTooltipProps) => {
   return (
     <TextWithTooltip {...rest}>
       <Trans>
-        Net APY is the combined effect of all supply and borrow positions on net worth, including
-        incentives. It is possible to have a negative net APY if debt APY is higher than supply APY.
+        Net APY is the combined effect of all supply and borrow positions on net worth, Merkl
+        incentives are not included. It is possible to have a negative net APY if debt APY is higher
+        than supply APY.
       </Trans>
     </TextWithTooltip>
   );
